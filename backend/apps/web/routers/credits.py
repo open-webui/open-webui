1f1aefa6e843fc6df2b5e0555efadf776f7183eb
Karolis Rusenas <karolis@rusenas.dev>
2023-12-28 11:59:01 +0000

    stripe checkout start

--- backend/apps/web/routers/credits.py
+++ backend/apps/web/routers/credits.py
@@ -15,6 +15,14 @@ from utils.utils import (
 )
 from constants import ERROR_MESSAGES
 
+import stripe
+
+# This is your test secret API key.
+stripe.api_key = os.environ.get('STRIPE_API_KEY')
+price_id = os.environ.get('STRIPE_PRICE_ID')
+
+app_url = 'http://localhost:8888'
+
 class UserCredits(BaseModel):
     email: str
     balance: float
@@ -46,4 +54,40 @@ async def get_user_credits(cred=Depends(bearer_scheme)):
             detail=ERROR_MESSAGES.INVALID_TOKEN,
         )
         
+class UserCreditsTopUp(BaseModel):
+    url: str
 
+@router.post("/checkout-session", response_model=Optional[UserCreditsTopUp])
+async def start_user_checkout(cred=Depends(bearer_scheme)):
+    token = cred.credentials
+    user = Users.get_user_by_token(token)
+
+    if user:        
+        try:
+            print('trying to create checkout session')
+            checkout_session = stripe.checkout.Session.create(
+                customer_email=user.email,
+                line_items=[
+                    {
+                        'price': price_id,
+                        'quantity': 5,
+                    },
+                ],
+                mode='payment',
+                success_url=app_url + '/success.html',
+                cancel_url=app_url + '/cancel.html',
+            )
+        except Exception as e:
+            print(e)
+            return str(e)
+
+        print('checkout session created', checkout_session.url)
+        
+        return {
+            "url": checkout_session.url
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=ERROR_MESSAGES.INVALID_TOKEN,
+        )
