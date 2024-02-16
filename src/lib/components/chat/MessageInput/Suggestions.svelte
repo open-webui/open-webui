99faf9032488190f2eb14c3506e5df3aa37af534
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-02-16 13:39:32 -0800

    fix: prompt suggestions styling

--- src/lib/components/chat/MessageInput/Suggestions.svelte
+++ src/lib/components/chat/MessageInput/Suggestions.svelte
@@ -12,7 +12,9 @@
 
 <div class=" flex flex-wrap-reverse mb-3 md:p-1 text-left w-full">
 	{#each prompts as prompt, promptIdx}
-		<div class="{promptIdx > 1 ? 'hidden sm:inline-flex' : ''} basis-full sm:basis-1/2 p-[5px]">
+		<div
+			class="{promptIdx > 1 ? 'hidden sm:inline-flex' : ''} basis-full sm:basis-1/2 p-[5px] px-2"
+		>
 			<button
 				class=" flex-1 flex justify-between w-full h-full px-4 py-2.5 bg-white hover:bg-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 outline outline-1 outline-gray-200 dark:outline-gray-800 rounded-lg transition group"
 				on:click={() => {
@@ -22,7 +24,7 @@
 				<div class="flex flex-col text-left self-center">
 					{#if prompt.title && prompt.title[0] !== ''}
 						<div class="text-sm font-medium dark:text-gray-300">{prompt.title[0]}</div>
-						<div class="text-sm text-gray-500">{prompt.title[1]}</div>
+						<div class="text-sm text-gray-500 line-clamp-1">{prompt.title[1]}</div>
 					{:else}
 						<div class=" self-center text-sm font-medium dark:text-gray-300 line-clamp-2">
 							{prompt.content}
