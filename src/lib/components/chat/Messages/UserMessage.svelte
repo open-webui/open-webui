c3ed6fe974c5debcd1d4281c2b331c9199db5c42
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-02-18 14:58:26 -0800

    Update UserMessage.svelte

--- src/lib/components/chat/Messages/UserMessage.svelte
+++ src/lib/components/chat/Messages/UserMessage.svelte
@@ -295,7 +295,7 @@
 								viewBox="0 0 24 24"
 								stroke-width="1.5"
 								stroke="currentColor"
-								class="w-6 h-6"
+								class="w-4 h-4"
 							>
 								<path
 									stroke-linecap="round"
