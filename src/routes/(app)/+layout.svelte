5519ebb5314b02ca471b85ac187350eb02f63db3
Averath <kochamame@gmail.com>
2024-02-22 18:08:17 +0100

    Add vertex ai model

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -12,6 +12,7 @@
 	import { getPrompts } from '$lib/apis/prompts';
 
 	import { getOpenAIModels } from '$lib/apis/openai';
+	import { getVertexAIModels } from '$lib/apis/vertexai';
 
 	import {
 		user,
@@ -58,6 +59,13 @@
 		});
 
 		models.push(...(openAIModels ? [{ name: 'hr' }, ...openAIModels] : []));
+
+		const vertexAIModels = await getVertexAIModels(localStorage.token).catch((error) => {
+			console.log(error);
+			return null;
+		});
+
+		models.push(...(vertexAIModels ? [{ name: 'hr' }, ...vertexAIModels] : []));
 
 		return models;
 	};
