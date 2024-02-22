7a730c3f0f162f95ebb901106b042fcebbcf161c
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-02-21 17:10:27 -0800

    refac: versioning using package

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	define: {
+		APP_VERSION: JSON.stringify(process.env.npm_package_version)
+	}
 });
