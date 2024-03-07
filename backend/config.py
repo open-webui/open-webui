c255cba198c0d7de924890fa5cb5bc035e3ded7b
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-03-06 16:13:25 -0800

    feat: multiple openai apis

--- backend/config.py
+++ backend/config.py
@@ -240,7 +240,7 @@ if OPENAI_API_BASE_URL == "":
 OPENAI_API_KEYS = os.environ.get("OPENAI_API_KEYS", "")
 OPENAI_API_KEYS = OPENAI_API_KEYS if OPENAI_API_KEYS != "" else OPENAI_API_KEY
 
-OPENAI_API_KEYS = [url.strip() for url in OPENAI_API_BASE_URL.split(";")]
+OPENAI_API_KEYS = [url.strip() for url in OPENAI_API_KEYS.split(";")]
 
 
 OPENAI_API_BASE_URLS = os.environ.get("OPENAI_API_BASE_URLS", "")
