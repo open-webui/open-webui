2bc3b3c8cb573674974d9f517d3b491d778af76d
Silentoplayz <50341825+Silentoplayz@users.noreply.github.com>
2024-03-13 14:07:52 -0400

    Add files via upload
    -Updated `General.svelte` file.

--- src/lib/components/chat/Settings/General.svelte
+++ src/lib/components/chat/Settings/General.svelte
@@ -11,12 +11,47 @@
 	export let getModels: Function;
 
 	// General
-	let themes = ['dark', 'light', 'rose-pine dark', 'rose-pine-dawn light'];
+	let themes = ['dark', 'light', 'rose-pine dark', 'rose-pine-dawn light', 'oled-dark'];
 	let theme = 'dark';
 	let notificationEnabled = false;
 	let system = '';
 
 	let showAdvanced = false;
+
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+		if (theme === 'system') {
+			updateSystemTheme();
+		}
+	});
+
+	function updateSystemTheme() {
+		const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+		const systemTheme = isDarkMode ? 'dark' : 'light';
+		applyTheme(systemTheme);
+	}
+
+	function applyTheme(theme: string) {
+		localStorage.theme = theme;
+
+		if (theme === 'system') {
+			updateSystemTheme();
+			return;
+		}
+
+		themes
+			.filter((e) => e !== theme)
+			.forEach((e) => {
+				e.split(' ').forEach((e) => {
+					document.documentElement.classList.remove(e);
+				});
+			});
+
+		theme.split(' ').forEach((e) => {
+			document.documentElement.classList.add(e);
+		});
+
+		console.log(theme)
+	}
 
 	const toggleNotification = async () => {
 		const permission = await Notification.requestPermission();
@@ -123,24 +158,10 @@
 						class="w-fit pr-8 rounded py-2 px-2 text-xs bg-transparent outline-none text-right"
 						bind:value={theme}
 						placeholder="Select a theme"
-						on:change={(e) => {
-							localStorage.theme = theme;
-
-							themes
-								.filter((e) => e !== theme)
-								.forEach((e) => {
-									e.split(' ').forEach((e) => {
-										document.documentElement.classList.remove(e);
-									});
-								});
-
-							theme.split(' ').forEach((e) => {
-								document.documentElement.classList.add(e);
-							});
-
-							console.log(theme);
-						}}
+						on:change="{() => applyTheme(theme)}"
 					>
+						<option value="system">System</option>
+						<option value="oled-dark">OLED Dark</option>
 						<option value="dark">Dark</option>
 						<option value="light">Light</option>
 						<option value="rose-pine dark">Rosé Pine</option>
@@ -262,7 +283,7 @@
 
 	<div class="flex justify-end pt-3 text-sm font-medium">
 		<button
-			class="  px-4 py-2 bg-emerald-700 hover:bg-emerald-800 text-gray-100 transition rounded-lg"
+			class=" px-4 py-2 bg-emerald-600 hover:bg-emerald-700 text-gray-100 transition rounded"
 			on:click={() => {
 				saveSettings({
 					system: system !== '' ? system : undefined,
