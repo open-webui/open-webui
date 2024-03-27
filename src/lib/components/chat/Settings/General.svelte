dfeadf9595e6893bf5134e55ad911527071098f5
Danny Liu <dannyjialiliu@gmail.com>
2024-03-27 16:05:37 -0700

    ordering of themes in select menu, group dark themes together

--- src/lib/components/chat/Settings/General.svelte
+++ src/lib/components/chat/Settings/General.svelte
@@ -147,10 +147,10 @@
 					>
 						<option value="system">⚙️ {$i18n.t('System')}</option>
 						<option value="dark">🌑 {$i18n.t('Dark')}</option>
+						<option value="oled-dark">🌌 {$i18n.t('OLED Dark')}</option>
 						<option value="light">☀️ {$i18n.t('Light')}</option>
 						<option value="rose-pine dark">🪻 {$i18n.t('Rosé Pine')}</option>
 						<option value="rose-pine-dawn light">🌷 {$i18n.t('Rosé Pine Dawn')}</option>
-						<option value="oled-dark">🌌 {$i18n.t('OLED Dark')}</option>
 					</select>
 				</div>
 			</div>
