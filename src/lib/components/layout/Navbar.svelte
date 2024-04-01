698bfcf87ff36f476a81e9476e08dd301852de86
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-04-01 01:48:44 -0700

    fix: navbar styling

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -140,7 +140,7 @@
 			</div>
 
 			<div class="self-start flex flex-none items-center">
-				<div class="flex self-center w-[1px] h-5 mx-2 bg-stone-700" />
+				<div class="flex self-center w-[1px] h-5 mx-2 bg-gray-300 dark:bg-stone-700" />
 
 				{#if !shareEnabled}
 					<Tooltip content="Settings">
