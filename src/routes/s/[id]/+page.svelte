807a60cccfe8972d5c07d82b45777b343eb64b68
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-04-02 09:01:15 -0700

    fix: styling

--- src/routes/s/[id]/+page.svelte
+++ src/routes/s/[id]/+page.svelte
@@ -137,26 +137,26 @@
 	<div
 		class="min-h-screen max-h-screen w-full flex flex-col text-gray-700 dark:text-gray-100 bg-white dark:bg-gray-900"
 	>
-		<div class="flex flex-col flex-auto justify-center">
+		<div class="flex flex-col flex-auto justify-center py-8">
+			<div class="px-3 w-full max-w-3xl mx-auto">
+				<div>
+					<div class=" text-3xl font-semibold line-clamp-1">
+						{title}
+					</div>
+
+					<div class=" mt-1 text-gray-400">
+						{dayjs(chat.chat.timestamp).format('MMMM D, YYYY')}
+					</div>
+				</div>
+
+				<hr class=" dark:border-gray-800 mt-6 mb-2" />
+			</div>
+
 			<div
-				class=" py-6 flex flex-col justify-center w-full flex-auto overflow-auto h-0 max-w-3xl mx-auto"
+				class=" flex flex-col justify-center w-full flex-auto overflow-auto h-0"
 				id="messages-container"
 			>
 				<div class=" h-full w-full flex flex-col py-4">
-					<div class="px-3">
-						<div>
-							<div class=" text-3xl font-semibold line-clamp-1">
-								{title}
-							</div>
-
-							<div class=" mt-1 text-gray-400">
-								{dayjs(chat.chat.timestamp).format('MMMM D, YYYY')}
-							</div>
-						</div>
-
-						<hr class=" dark:border-gray-800 my-6" />
-					</div>
-
 					<div class="py-2">
 						<Messages
 							chatId={$chatId}
