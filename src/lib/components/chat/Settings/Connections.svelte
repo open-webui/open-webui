3fe7894dae12be518587afe72f1d28c63ffea1a1
algorithmlover2016 <yubo.upt@gmail.com>
2024-04-19 11:13:01 +0800

    add api -version

--- src/lib/components/chat/Settings/Connections.svelte
+++ src/lib/components/chat/Settings/Connections.svelte
@@ -28,6 +28,16 @@
 
 	let showOpenAI = false;
 
+	let AZURE_OPENAI_API_KEY = '';
+	let AZURE_OPENAI_API_VERSION = '';
+	let AZURE_OPENAI_API_BASE_URL = '';
+
+	let AZURE_OPENAI_API_KEYS = [''];
+	let AZURE_OPENAI_API_VERSIONS = [''];
+	let AZURE_OPENAI_API_BASE_URLS = [''];
+
+	let showAzureOpenAI = false;
+
 	const updateOpenAIHandler = async () => {
 		OPENAI_API_BASE_URLS = await updateOpenAIUrls(localStorage.token, OPENAI_API_BASE_URLS);
 		OPENAI_API_KEYS = await updateOpenAIKeys(localStorage.token, OPENAI_API_KEYS);
@@ -129,6 +139,105 @@
 													(url, urlIdx) => idx !== urlIdx
 												);
 												OPENAI_API_KEYS = OPENAI_API_KEYS.filter((key, keyIdx) => idx !== keyIdx);
+											}}
+											type="button"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 16 16"
+												fill="currentColor"
+												class="w-4 h-4"
+											>
+												<path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5h-8.5Z" />
+											</svg>
+										</button>
+									{/if}
+								</div>
+							</div>
+							<div class=" mb-1 text-xs text-gray-400 dark:text-gray-500">
+								{$i18n.t('WebUI will make requests to')}
+								<span class=" text-gray-200">'{url}/models'</span>
+							</div>
+						{/each}
+					</div>
+				{/if}
+			</div>
+		</div>
+
+
+		<div class=" space-y-3">
+			<div class="mt-2 space-y-2 pr-1.5">
+				<div class="flex justify-between items-center text-sm">
+					<div class="  font-medium">{$i18n.t('AzureOpenAI API')}</div>
+					<button
+						class=" text-xs font-medium text-gray-500"
+						type="button"
+						on:click={() => {
+							showAzureOpenAI = !showAzureOpenAI;
+						}}>{showAzureOpenAI ? $i18n.t('Hide') : $i18n.t('Show')}</button
+					>
+				</div>
+
+				{#if showAzureOpenAI}
+					<div class="flex flex-col gap-1">
+						{#each AZURE_OPENAI_API_BASE_URLS as url, idx}
+							<div class="flex w-full gap-2">
+								<div class="flex-1">
+									<input
+										class="w-full rounded-lg py-2 px-4 text-sm dark:text-gray-300 dark:bg-gray-850 outline-none"
+										placeholder={$i18n.t('AZURE API Base URL')}
+										bind:value={url}
+										autocomplete="off"
+									/>
+								</div>
+
+								<div class="flex-1">
+									<input
+										class="w-full rounded-lg py-2 px-4 text-sm dark:text-gray-300 dark:bg-gray-850 outline-none"
+										placeholder={$i18n.t('AZURE API Key')}
+										bind:value={AZURE_OPENAI_API_KEYS[idx]}
+										autocomplete="off"
+									/>
+								</div>
+								<div class="flex-1">
+									<input
+										class="w-full rounded-lg py-2 px-4 text-sm dark:text-gray-300 dark:bg-gray-850 outline-none"
+										placeholder={$i18n.t('AZURE API Version')}
+										bind:value={AZURE_OPENAI_API_VERSIONS[idx]}
+										autocomplete="off"
+									/>
+								</div>
+								<div class="self-center flex items-center">
+									{#if idx === 0}
+										<button
+											class="px-1"
+											on:click={() => {
+												AZURE_OPENAI_API_BASE_URLS = [...AZURE_OPENAI_API_BASE_URLS, ''];
+												AZURE_OPENAI_API_KEYS = [...AZURE_OPENAI_API_KEYS, ''];
+												AZURE_OPENAI_API_VERSIONS = [...AZURE_OPENAI_API_VERSIONS, ''];
+											}}
+											type="button"
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 16 16"
+												fill="currentColor"
+												class="w-4 h-4"
+											>
+												<path
+													d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z"
+												/>
+											</svg>
+										</button>
+									{:else}
+										<button
+											class="px-1"
+											on:click={() => {
+												AZURE_OPENAI_API_BASE_URLS = AZURE_OPENAI_API_BASE_URLS.filter(
+													(url, urlIdx) => idx !== urlIdx
+												);
+												AZURE_OPENAI_API_KEYS = AZURE_OPENAI_API_KEYS.filter((key, keyIdx) => idx !== keyIdx);
+												AZURE_OPENAI_API_VERSIONS = AZURE_OPENAI_API_VERSIONS.filter((key, versionIdx) => idx !== versionIdx);
 											}}
 											type="button"
 										>
