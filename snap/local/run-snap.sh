461f41bdd3a81e53e7c1869536cd32080ed3d2a7
Matias Piipari <matias.piipari@gmail.com>
2024-04-21 00:38:50 +0300

    Set STATIC_DIR

--- snap/local/run-snap.sh
+++ snap/local/run-snap.sh
@@ -3,12 +3,13 @@
 set -e
 
 DATA_DIR=$(snapctl get data-dir) \
-PORT=$(snapctl get port) \
+STATIC_DIR=$SNAP_DATA/static \
+FRONTEND_BUILD_DIR=$SNAP/build \
 HOST=$(snapctl get host) \
+PORT=$(snapctl get port) \
 WEBUI_SECRET_KEY=$(snapctl get secret-key) \
 OLLAMA_API_BASE_URL=$(snapctl get ollama-api-base-url) \
 OPENAI_API_BASE_URL=$(snapctl get openai-api-base-url) \
 OPENAI_API_KEY=$(snapctl get openai-api-key) \
 ENABLE_SIGNUP=$(snapctl get enable-signup) \
-FRONTEND_BUILD_DIR=$SNAP/build \
-$SNAP/backend/start.sh $@
+    $SNAP/backend/start.sh $@
