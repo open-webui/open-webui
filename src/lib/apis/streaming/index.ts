4ad7ef84a4357e57232dd9a7db5a9e5be0335c74
Arman Ordookhani <arman.ordookhani@vestiairecollective.com>
2024-04-26 15:45:29 +0200

    add else

--- src/lib/apis/streaming/index.ts
+++ src/lib/apis/streaming/index.ts
@@ -31,7 +31,7 @@ async function* openAIStreamToIterator(
 				console.log(line);
 				if (line === 'data: [DONE]') {
 					yield { done: true, value: '' };
-				} if (line.startsWith(':')) {
+				} else if (line.startsWith(':')) {
 					// Events starting with : are comments https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#event_stream_format
 					// OpenRouter sends heartbeats like ": OPENROUTER PROCESSING"
 					continue
