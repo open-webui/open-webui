7e0a2d32a365c09c6f2a501b6e24eea7c4ac41b5
Yanyutin753 <153794571+yubb-ai@users.noreply.github.com>
2024-04-28 19:12:03 +0800

    add YUBB theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -74,8 +74,8 @@
 
 	<!-- rosepine themes have been disabled as it's not up to date with our latest version. -->
 	<!-- feel free to make a PR to fix if anyone wants to see it return -->
-	<!-- <link rel="stylesheet" type="text/css" href="/themes/rosepine.css" />
-	<link rel="stylesheet" type="text/css" href="/themes/rosepine-dawn.css" /> -->
+	<link rel="stylesheet" type="text/css" href="/themes/rosepine.css" />
+	<link rel="stylesheet" type="text/css" href="/themes/rosepine-dawn.css" />
 </svelte:head>
 
 {#if loaded}
