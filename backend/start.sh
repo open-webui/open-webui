b17eae504faa0b93ea8397cfe6072746ba000393
Lip.che <chehaoyu.zhang@foxmail.com>
2024-04-28 14:36:34 +0800

    8000

--- backend/start.sh
+++ backend/start.sh
@@ -5,7 +5,7 @@ cd "$SCRIPT_DIR" || exit
 
 KEY_FILE=.webui_secret_key
 
-PORT="${PORT:-8080}"
+PORT="${PORT:-8000}"
 if test "$WEBUI_SECRET_KEY $WEBUI_JWT_SECRET_KEY" = " "; then
   echo "No WEBUI_SECRET_KEY provided"
 
