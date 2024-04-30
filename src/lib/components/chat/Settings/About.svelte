c87deb27811c197ed3c5382790e333dd1617ae61
Yanyutin753 <132346501+Yanyutin753@users.noreply.github.com>
2024-04-30 11:23:46 +0800

    Update About.svelte

--- src/lib/components/chat/Settings/About.svelte
+++ src/lib/components/chat/Settings/About.svelte
@@ -135,8 +135,8 @@
 			{/if}{$i18n.t('Created by')}
 			<a
 				class=" text-gray-500 dark:text-gray-300 font-medium"
-				href="https://github.com/tjbck"
-				target="_blank">Timothy J. Baek</a
+				href="https://github.com/open-webui/open-webui"
+				target="_blank">open-webui</a
 			>
 		</div>
 	</div>
