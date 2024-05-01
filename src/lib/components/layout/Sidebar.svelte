e60c87d750dd456363a2c726b027e91b4f5ea6ea
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-01 16:08:06 -0700

    fix: delete chat shortcut

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -619,6 +619,27 @@
 											<ArchiveBox />
 										</button>
 									</Tooltip>
+
+									{#if chat.id === $chatId}
+										<button
+											id="delete-chat-button"
+											class="hidden"
+											on:click={() => {
+												chatDeleteId = chat.id;
+											}}
+										>
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												viewBox="0 0 16 16"
+												fill="currentColor"
+												class="w-4 h-4"
+											>
+												<path
+													d="M2 8a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM6.5 8a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM12.5 6.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z"
+												/>
+											</svg>
+										</button>
+									{/if}
 								</div>
 							{/if}
 						</div>
