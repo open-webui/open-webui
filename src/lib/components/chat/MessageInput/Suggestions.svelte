44884a888691129ab013fa534b4df6462a38ebe2
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-04-30 17:07:03 -0700

    feat: randomised suggestion

--- src/lib/components/chat/MessageInput/Suggestions.svelte
+++ src/lib/components/chat/MessageInput/Suggestions.svelte
@@ -4,7 +4,9 @@
 
 	let prompts = [];
 
-	$: prompts = suggestionPrompts;
+	$: prompts = suggestionPrompts
+		.reduce((acc, current) => [...acc, ...[current]], [])
+		.sort(() => Math.random() - 0.5);
 	// suggestionPrompts.length <= 4
 	// 	? suggestionPrompts
 	// 	: suggestionPrompts.sort(() => Math.random() - 0.5).slice(0, 4);
