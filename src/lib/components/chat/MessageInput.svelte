2bece7b4c5b670d3d4a0d985c54abdcca4868815
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-02 03:16:08 -0700

    fix: styling

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -500,7 +500,7 @@
 
 					{#if selectedModel !== ''}
 						<div
-							class="md:px-3 py-2.5 text-left w-full flex justify-between items-center absolute bottom-0 left-0 right-0 bg-gradient-to-t from-50% from-white dark:from-gray-900"
+							class="px-3 py-2.5 text-left w-full flex justify-between items-center absolute bottom-0 left-0 right-0 bg-gradient-to-t from-50% from-white dark:from-gray-900"
 						>
 							<div class="flex items-center gap-2 text-sm dark:text-gray-500">
 								<img
