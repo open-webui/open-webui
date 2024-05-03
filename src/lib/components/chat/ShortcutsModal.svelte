115c3a38a799e45fbf1d5eead586155945115f48
Yanyutin753 <132346501+Yanyutin753@users.noreply.github.com>
2024-05-03 17:49:58 +0800

    ✨add Symbol startup prompt startup instructions in ShortcutsModal

--- src/lib/components/chat/ShortcutsModal.svelte
+++ src/lib/components/chat/ShortcutsModal.svelte
@@ -208,9 +208,61 @@
 					</div>
 				</div>
 			</div>
+
+			<div class=" flex justify-between dark:text-gray-300 px-5 pt-4">
+				<div class=" text-lg font-medium self-center">{$i18n.t('Symbol startup prompt')}</div>
+			</div>
+
+			<div class="flex flex-col md:flex-row w-full p-5 md:space-x-4 dark:text-gray-200">
+				<div class=" flex flex-col w-full sm:flex-row sm:justify-center sm:space-x-6">
+					<div class="flex flex-col space-y-3 w-full self-start">
+						<div class="w-full flex justify-between items-center">
+							<div class=" text-sm">
+								{$i18n.t('Symbol startup prompt to activate RAG')}
+							</div>
+
+							<div class="flex space-x-1 text-xs">
+								<div
+									class=" h-fit py-1 px-2 flex items-center justify-center rounded border border-black/10 capitalize text-gray-600 dark:border-white/10 dark:text-gray-300"
+								>
+									#
+								</div>
+							</div>
+						</div>
+
+						<div class="w-full flex justify-between items-center">
+							<div class=" text-sm">
+								{$i18n.t('Symbol startup prompt to activate Chat Prompts')}
+							</div>
+
+							<div class="flex space-x-1 text-xs">
+								<div
+									class=" h-fit py-1 px-2 flex items-center justify-center rounded border border-black/10 capitalize text-gray-600 dark:border-white/10 dark:text-gray-300"
+								>
+									/
+								</div>
+							</div>
+						</div>
+
+						<div class="w-full flex justify-between items-center">
+							<div class=" text-sm">
+								{$i18n.t('Symbol startup prompt to activate Specific Models')}
+							</div>
+
+							<div class="flex space-x-1 text-xs">
+								<div
+									class=" h-fit py-1 px-2 flex items-center justify-center rounded border border-black/10 capitalize text-gray-600 dark:border-white/10 dark:text-gray-300"
+								>
+									@
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-</Modal>
+	</div></Modal
+>
 
 <style>
 	input::-webkit-outer-spin-button,
