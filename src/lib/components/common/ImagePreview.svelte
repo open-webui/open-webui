c915e9da681d198cb1e34c9efa55180d6d1e390e
Yanyutin753 <132346501+Yanyutin753@users.noreply.github.com>
2024-05-05 13:48:23 +0800

    Update ImagePreview.svelte

--- src/lib/components/common/ImagePreview.svelte
+++ src/lib/components/common/ImagePreview.svelte
@@ -14,7 +14,7 @@
 		fetch(url)
 			.then((response) => response.blob())
 			.then((blob) => {
-				const mimeType = mime.lookup(filename);
+				const mimeType = mime.getType(filename);
 				const newBlob = new Blob([blob], { type: mimeType });
 				const objectUrl = window.URL.createObjectURL(newBlob);
 				const link = document.createElement('a');
