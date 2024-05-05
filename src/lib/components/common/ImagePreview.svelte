6034ceb2cafde92ed80c9d159aaad75a6d22d882
Yanyutin753 <132346501+Yanyutin753@users.noreply.github.com>
2024-05-05 17:33:52 +0800

    test1

--- src/lib/components/common/ImagePreview.svelte
+++ src/lib/components/common/ImagePreview.svelte
@@ -13,8 +13,8 @@
 		fetch(url)
 			.then((response) => response.blob())
 			.then((blob) => {
-				const mime = require('mime-types');
-				const mimeType = mime.lookup(extension);
+				const mimeType = blob.type;
+				console.log('mimeType=' + mimeType);
 				const newBlob = new Blob([blob], { type: mimeType });
 				const objectUrl = window.URL.createObjectURL(newBlob);
 				const link = document.createElement('a');
