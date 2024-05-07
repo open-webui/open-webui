f58eb0d26640c144c84a4fe1f72abcd819e035f2
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-06 17:29:16 -0700

    feat: browser search engine support

--- backend/config.py
+++ backend/config.py
@@ -76,7 +76,10 @@ WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
 if WEBUI_NAME != "Open WebUI":
     WEBUI_NAME += " (Open WebUI)"
 
+WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
+
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+
 
 ####################################
 # ENV (dev,test,prod)
