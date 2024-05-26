bf8c32f09fe62422bc3321c32a0520c0745edcac
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-26 02:43:52 -0700

    refac: admin panel

--- src/lib/components/chat/Settings/Chats.svelte
+++ src/lib/components/chat/Settings/Chats.svelte
@@ -68,13 +68,6 @@
 		saveAs(blob, `chat-export-${Date.now()}.json`);
 	};
 
-	const exportAllUserChats = async () => {
-		let blob = new Blob([JSON.stringify(await getAllUserChats(localStorage.token))], {
-			type: 'application/json'
-		});
-		saveAs(blob, `all-chats-export-${Date.now()}.json`);
-	};
-
 	const deleteAllChatsHandler = async () => {
 		await goto('/');
 		await deleteAllChats(localStorage.token).catch((error) => {
@@ -298,36 +291,6 @@
 					</svg>
 				</div>
 				<div class=" self-center text-sm font-medium">{$i18n.t('Delete All Chats')}</div>
-			</button>
-		{/if}
-
-		{#if $user?.role === 'admin' && ($config?.admin_export_enabled ?? true)}
-			<hr class=" dark:border-gray-700" />
-
-			<button
-				class=" flex rounded-md py-2 px-3.5 w-full hover:bg-gray-200 dark:hover:bg-gray-800 transition"
-				on:click={() => {
-					exportAllUserChats();
-				}}
-			>
-				<div class=" self-center mr-3">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 16 16"
-						fill="currentColor"
-						class="w-4 h-4"
-					>
-						<path d="M2 3a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3Z" />
-						<path
-							fill-rule="evenodd"
-							d="M13 6H3v6a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V6ZM8.75 7.75a.75.75 0 0 0-1.5 0v2.69L6.03 9.22a.75.75 0 0 0-1.06 1.06l2.5 2.5a.75.75 0 0 0 1.06 0l2.5-2.5a.75.75 0 1 0-1.06-1.06l-1.22 1.22V7.75Z"
-							clip-rule="evenodd"
-						/>
-					</svg>
-				</div>
-				<div class=" self-center text-sm font-medium">
-					{$i18n.t('Export All Chats (All Users)')}
-				</div>
 			</button>
 		{/if}
 	</div>
