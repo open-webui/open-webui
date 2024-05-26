d77657f0d0ee690e3e30090b0c438a8d2931fbba
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-26 12:39:48 -0700

    refac: only show banners in landing

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -1007,7 +1007,7 @@
 			{initNewChat}
 		/>
 
-		{#if $banners.length > 0}
+		{#if $banners.length > 0 && !$chatId}
 			<div class="absolute top-16 w-full {$showSidebar ? 'md:max-w-[calc(100%-260px)]' : ''}">
 				<div class=" flex flex-col gap-1 w-full">
 					{#each $banners.filter( (b) => (b.dismissible ? !JSON.parse(localStorage.getItem('dismissedBannerIds') ?? '[]').includes(b.id) : true) ) as banner}
