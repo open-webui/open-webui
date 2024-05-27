b6b71c08f3e1811b819a8d8f68cb4d83e57163d0
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-05-26 23:28:52 -0700

    fix: styling

--- src/lib/components/admin/Settings/Banners.svelte
+++ src/lib/components/admin/Settings/Banners.svelte
@@ -76,16 +76,17 @@
 					<div class=" flex justify-between">
 						<div class="flex flex-row flex-1 border rounded-xl dark:border-gray-800">
 							<select
-								class="w-fit capitalize rounded-xl py-2 px-4 text-xs bg-transparent outline-none text-gray-900"
+								class="w-fit capitalize rounded-xl py-2 px-4 text-xs bg-transparent outline-none"
 								bind:value={banner.type}
 							>
 								{#if banner.type == ''}
-									<option value="" selected disabled class="">{$i18n.t('Type')}</option>
+									<option value="" selected disabled class="text-gray-900">{$i18n.t('Type')}</option
+									>
 								{/if}
-								<option value="info">{$i18n.t('Info')}</option>
-								<option value="warning">{$i18n.t('Warning')}</option>
-								<option value="error">{$i18n.t('Error')}</option>
-								<option value="success">{$i18n.t('Success')}</option>
+								<option value="info" class="text-gray-900">{$i18n.t('Info')}</option>
+								<option value="warning" class="text-gray-900">{$i18n.t('Warning')}</option>
+								<option value="error" class="text-gray-900">{$i18n.t('Error')}</option>
+								<option value="success" class="text-gray-900">{$i18n.t('Success')}</option>
 							</select>
 
 							<input
