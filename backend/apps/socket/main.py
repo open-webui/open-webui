85484392b2a646dca66926f7181d5e98c5d0d59a
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-06-03 23:39:52 -0700

    feat: websocket

---
+++ backend/apps/socket/main.py
@@ -0,0 +1,47 @@
+import socketio
+
+from apps.webui.models.users import Users
+from utils.utils import decode_token
+
+sio = socketio.AsyncServer(cors_allowed_origins=[], async_mode="asgi")
+app = socketio.ASGIApp(sio, socketio_path="/ws/socket.io")
+
+# Dictionary to maintain the user pool
+USER_POOL = {}
+
+
+@sio.event
+async def connect(sid, environ, auth):
+    print("connect ", sid)
+
+    user = None
+    data = decode_token(auth["token"])
+
+    if data is not None and "id" in data:
+        user = Users.get_user_by_id(data["id"])
+
+    if user:
+        USER_POOL[sid] = {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email,
+            "role": user.role,
+        }
+        print(f"user {user.name}({user.id}) connected with session ID {sid}")
+    else:
+        print("Authentication failed. Disconnecting.")
+        await sio.disconnect(sid)
+
+
+@sio.event
+def disconnect(sid):
+    if sid in USER_POOL:
+        disconnected_user = USER_POOL.pop(sid)
+        print(f"user {disconnected_user} disconnected with session ID {sid}")
+    else:
+        print(f"Unknown session ID {sid} disconnected")
+
+
+@sio.event
+def disconnect(sid):
+    print("disconnect", sid)
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
+	import { io } from 'socket.io-client';
+
 	import { onMount, tick, setContext } from 'svelte';
-	import { config, user, theme, WEBUI_NAME, mobile } from '$lib/stores';
+	import { config, user, theme, WEBUI_NAME, mobile, socket } from '$lib/stores';
 	import { goto } from '$app/navigation';
 	import { Toaster, toast } from 'svelte-sonner';
 
@@ -12,7 +14,7 @@
 
 	import 'tippy.js/dist/tippy.css';
 
-	import { WEBUI_BASE_URL } from '$lib/constants';
+	import { WEBUI_BASE_URL, WEBUI_HOSTNAME } from '$lib/constants';
 	import i18n, { initI18n, getLanguages } from '$lib/i18n';
 
 	setContext('i18n', i18n);
@@ -55,10 +57,20 @@
 		if (backendConfig) {
 			// Save Backend Status to Store
 			await config.set(backendConfig);
-
 			await WEBUI_NAME.set(backendConfig.name);
 
 			if ($config) {
+				const _socket = io(`${WEBUI_BASE_URL}`, {
+					path: '/ws/socket.io',
+					auth: { token: localStorage.token }
+				});
+
+				_socket.on('connect', () => {
+					console.log('connected');
+				});
+
+				socket.set(_socket);
+
 				if (localStorage.token) {
 					// Get Session User Info
 					const sessionUser = await getSessionUser(localStorage.token).catch((error) => {
