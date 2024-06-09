d2a7411548a107dd28f959268509d65ef36b96b0
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2024-06-09 18:28:42 +0200

    quick fix

--- backend/apps/audio/providers/alltalk/alltalkService.py
+++ backend/apps/audio/providers/alltalk/alltalkService.py
@@ -2,7 +2,7 @@ from apps.audio.settings import get_config
 from apps.audio.providers.alltalk.alltalkApi import AllTalkTTSAPI
 
 config = get_config()
-baseUrl = config.ALLTALK_API_BASE_URL
+baseUrl = config.TTS_ALLTALK_API_BASE_URL
 
 # Initialize AllTalk TTS API instance
 tts = AllTalkTTSAPI(baseUrl)
@@ -13,12 +13,12 @@ def update_alltalk_base_url(base_url: str):
 def get_alltalk_config():
     return {
         "status": True,
-        "ALLTALK_API_BASE_URL": config.ALLTALK_API_BASE_URL,
-        "ALLTALK_API_MODEL": config.ALLTALK_API_MODEL,
-        "ALLTALK_API_VOICE": config.ALLTALK_API_VOICE,
-        "ALLTALK_API_DEEPSPEED": config.ALLTALK_API_DEEPSPEED,
-        "ALLTALK_API_LOW_VRAM": config.ALLTALK_API_LOW_VRAM,
-        "ALLTALK_API_USE_STREAMING": config.ALLTALK_API_USE_STREAMING,
-        "ALLTALK_API_USE_NARRATOR": config.ALLTALK_API_USE_NARRATOR,
-        "ALLTALK_API_NARRATOR_VOICE": config.ALLTALK_API_NARRATOR_VOICE
+        "ALLTALK_API_BASE_URL": config.TTS_ALLTALK_API_BASE_URL,
+        "ALLTALK_API_MODEL": config.TTS_ALLTALK_API_MODEL,
+        "ALLTALK_API_VOICE": config.TTS_ALLTALK_API_VOICE,
+        "ALLTALK_API_DEEPSPEED": config.TTS_ALLTALK_API_DEEPSPEED,
+        "ALLTALK_API_LOW_VRAM": config.TTS_ALLTALK_API_LOW_VRAM,
+        "ALLTALK_API_USE_STREAMING": config.TTS_ALLTALK_API_USE_STREAMING,
+        "ALLTALK_API_USE_NARRATOR": config.TTS_ALLTALK_API_USE_NARRATOR,
+        "ALLTALK_API_NARRATOR_VOICE": config.TTS_ALLTALK_API_NARRATOR_VOICE
     }
