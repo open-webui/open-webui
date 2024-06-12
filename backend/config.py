e130ad74d147a6871df9a9f043a16b89577b2f75
Mikael Turøy <mikael.turoy@holbergeeg.org>
2024-06-12 22:37:35 +0200

    Added timeout setting for ollama streaming response

--- backend/config.py
+++ backend/config.py
@@ -425,6 +425,7 @@ OLLAMA_API_BASE_URL = os.environ.get(
 )
 
 OLLAMA_BASE_URL = os.environ.get("OLLAMA_BASE_URL", "")
+OLLAMA_GENERATE_TIMEOUT = int(os.environ.get("OLLAMA_GENERATE_TIMEOUT", "900"))
 K8S_FLAG = os.environ.get("K8S_FLAG", "")
 USE_OLLAMA_DOCKER = os.environ.get("USE_OLLAMA_DOCKER", "false")
 
