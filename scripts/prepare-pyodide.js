a4748af822138de7a95f781b74066df0e0381bac
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-06-17 14:26:10 -0700

    chore: bump pyodide

--- scripts/prepare-pyodide.js
+++ scripts/prepare-pyodide.js
@@ -1,4 +1,6 @@
 const packages = [
+	'micropip',
+	'packaging',
 	'requests',
 	'beautifulsoup4',
 	'numpy',
@@ -11,13 +13,25 @@ const packages = [
 ];
 
 import { loadPyodide } from 'pyodide';
-import { writeFile, copyFile, readdir } from 'fs/promises';
+import { writeFile, readFile, copyFile, readdir, rmdir } from 'fs/promises';
 
 async function downloadPackages() {
 	console.log('Setting up pyodide + micropip');
 	const pyodide = await loadPyodide({
 		packageCacheDir: 'static/pyodide'
 	});
+
+	const packageJson = JSON.parse(await readFile('package.json'));
+	const pyodideVersion = packageJson.dependencies.pyodide;
+
+	const pyodidePackageJson = JSON.parse(await readFile('static/pyodide/package.json'));
+	const pyodidePackageVersion = pyodidePackageJson.version;
+
+	if (pyodideVersion.replace('^', '') !== pyodidePackageVersion) {
+		console.log('Pyodide version mismatch, removing static/pyodide directory');
+		await rmdir('static/pyodide', { recursive: true });
+	}
+
 	await pyodide.loadPackage('micropip');
 	const micropip = pyodide.pyimport('micropip');
 	console.log('Downloading Pyodide packages:', packages);
