7c304d225be5e22af2add04698c08f4cf4a34948
SimonOriginal <worcposj44@gmail.com>
2024-06-20 21:57:33 +0200

    fix key {.t('Created by')}

--- src/lib/components/chat/Settings/About.svelte
+++ src/lib/components/chat/Settings/About.svelte
@@ -132,9 +132,8 @@
 		<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
 			{#if !$WEBUI_NAME.includes('Open WebUI')}
 				<span class=" text-gray-500 dark:text-gray-300 font-medium">{$WEBUI_NAME}</span> -
-			{/if}{
-				$i18n.t('Created by')
-				}
+			{/if}
+				{$i18n.t('Created by')}
 			<a
 				class=" text-gray-500 dark:text-gray-300 font-medium"
 				href="https://github.com/tjbck"
