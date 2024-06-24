1f30b635810d09bb4e0d3dd6c9d90a8d22791805
Jannes Höke <mail@jhoeke.de>
2024-06-24 23:09:30 +0200

    fix: Add all results from queryMemory to userContext instead of only the first

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -529,12 +529,12 @@
 							});
 							if (res) {
 								if (res.documents[0].length > 0) {
-									userContext = res.documents.reduce((acc, doc, index) => {
-										const createdAtTimestamp = res.metadatas[index][0].created_at;
+									userContext = res.documents[0].reduce((acc, doc, index) => {
+										const createdAtTimestamp = res.metadatas[0][index].created_at;
 										const createdAtDate = new Date(createdAtTimestamp * 1000)
 											.toISOString()
 											.split('T')[0];
-										acc.push(`${index + 1}. [${createdAtDate}]. ${doc[0]}`);
+										acc.push(`${index + 1}. [${createdAtDate}]. ${doc}`);
 										return acc;
 									}, []);
 								}
