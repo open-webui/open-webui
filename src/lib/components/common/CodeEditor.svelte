9fb30d47874ec9e423599485869c9bbbe9a221d5
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-06-24 12:23:12 -0700

    fix

--- src/lib/components/common/CodeEditor.svelte
+++ src/lib/components/common/CodeEditor.svelte
@@ -10,11 +10,12 @@
 	import { python } from '@codemirror/lang-python';
 	import { oneDark } from '@codemirror/theme-one-dark';
 
-	import { onMount, createEventDispatcher } from 'svelte';
+	import { onMount, createEventDispatcher, getContext } from 'svelte';
 	import { formatPythonCode } from '$lib/apis/utils';
 	import { toast } from 'svelte-sonner';
 
 	const dispatch = createEventDispatcher();
+	const $i18n = getContext('i18n');
 
 	export let boilerplate = '';
 	export let value = '';
