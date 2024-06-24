465c3a99879d34526f1c70ad89af44652e783e85
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-06-24 13:11:49 -0700

    refac: playground styling

--- src/routes/(app)/playground/+page.svelte
+++ src/routes/(app)/playground/+page.svelte
@@ -1,15 +1,5 @@
 <script>
-	import { showSidebar } from '$lib/stores';
-
 	import Playground from '$lib/components/playground/Playground.svelte';
 </script>
 
-<div
-	class=" flex flex-col w-full min-h-screen max-h-screen {$showSidebar
-		? 'md:max-w-[calc(100%-260px)]'
-		: ''}"
->
-	<div class=" py-4 px-5 flex-1 max-h-full overflow-y-auto">
-		<Playground />
-	</div>
-</div>
+<Playground />
