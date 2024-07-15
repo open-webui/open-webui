9ab47cf628356df3024d08940d3067d61f2466a1
SimonOriginal <worcposj44@gmail.com>
2024-07-15 16:20:18 +0200

    refac 5

--- src/lib/components/workspace/common/ManifestModal.svelte
+++ src/lib/components/workspace/common/ManifestModal.svelte
@@ -63,9 +63,10 @@
 							<a 
 								href={manifest.funding_url} 
 								target="_blank" 
-								class="underline text-blue-400 hover:text-blue-300">{manifest.funding_url}</a
-							>
+								class="underline text-blue-400 hover:text-blue-300">{manifest.funding_url}
+							</a>
 						</div>
+
 					</div>
 
 					<div class="flex justify-end pt-3 text-sm font-medium">
