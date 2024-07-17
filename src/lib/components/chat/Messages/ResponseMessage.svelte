20895f67b86aec24c84fb5b5434741023395d56f
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-07-17 16:10:23 +0800

    Update ResponseMessage.svelte

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -116,8 +116,8 @@
 	}
 
 	function escapeBrackets(text) {
-		const pattern = /(```[\s\S]*?```|`.*?`)|\\\[([\s\S]*?[^\\])\\\]|\\\((.*?)\\\)/g;
-		return text.replace(pattern, (match, codeBlock, squareBracket, roundBracket) => {
+		const pattern = /(```[\S\s]*?```|`.*?`)|\\\[([\S\s]*?[^\\])\\]|\\\((.*?)\\\)/g;
+		return text.replaceAll(pattern, (match, codeBlock, squareBracket, roundBracket) => {
 			if (codeBlock) {
 				return codeBlock;
 			} else if (squareBracket) {
