306d40fab940105383401b41b927cb7bf2f68d76
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-07-17 15:32:18 +0800

    💄 fix LaTex

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -100,7 +100,44 @@
 		extensions: any;
 	};
 
+	function escapeDollarNumber(text: string) {
+		let escapedText = '';
+
+		for (let i = 0; i < text.length; i += 1) {
+			let char = text[i];
+			const nextChar = text[i + 1] || ' ';
+
+			if (char === '$' && nextChar >= '0' && nextChar <= '9') {
+				char = '\\$';
+			}
+
+			escapedText += char;
+		}
+
+		return escapedText;
+	}
+
+	function escapeBrackets(text: string) {
+		const pattern = /(```[\s\S]*?```|`.*?`)|\\\[([\s\S]*?[^\\])\\\]|\\\((.*?)\\\)/g;
+		return text.replace(pattern, (match, codeBlock, squareBracket, roundBracket) => {
+			if (codeBlock) {
+				return codeBlock;
+			} else if (squareBracket) {
+				return `$$${squareBracket}$$`;
+			} else if (roundBracket) {
+				return `$${roundBracket}$`;
+			}
+			return match;
+		});
+	}
+
+	function escapeMhchem(text: string) {
+		return text.replaceAll('$\\ce{', '$\\\\ce{').replaceAll('$\\pu{', '$\\\\pu{');
+	}
+
 	$: if (message) {
+		let processedContent = escapeMhchem(escapeBrackets(escapeDollarNumber(message.content)));
+		message.content = processedContent;
 		renderStyling();
 	}
 
