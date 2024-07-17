6476ffb7f2579219f703d44f241e6d7ac671a6a4
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-07-17 23:36:56 +0800

    fix

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -214,10 +214,13 @@
 					// • auto-render specific keys, e.g.:
 					delimiters: [
 						{ left: '$$', right: '$$', display: true },
-						{ left: '$', right: '$', display: false },
 						{ left: '\\(', right: '\\)', display: false },
-						{ left: '\\[', right: '\\]', display: true },
-						{ left: '[', right: ']', display: false }
+						{ left: '\\begin{equation}', right: '\\end{equation}', display: true },
+						{ left: '\\begin{align}', right: '\\end{align}', display: true },
+						{ left: '\\begin{alignat}', right: '\\end{alignat}', display: true },
+						{ left: '\\begin{gather}', right: '\\end{gather}', display: true },
+						{ left: '\\begin{CD}', right: '\\end{CD}', display: true },
+						{ left: '\\[', right: '\\]', display: true }
 					],
 					// • rendering keys, e.g.:
 					throwOnError: false
