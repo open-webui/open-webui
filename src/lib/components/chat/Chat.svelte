031bf2b098d0ba73931fe6f62fd438167499e9b0
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-07-19 19:26:42 +0800

    🤗 修改深色主题背景图片的不透明度为85%

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -1522,7 +1522,7 @@
 			/>
 
 			<div
-				class="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-white to-white/85 dark:from-gray-900 dark:to-[#171717]/90 z-0"
+				class="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-white to-white/85 dark:from-gray-900 dark:to-[#171717]/85 z-0"
 			/>
 		{/if}
 
