b34a7e08382831fe4453e4e865ab95bb5dbddc49
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-07-21 09:44:48 +0800

    更新样式
    Revert "Update CodeBlock.svelte"
    
    This reverts commit 2d7a9c2afeaefd9abfff489d48c6746a775cbfbc.
    
    Update CodeBlock.svelte

--- src/lib/components/chat/Messages/CodeBlock.svelte
+++ src/lib/components/chat/Messages/CodeBlock.svelte
@@ -325,7 +325,7 @@ __builtins__.input = input`);
 				</div>
 			</div>
 			<div style="display: {expanded ? 'block' : 'none'};">
-				<iframe bind:this={sandpackIframe} title="HTML Preview" class="w-full h-96 mt-4 bg-white" />
+				<iframe bind:this={sandpackIframe} title="HTML Preview" class="h-96 mt-4 bg-white" style="width: 100.5%;"/>
 			</div>
 		</div>
 	{/if}
