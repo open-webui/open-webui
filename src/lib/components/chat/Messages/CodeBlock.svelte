12ffe43b2b0dd2e30e5f1c6f7df09f4189cfa76a
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-07-21 11:22:37 +0800

    🤯 fix 样式

--- src/lib/components/chat/Messages/CodeBlock.svelte
+++ src/lib/components/chat/Messages/CodeBlock.svelte
@@ -328,13 +328,8 @@ __builtins__.input = input`);
 					>
 				</div>
 			</div>
-			<div style="display: {expanded ? 'block' : 'none'};">
-				<iframe
-					bind:this={sandpackIframe}
-					title="HTML Preview"
-					class="mt-4 bg-white"
-					style="width: 100.5%; height: 24.1rem;"
-				/>
+			<div style="display: {expanded ? 'flex' : 'none'};">
+				<iframe bind:this={sandpackIframe} title="HTML Preview" class="w-full h-96 mt-4 bg-white" />
 			</div>
 		</div>
 	{/if}
