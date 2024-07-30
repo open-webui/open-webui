cec90eb8aa89c21b65a77472863329749feca7a9
Raphaël <126982829+raphrapide100@users.noreply.github.com>
2024-07-30 15:43:22 +0300

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -77,13 +77,13 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "RaphAK")
+if WEBUI_NAME != "RaphAI":
+    WEBUI_NAME += " (RaphAI)"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = ""
 
 
 ####################################
