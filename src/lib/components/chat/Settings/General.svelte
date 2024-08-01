645856ea69fdb74fed7075b154037804a318af80
raestrada95 <raestrada95@gmail.com>
2024-08-01 00:03:52 -0600

    Remove translation help message for en-US from UI

--- src/lib/components/chat/Settings/General.svelte
+++ src/lib/components/chat/Settings/General.svelte
@@ -172,18 +172,6 @@
 					</select>
 				</div>
 			</div>
-			{#if $i18n.language === 'en-US'}
-				<div class="mb-2 text-xs text-gray-400 dark:text-gray-500">
-					Couldn't find your language?
-					<a
-						class=" text-gray-300 font-medium underline"
-						href="https://github.com/open-webui/open-webui/blob/main/docs/CONTRIBUTING.md#-translations-and-internationalization"
-						target="_blank"
-					>
-						Help us translate Open WebUI!
-					</a>
-				</div>
-			{/if}
 
 			<div>
 				<div class=" py-0.5 flex w-full justify-between">
