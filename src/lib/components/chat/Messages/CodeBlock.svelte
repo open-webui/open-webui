63678de12eaee6a7f4e0bad10e906856c6b6f915
raestrada95 <raestrada95@gmail.com>
2024-08-02 22:05:00 -0600

    Add basic code live preview with codesandbox

--- src/lib/components/chat/Messages/CodeBlock.svelte
+++ src/lib/components/chat/Messages/CodeBlock.svelte
@@ -2,13 +2,20 @@
 	import { copyToClipboard } from '$lib/utils';
 	import hljs from 'highlight.js';
 	import 'highlight.js/styles/github-dark.min.css';
-	import { onMount, tick } from 'svelte';
+	import { loadSandpackClient } from '@codesandbox/sandpack-client';
 
 	export let id = '';
+
 	export let lang = '';
 	export let code = '';
 
 	let highlightedCode = null;
+	let executing = false;
+
+	let stdout = null;
+	let stderr = null;
+	let result = null;
+
 	let copied = false;
 
 	const copyCode = async () => {
@@ -19,6 +26,73 @@
 			copied = false;
 		}, 1000);
 	};
+
+	const checkPythonCode = (str) => {
+		// Check if the string contains typical Python syntax characters
+		const pythonSyntax = [
+			'def ',
+			'else:',
+			'elif ',
+			'try:',
+			'except:',
+			'finally:',
+			'yield ',
+			'lambda ',
+			'assert ',
+			'nonlocal ',
+			'del ',
+			'True',
+			'False',
+			'None',
+			' and ',
+			' or ',
+			' not ',
+			' in ',
+			' is ',
+			' with '
+		];
+
+		for (let syntax of pythonSyntax) {
+			if (str.includes(syntax)) {
+				return true;
+			}
+		}
+
+		// If none of the above conditions met, it's probably not Python code
+		return false;
+	};
+
+	let sandpackIframe;
+	let sandpackClient;
+	const executeHTML = async (code) => {
+		// Initialize Sandpack client
+		const content = {
+			files: {
+				'/package.json': {
+					code: JSON.stringify({
+						main: 'index.html',
+						devDependencies: {}
+					})
+				},
+				'/index.html': { code }
+			},
+			environment: 'vanilla',
+			template: 'static'
+		};
+		if (sandpackClient) {
+			sandpackClient.updateSandbox(content);
+		} else {
+			sandpackClient = await loadSandpackClient(sandpackIframe, content, {
+				showOpenInCodeSandbox: true
+			});
+		}
+	};
+
+	$: if (lang.toLowerCase() == 'php' || lang.toLowerCase() == 'html') {
+		if (!!sandpackIframe) {
+			executeHTML(code);
+		}
+	}
 
 	let debounceTimeout;
 	$: if (code) {
@@ -49,11 +123,29 @@
 	</div>
 
 	<pre
-		class="hljs p-4 px-5 overflow-x-auto"
-		style="border-top-left-radius: 0px; border-top-right-radius: 0px;">
-		<code
+		class=" hljs p-4 px-5 overflow-x-auto"
+		style="border-top-left-radius: 0px; border-top-right-radius: 0px; {(executing ||
+			stdout ||
+			stderr ||
+			result) &&
+			'border-bottom-left-radius: 0px; border-bottom-right-radius: 0px;'}"><code
 			class="language-{lang} rounded-t-none whitespace-pre"
 			>{#if highlightedCode}{@html highlightedCode}{:else}{code}{/if}</code
 		>
 	</pre>
+
+	{#if lang.toLowerCase() == 'php' || lang.toLowerCase() == 'html'}
+		<div class="bg-[#202123] text-white px-4 py-4 rounded-b-lg">
+			<div class="text-gray-500 text-xs mb-1 flex justify-between items-center">
+				<span>HTML</span>
+				<button
+					class="copy-code-button bg-none border-none p-1"
+					on:click={() => {
+						executeHTML(code);
+					}}>Refresh</button
+				>
+			</div>
+			<iframe bind:this={sandpackIframe} title="HTML Preview" class="w-full h-96 mt-4 bg-white" />
+		</div>
+	{/if}
 </div>
