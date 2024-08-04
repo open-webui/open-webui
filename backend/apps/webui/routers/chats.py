73954f4a632f1bf2e10f710bd48c68fb935a2d99
Jun Siang Cheah <git@jscheah.me>
2024-08-04 16:00:52 +0100

    fix: admins viewing shared chats with ENABLE_ADMIN_CHAT_ACCESS set to false

--- backend/apps/webui/routers/chats.py
+++ backend/apps/webui/routers/chats.py
@@ -186,7 +186,7 @@ async def get_shared_chat_by_id(share_id: str, user=Depends(get_verified_user)):
             status_code=status.HTTP_401_UNAUTHORIZED, detail=ERROR_MESSAGES.NOT_FOUND
         )
 
-    if user.role == "user":
+    if user.role == "user" or (user.role == "admin" and not ENABLE_ADMIN_CHAT_ACCESS):
         chat = Chats.get_chat_by_share_id(share_id)
     elif user.role == "admin" and ENABLE_ADMIN_CHAT_ACCESS:
         chat = Chats.get_chat_by_id(share_id)
