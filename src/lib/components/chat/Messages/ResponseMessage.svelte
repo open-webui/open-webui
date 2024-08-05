db5830dc28e91d7e53357b86f90992129d80c06b
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-08-05 19:40:46 +0200

    refac: prose styling

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -413,7 +413,7 @@
 				{/if}
 
 				<div
-					class="prose chat-{message.role} w-full max-w-full dark:prose-invert prose-p:my-0 prose-img:my-1 whitespace-pre-line"
+					class="prose chat-{message.role} w-full max-w-full dark:prose-invert prose-p:my-0 prose-img:my-1 prose-headings:my-1.5 whitespace-pre-line"
 				>
 					<div>
 						{#if (message?.statusHistory ?? [...(message?.status ? [message?.status] : [])]).length > 0}
