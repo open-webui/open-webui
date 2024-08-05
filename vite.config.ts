9357d39fec7f050c384df98c63cce79864040772
Adam Warniment <adam.warniment@infotrustllc.com>
2024-08-05 17:56:51 -0400

    pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-
-// /** @type {import('vite').Plugin} */
-// const viteServerConfig = {
-// 	name: 'log-request-middleware',
-// 	configureServer(server) {
-// 		server.middlewares.use((req, res, next) => {
-// 			res.setHeader('Access-Control-Allow-Origin', '*');
-// 			res.setHeader('Access-Control-Allow-Methods', 'GET');
-// 			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-// 			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-// 			next();
-// 		});
-// 	}
-// };
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	define: {
-		APP_VERSION: JSON.stringify(process.env.npm_package_version),
-		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
-	},
-	build: {
-		sourcemap: true
-	},
-	worker: {
-		format: 'es'
-	}
+    plugins: [sveltekit()],
+    define: {
+        APP_VERSION: JSON.stringify(process.env.npm_package_version),
+        APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
+    },
+    build: {
+        sourcemap: true
+    },
+    worker: {
+        format: 'es'
+    },
+    server: {
+        fs: {
+            // Allow serving files from the /static directory
+            allow: [
+                // Adjust this path to match the actual location of your /static directory
+                path.resolve(__dirname, 'static')
+            ]
+        }
+    }
 });
