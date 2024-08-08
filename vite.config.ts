09c3e279900bde8ed1123522a1589a4878280b15
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-08-09 01:17:09 +0800

    Revert "Fix"
    This reverts commit ddcc2da11475d51fa84aa1c6a1c8936bedf419e2.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { terser } from 'rollup-plugin-terser';
 
 // /** @type {import('vite').Plugin} */
 // const viteServerConfig = {
@@ -23,21 +22,7 @@ export default defineConfig({
 		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
 	},
 	build: {
-		sourcemap: true,
-		minify: 'terser',
-		rollupOptions: {
-			plugins: [terser({
-				compress: {
-					drop_console: true, // 移除 console 语句
-					drop_debugger: true // 移除 debugger 语句
-				},
-				mangle: {
-					properties: {
-						regex: /^_/, // 混淆以 _ 开头的私有属性
-					},
-				}
-			})]
-		}
+		sourcemap: true
 	},
 	worker: {
 		format: 'es'
