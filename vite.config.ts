004b3d366a8ce5c5aca4e284027bcbbe0754b9dd
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-08-09 01:38:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,27 +18,13 @@ import terser from '@rollup/plugin-terser';
 
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), terser()],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
 	},
 	build: {
-		sourcemap: true,  // 如果不需要sourcemap，可以改为 false
-		minify: 'terser',
-		rollupOptions: {
-			plugins: [terser({
-				compress: {
-					drop_console: true, // 移除 console 语句
-					drop_debugger: true // 移除 debugger 语句
-				},
-				mangle: {
-					properties: {
-						regex: /^_/, // 混淆以 _ 开头的私有属性
-					},
-				}
-			})]
-		}
+		sourcemap: true
 	},
 	worker: {
 		format: 'es'
