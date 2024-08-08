0408dda9589ad94d1f05f40c5407b5c39ab3bffa
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-08-09 01:04:33 +0800

    Revert "Update vite.config.ts"
    This reverts commit 175fabb3865e6691577691e37a663e6ff0b4dafc.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import obfuscator from 'vite-plugin-obfuscator';
 
 // /** @type {import('vite').Plugin} */
 // const viteServerConfig = {
@@ -16,7 +17,24 @@ import { defineConfig } from 'vite';
 // };
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		// 集成 SvelteKit
+		sveltekit(),
+		// 集成 obfuscator 插件并配置混淆选项
+		obfuscator({
+			globals: ['window'], // 全局对象
+			compact: true, // 紧凑代码
+			controlFlowFlattening: true, // 启用控制流平坦化
+			controlFlowFlatteningThreshold: 0.75, // 控制流平坦化的应用比例
+			deadCodeInjection: true, // 启用死代码注入
+			deadCodeInjectionThreshold: 0.4, // 死代码注入的应用比例
+			debugProtection: true, // 启用调试保护
+			debugProtectionInterval: true, // 启用调试保护间隔
+			disableConsoleOutput: true, // 禁用控制台输出
+			stringArray: true, // 启用字符串数组
+			stringArrayThreshold: 0.75 // 字符串数组的应用比例
+		})
+	],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
