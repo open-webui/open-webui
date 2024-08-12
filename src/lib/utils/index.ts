6db027444d5efb6550ab5746030e6b8e5deec4dd
Yang Yang <153794571+yubb-ai@users.noreply.github.com>
2024-08-12 20:59:40 +0800

    Update index.ts

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -38,28 +38,28 @@ function escapeBrackets(text: string) {
 	});
 }
 
-// const convertLatexToSingleLine = (content: string) => {
-// 	// Patterns to match multiline LaTeX blocks
-// 	const patterns = [
-// 		/(\$\$\s[\s\S]*?\s\$\$)/g, // Match $$ ... $$
-// 		/(\\\[[\s\S]*?\\\])/g, // Match \[ ... \]
-// 		/(\\begin\{[a-z]+\}[\s\S]*?\\end\{[a-z]+\})/g // Match \begin{...} ... \end{...}
-// 	];
+const convertLatexToSingleLine = (content: string) => {
+ 	// Patterns to match multiline LaTeX blocks
+ const patterns = [
+  /(\$\$\s[\s\S]*?\s\$\$)/g, // Match $$ ... $$
+		/(\\\[[\s\S]*?\\\])/g, // Match \[ ... \]
+		/(\\begin\{[a-z]+\}[\s\S]*?\\end\{[a-z]+\})/g // Match \begin{...} ... \end{...}
+ ];
 
-// 	patterns.forEach((pattern) => {
-// 		content = content.replace(pattern, (match) => {
-// 			return match.replace(/\s*\n\s*/g, ' ').trim();
-// 		});
-// 	});
+ patterns.forEach((pattern) => {
+ 	content = content.replace(pattern, (match) => {
+ 		return match.replace(/\s*\n\s*/g, ' ').trim();
+ 	});
+ });
 
-// 	return content;
-// };
+ return content;
+};
 
 export const sanitizeResponseContent = (content: string) => {
 	// replace single backslash with double backslash
 	content = content.replace(/\\\\/g, '\\\\\\\\');
-	content = escapeBrackets(escapeDollarNumber(content));
-	// content = convertLatexToSingleLine(content);
+	// content = escapeBrackets(escapeDollarNumber(content));
+	content = convertLatexToSingleLine(content);
 
 	// First, temporarily replace valid <video> tags with a placeholder
 	const videoTagRegex = /<video\s+src="([^"]+)"\s+controls><\/video>/gi;
