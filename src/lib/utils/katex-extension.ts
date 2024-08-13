86ac0c15acda71d244f45a87e7c928a78291fec0
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-08-13 12:44:31 +0800

    Update katex-extension.ts

--- src/lib/utils/katex-extension.ts
+++ src/lib/utils/katex-extension.ts
@@ -2,8 +2,8 @@ import katex from 'katex';
 
 // const inlineRule = /^(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1(?=[\s?!\.,:？！。，：]|$)/;
 // const inlineRuleNonStandard = /^(\${1,2})(?!\$)((?:\\.|[^\\\n])*?(?:\\.|[^\\\n\$]))\1/; // Non-standard, even if there are no spaces before and after $ or $$, try to parse
-const inlineRule = /(\${1,2})([^$]+?)\1/;
-const inlineRuleNonStandard = /(\`{1,2})([^\`]+?)\1/;
+const inlineRule = /\$\$([\s\S]*?)\$\$|\$([\s\S]*?)\$/g;
+const inlineRuleNonStandard = /\$\$([\s\S]*?)\$\$|\$([\s\S]*?)\$/g;
 
 const blockRule = /^(\${1,2})\n((?:\\[^]|[^\\])+?)\n\1(?:\n|$)/;
 
@@ -27,14 +27,16 @@ function inlineKatex(options, renderer) {
     name: 'inlineKatex',
     level: 'inline',
     tokenizer(src) {
-      const match = src.match(ruleReg);
-      console.log('inline match:', match);
+      const match = ruleReg.exec(src);
       if (match) {
+        console.log('inline match:', match);
+        const isBlock = match[0].startsWith('$$');
+        const content = isBlock ? match[1] : match[2];
         return {
           type: 'inlineKatex',
           raw: match[0],
-          text: match[2].trim(),
-          displayMode: match[1].length === 2,
+          text: content.trim(),
+          displayMode: isBlock,
         };
       }
     },
@@ -49,11 +51,12 @@ function blockKatex(options, renderer) {
     tokenizer(src) {
       const match = src.match(blockRule);
       if (match) {
+        console.log('block match:', match);
         return {
           type: 'blockKatex',
           raw: match[0],
           text: match[2].trim(),
-          displayMode: match[1].length === 2,
+          displayMode: true,
         };
       }
     },
