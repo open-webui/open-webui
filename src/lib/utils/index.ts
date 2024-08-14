d2aca683e5b8d909b680cb0c2cdb1c90dd4764bd
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-08-15 00:09:33 +0800

    Update index.ts

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -6,42 +6,12 @@ import { WEBUI_BASE_URL } from '$lib/constants';
 // Helper functions
 //////////////////////////
 
-// function escapeDollarNumber(text: string) {
-// 	let escapedText = '';
-// 	for (let i = 0; i < text.length; i += 1) {
-// 		let char = text[i];
-// 		const nextChar = text[i + 1] || ' ';
-// 		if (char === '$' && nextChar >= '0' && nextChar <= '9') {
-// 			char = '\\$';
-// 		}
-// 		escapedText += char;
-// 	}
-// 	return escapedText;
-// }
-
-function escapeBrackets(text: string) {
-	let cleanSquareBracket = '';
-	let cleanRoundBracket = '';
-
-	const pattern = /(```[\s\S]*?```|`.*?`)|\\\[([\s\S]*?[^\\])\\\]|\\\((.*?)\\\)/g;
-	return text.replace(pattern, (match, codeBlock, squareBracket, roundBracket) => {
-		if (codeBlock) {
-			return codeBlock;
-		} else if (squareBracket) {
-			cleanSquareBracket = squareBracket.replace(/\s*\n\s*/g, ' ').trim();
-			return `$$ ${cleanSquareBracket} $$`;
-		} else if (roundBracket) {
-			cleanRoundBracket = roundBracket.replace(/\s*\n\s*/g, ' ').trim();
-			return `$ ${cleanRoundBracket} $`;
-		}
-		return match.replace(/\s*\n\s*/g, ' ').trim();
-	});
-}
-
-const convertLatexToSingleLine = (content: string) => {
+const convertLatexToSingleLine = (content) => {
 	// Patterns to match multiline LaTeX blocks
 	const patterns = [
 		/(\$\$\s[\s\S]*?\s\$\$)/g, // Match $$ ... $$
+		/(\\\[[\s\S]*?\\\])/g, // Match \[ ... \]
+		/(\\begin\{[a-z]+\}[\s\S]*?\\end\{[a-z]+\})/g // Match \begin{...} ... \end{...}
 	];
 
 	patterns.forEach((pattern) => {
@@ -56,7 +26,8 @@ const convertLatexToSingleLine = (content: string) => {
 export const sanitizeResponseContent = (content: string) => {
 	// replace single backslash with double backslash
 	content = content.replace(/\\\\/g, '\\\\\\\\');
-	content = convertLatexToSingleLine(escapeBrackets(content));
+
+	content = convertLatexToSingleLine(content);
 
 	// First, temporarily replace valid <video> tags with a placeholder
 	const videoTagRegex = /<video\s+src="([^"]+)"\s+controls><\/video>/gi;
@@ -73,8 +44,8 @@ export const sanitizeResponseContent = (content: string) => {
 		.replace(/<\|[a-z]+\|$/, '')
 		.replace(/<$/, '')
 		.replaceAll(/<\|[a-z]+\|>/g, ' ')
-		// .replaceAll('<', '&lt;')
-		// .replaceAll('>', '&gt;')
+		.replaceAll('<', '&lt;')
+		.replaceAll('>', '&gt;')
 		.trim();
 
 	// Replace placeholders with original <video> tags
