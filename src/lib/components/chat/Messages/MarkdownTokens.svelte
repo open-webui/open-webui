33a99817da98271016c826eeb290036856eccf1c
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-08-15 11:39:53 +0800

    fix

--- src/lib/components/chat/Messages/MarkdownTokens.svelte
+++ src/lib/components/chat/Messages/MarkdownTokens.svelte
@@ -123,6 +123,13 @@
 				displayMode={token?.displayMode ?? false}
 			/>
 		{/if}
+	{:else if token.type === 'blockKatex'}
+		{#if token.text}
+			<KatexRenderer
+				content={revertSanitizedResponseContent(token.text)}
+				displayMode={token?.displayMode ?? false}
+			/>
+		{/if}
 	{:else if token.type === 'space'}
 		{''}
 	{:else}
