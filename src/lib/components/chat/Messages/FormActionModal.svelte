52dc5da930d9f0ad9bb6d72b4d2e5bb0ab383f04
Yewang Xie <yewangxie@outlook.com>
2024-08-15 17:15:30 +0400

    update: response msg after leave request & bg color

--- src/lib/components/chat/Messages/FormActionModal.svelte
+++ src/lib/components/chat/Messages/FormActionModal.svelte
@@ -48,15 +48,30 @@
 	}
 
 	const dateFormatter = (val) => {
-		// const date = val ? new Date(val) : new Date();
-		// const day = String(date.getDate()).padStart(2, '0'); // 获取日，并确保两位数格式
-		// const month = String(date.getMonth() + 1).padStart(2, '0'); // 获取月，并确保两位数格式（月份从0开始，所以需要+1）
-		// const year = String(date.getFullYear()).slice(-2); // 获取年的最后两位
-		// return `${day}-${month}-${year}`;
 		const date = val ? new Date(val) : new Date()
-		const dateString = date.toDateString().split(' ')
-		dateString.splice(0,1)
-		return `${dateString[1]} ${dateString[0]} ${dateString[2]}`
+		// const dateString = date.toDateString().split(' ')
+		// dateString.splice(0,1)
+		// return `${dateString[1]} ${dateString[0]} ${dateString[2]}`
+		const months = [
+			"January", "February", "March", "April", "May", "June",
+			"July", "August", "September", "October", "November", "December"
+		];
+		
+		const day = date.getDate();
+		const month = months[date.getMonth()];
+		const year = date.getFullYear();
+
+		// 添加后缀
+		let suffix = "th";
+		if (day % 10 === 1 && day !== 11) {
+			suffix = "st";
+		} else if (day % 10 === 2 && day !== 12) {
+			suffix = "nd";
+		} else if (day % 10 === 3 && day !== 13) {
+			suffix = "rd";
+		}
+
+		return `${month} ${day}${suffix}, ${year}`;
 	};
 	const handleConfirm = () => {
 		if (!$user.extra_sso) {
