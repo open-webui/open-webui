f36569961d0bb9b73d14836007f0d14e13470553
Ruben Kaiser <ruben@kaiser.fyi>
2024-08-15 13:15:01 +0200

    feat(backend/dev.sh): Add Python version print and display

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,2 +1,6 @@
+echo "Python version:"
+python --version
+
+echo "Starting backend dev server..."
 PORT="${PORT:-8080}"
 uvicorn main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
