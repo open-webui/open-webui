d224566957f836dab5262152b3eb2796a179d001
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-08-16 00:21:57 +0200

    refac: styling

--- src/lib/components/chat/Messages/UserMessage.svelte
+++ src/lib/components/chat/Messages/UserMessage.svelte
@@ -165,13 +165,13 @@
 				</div>
 			{:else}
 				<div class="w-full">
-					<div class="flex {($settings?.chatBubble ?? true) ? 'justify-end' : ''} pb-2">
+					<div class="flex {($settings?.chatBubble ?? true) ? 'justify-end pb-1' : 'w-full'}">
 						<div
 							class="rounded-3xl {($settings?.chatBubble ?? true)
 								? `max-w-[90%] px-5 py-2  bg-gray-50 dark:bg-gray-850 ${
 										message.files ? 'rounded-tr-lg' : ''
 									}`
-								: ''}"
+								: ' w-full'}"
 						>
 							{#if message.content}
 								<div class="">
