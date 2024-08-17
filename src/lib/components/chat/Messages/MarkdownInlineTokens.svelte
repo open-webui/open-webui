c1823b4b73517ad276572ed607db09490142d182
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-08-17 16:02:46 +0200

    refac

--- src/lib/components/chat/Messages/MarkdownInlineTokens.svelte
+++ src/lib/components/chat/Messages/MarkdownInlineTokens.svelte
@@ -36,7 +36,7 @@
 			<svelte:self id={`${id}-em`} tokens={token.tokens} />
 		</em>
 	{:else if token.type === 'codespan'}
-		<code class="codespan">{token.text}</code>
+		<code class="codespan">{unescapeHtml(token.text)}</code>
 	{:else if token.type === 'br'}
 		<br />
 	{:else if token.type === 'del'}
