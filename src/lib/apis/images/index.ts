efcb56f0dc71caf2333dad4f13135e0f0390061e
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-08-21 17:27:39 +0200

    refac

--- src/lib/apis/images/index.ts
+++ src/lib/apis/images/index.ts
@@ -67,6 +67,38 @@ export const updateConfig = async (token: string = '', config: object) => {
 	return res;
 };
 
+export const verifyConfigUrl = async (token: string = '') => {
+	let error = null;
+
+	const res = await fetch(`${IMAGES_API_BASE_URL}/config/url/verify`, {
+		method: 'GET',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			...(token && { authorization: `Bearer ${token}` })
+		}
+	})
+		.then(async (res) => {
+			if (!res.ok) throw await res.json();
+			return res.json();
+		})
+		.catch((err) => {
+			console.log(err);
+			if ('detail' in err) {
+				error = err.detail;
+			} else {
+				error = 'Server connection failed';
+			}
+			return null;
+		});
+
+	if (error) {
+		throw error;
+	}
+
+	return res;
+};
+
 export const getImageGenerationConfig = async (token: string = '') => {
 	let error = null;
 
