a02b5f00c3563b64606954304d7a0abc751cdb6c
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-08-22 14:24:49 +0200

    fix: latex rendering issue

--- src/lib/components/chat/Messages/Markdown/KatexRenderer.svelte
+++ src/lib/components/chat/Messages/Markdown/KatexRenderer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import katex from 'katex';
 	import 'katex/contrib/mhchem';
+	import 'katex/dist/katex.min.css';
 
 	export let content: string;
 	export let displayMode: boolean = false;
