553293f4d5eb36f6fb7ad33e68be1c0d2e4a48d1
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-08-23 16:45:07 +0200

    refac: styling

--- src/lib/components/chat/MessageInput/CallOverlay.svelte
+++ src/lib/components/chat/MessageInput/CallOverlay.svelte
@@ -778,8 +778,8 @@
 								: rmsLevel * 100 > 2
 									? 'size-48'
 									: rmsLevel * 100 > 1
-										? 'size-[11.5rem]'
-										: 'size-44'}  transition-all rounded-full {(model?.info?.meta
+										? 'size-44'
+										: 'size-40'}  transition-all rounded-full {(model?.info?.meta
 								?.profile_image_url ?? '/static/favicon.png') !== '/static/favicon.png'
 								? ' bg-cover bg-center bg-no-repeat'
 								: 'bg-black dark:bg-white'} "
