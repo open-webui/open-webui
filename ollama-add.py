a7216e4c9aa11698efc9d0d5824986e3a36da0b6
modelearth <model@dreamstudio.com>
2024-08-26 02:41:34 -0400

    Remove comments to add back ollama for sync

--- ollama-add.py
+++ ollama-add.py
@@ -1,6 +1,7 @@
 # ollama-add.py
 
 import importlib
+import re
 
 ollama_common = importlib.import_module("ollama-common")
 phrases = ollama_common.phrases
@@ -8,7 +9,7 @@ phrases = ollama_common.phrases
 def remove_initial_hash_from_file(phrases):
     for filename, phrase_list in phrases.items():
         # Print the filename to the terminal
-        print(f"Processing file: {filename}")
+        print(f"\rProcessing file: {filename}")
 
         # Read lines from the file
         with open(filename, 'r') as file:
@@ -17,12 +18,14 @@ def remove_initial_hash_from_file(phrases):
         # Process the lines to remove initial '#' where necessary
         updated_lines = []
         for line in lines:
-            stripped_line = line.lstrip('#').strip()  # Remove initial '#' and then any leading spaces
-            #print(f"{stripped_line}")
+            stripped_line = re.sub(r'^\s*#', '', line, 1).rstrip() # Remove initial spaces and '#' and then any trailing spaces
+            print(f"1{stripped_line}2")
             for phrase in phrase_list:
+                print(f"3{phrase}4")
                 if stripped_line == phrase:
-                    if line.startswith('#'):
-                        updated_lines.append(line[1:])  # Remove the initial '#' and retain spaces
+                    print(f"Found {phrase}")
+                    if re.match(r'^\s*#', line): # If # is either the first character in the line or is preceded only by whitespace
+                        updated_lines.append(line.replace('#', '', 1))  # Remove the initial '#' and retain spaces
                     else:
                         updated_lines.append(line)
                     break
--- ollama-common.py
+++ ollama-common.py
@@ -7,5 +7,30 @@ phrases = {
         '# Expose Ollama API outside the container stack',
         'ports:',
         '- ${OLLAMA_WEBAPI_PORT-11434}:11434'
+    ],
+    "docker-compose.yaml": [
+        'ollama:',
+        'volumes:',
+        '- ollama:/root/.ollama',
+        'container_name: ollama',
+        'pull_policy: always',
+        'tty: true',
+        'restart: unless-stopped',
+        'image: ollama/ollama:${OLLAMA_DOCKER_TAG-latest}',
+
+        'args:',
+        "OLLAMA_BASE_URL: '/ollama'",
+
+        "depends_on:",
+        "  - ollama",
+
+        "environment:",
+        "  - 'OLLAMA_BASE_URL=http://ollama:11434'",
+        "  - 'WEBUI_SECRET_KEY='",
+
+        "ollama: {}"
     ]
 }
+
+# 
+# SKIP_OLLAMA_TESTS: 'true'
