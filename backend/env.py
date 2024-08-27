33a41c4dd66cca9ea5c2dbec827ddd9adf4b84fb
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-08-28 01:59:37 +0800

    Update env.py

--- backend/env.py
+++ backend/env.py
@@ -76,12 +76,10 @@ log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL =  os.environ.get("WEBUI_FAVICON_URL", f"{WEBUI_URL}/favicon.png")
 
 
 ####################################
