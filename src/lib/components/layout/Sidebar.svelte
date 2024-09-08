83a3e53d8d24d82829a89d2274b9290a6b9f8b60
Sebastian <sebastian.danielsson@proton.me>
2024-09-08 17:36:51 +0200

    Add padding to compensate for iPhone nav bar

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -551,7 +551,7 @@
 			</div>
 		</div>
 
-		<div class="px-2.5">
+		<div class="px-2.5 pb-safe-bottom">
 			<!-- <hr class=" border-gray-900 mb-1 w-full" /> -->
 
 			<div class="flex flex-col font-primary">
