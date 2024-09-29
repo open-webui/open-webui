cf6239112bd7c6ce0dc705491ef57453ba539d33
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-09-30 01:01:39 +0200

    refac

--- src/lib/components/chat/Messages/Error.svelte
+++ src/lib/components/chat/Messages/Error.svelte
@@ -4,12 +4,12 @@
 	export let content = '';
 </script>
 
-<div class="flex my-2 gap-2.5 border px-4 py-3 border-red-800 bg-red-800/30 font-medium rounded-lg">
+<div class="flex my-2 gap-2.5 border px-4 py-3 border-red-800 bg-red-800/30 rounded-lg">
 	<div class=" self-start mt-0.5">
 		<Info className="size-5" />
 	</div>
 
 	<div class=" self-center text-sm">
-		{content}
+		{typeof content === 'string' ? content : JSON.stringify(content)}
 	</div>
 </div>
