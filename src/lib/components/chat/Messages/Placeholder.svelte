12a621647793b3c52645ff69df6c4ded86df9aa0
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-05 10:23:47 -0700

    refac: styling

--- src/lib/components/chat/Messages/Placeholder.svelte
+++ src/lib/components/chat/Messages/Placeholder.svelte
@@ -198,7 +198,7 @@
 			</div>
 		</div>
 		<div class="mx-auto max-w-2xl font-primary" in:fade={{ duration: 200, delay: 200 }}>
-			<div class="mx-4">
+			<div class="mx-5">
 				<Suggestions
 					suggestionPrompts={models[selectedModelIdx]?.info?.meta?.suggestion_prompts ??
 						$config?.default_prompt_suggestions ??
