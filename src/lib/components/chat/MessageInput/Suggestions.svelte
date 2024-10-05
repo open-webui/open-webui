f5004fd9d4782069ecf3a6411843eeb7b32de148
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-05 13:26:52 -0700

    refac: styling

--- src/lib/components/chat/MessageInput/Suggestions.svelte
+++ src/lib/components/chat/MessageInput/Suggestions.svelte
@@ -15,7 +15,7 @@
 </script>
 
 {#if prompts.length > 0}
-	<div class="mb-2 flex gap-1 text-sm font-medium items-center text-gray-400 dark:text-gray-600">
+	<div class="mb-1 flex gap-1 text-sm font-medium items-center text-gray-400 dark:text-gray-600">
 		<Bolt />
 		{$i18n.t('Suggested')}
 	</div>
