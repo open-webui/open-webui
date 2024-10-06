7b6723a9553d241f512a870e69779251a7aab4ec
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-06 16:22:19 -0700

    refac: styling

--- src/lib/components/chat/Artifacts.svelte
+++ src/lib/components/chat/Artifacts.svelte
@@ -32,7 +32,7 @@
 	const getContents = () => {
 		contents = [];
 		messages.forEach((message) => {
-			if (message?.content) {
+			if (message?.role !== 'user' && message?.content) {
 				const codeBlockContents = message.content.match(/```[\s\S]*?```/g);
 				let codeBlocks = [];
 
