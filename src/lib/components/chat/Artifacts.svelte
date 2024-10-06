539e95a2068faa40384905cf1f2383cb4cbdbbad
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-06 00:00:39 -0700

    refac

--- src/lib/components/chat/Artifacts.svelte
+++ src/lib/components/chat/Artifacts.svelte
@@ -57,12 +57,12 @@
                         <head>
                             <meta charset="UTF-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            ${cssContent}
 							<${''}style>
 								body {
 									background-color: white; /* Ensure the iframe has a white background */
 								}
 							</${''}style>
+							${cssContent}
                         </head>
                         <body>
                             ${htmlContent}
