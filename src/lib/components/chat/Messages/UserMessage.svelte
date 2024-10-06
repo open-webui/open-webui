65526732646b23c143b78fb80086fd1e81040e52
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-06 16:22:19 -0700

    refac: styling

--- src/lib/components/chat/Messages/UserMessage.svelte
+++ src/lib/components/chat/Messages/UserMessage.svelte
@@ -94,7 +94,7 @@
 				: (user?.profile_image_url ?? '/user.png')}
 		/>
 	{/if}
-	<div class="w-full overflow-hidden pl-1">
+	<div class="w-full w-0 pl-1">
 		{#if !($settings?.chatBubble ?? true)}
 			<div>
 				<Name>
