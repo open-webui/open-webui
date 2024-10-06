3fcdca2f283378f37d55237df5ea112eaa0e0e99
Peter De-Ath <peter@de-ath.co.uk>
2024-10-06 23:03:34 +0100

    backwards comp: use RAG_EMBEDDING_OPENAI_BATCH_SIZE

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -989,7 +989,7 @@ RAG_EMBEDDING_MODEL_TRUST_REMOTE_CODE = (
 RAG_EMBEDDING_BATCH_SIZE = PersistentConfig(
     "RAG_EMBEDDING_BATCH_SIZE",
     "rag.embedding_batch_size",
-    int(os.environ.get("RAG_EMBEDDING_BATCH_SIZE", "1")),
+    int(os.environ.get("RAG_EMBEDDING_BATCH_SIZE") or os.environ.get("RAG_EMBEDDING_OPENAI_BATCH_SIZE", "1"))
 )
 
 RAG_RERANKING_MODEL = PersistentConfig(
