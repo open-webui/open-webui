a7aa66903878f3382d71c3d22cbfae1f83e502c0
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-21 21:32:45 -0700

    refac: styling

--- src/lib/components/common/Collapsible.svelte
+++ src/lib/components/common/Collapsible.svelte
@@ -12,7 +12,8 @@
 
 	export let open = false;
 	export let className = '';
-	export let buttonClassName = 'w-fit';
+	export let buttonClassName =
+		'w-fit text-gray-500 hover:text-gray-700 dark:hover:text-gray-300 transition';
 	export let title = null;
 
 	export let grow = false;
@@ -33,10 +34,8 @@
 				}
 			}}
 		>
-			<div
-				class=" w-fit font-medium flex items-center justify-between gap-2 text-gray-500 hover:text-gray-700 dark:hover:text-gray-300 transition"
-			>
-				<div class=" ">
+			<div class=" w-full font-medium flex items-center justify-between gap-2">
+				<div class="">
 					{title}
 				</div>
 
