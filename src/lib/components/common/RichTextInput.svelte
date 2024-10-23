0a765744b8d1467e09a51145054453743ae0c75f
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-10-22 18:12:08 -0700

    refac

--- src/lib/components/common/RichTextInput.svelte
+++ src/lib/components/common/RichTextInput.svelte
@@ -386,8 +386,6 @@
 
 				value = serializeEditorContent(newState.doc); // Convert ProseMirror content to markdown text
 				eventDispatch('input', { value });
-
-				console.log('Editor content:', value);
 			},
 			handleDOMEvents: {
 				focus: (view, event) => {
