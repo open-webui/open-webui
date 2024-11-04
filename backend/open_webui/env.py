84efc84ab2ffb0e7de8290aa7032825b7773abb0
gowshik450526511 <gowshik511.123@gmail.com>
2024-11-05 01:06:36 +0530

    Added favicon_url and title

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -98,14 +98,11 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = "Unbound AI Chat"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "static/favicon.png"
 
 
 ####################################
