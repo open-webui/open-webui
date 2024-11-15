bf2087c67bd2854b40593e6cf925281da6dd85c8
Timothy Jaeryang Baek <tim@openwebui.com>
2024-11-15 02:06:02 -0800

    refac

--- src/lib/components/admin/Settings/Connections/ManageOllamaModal.svelte
+++ src/lib/components/admin/Settings/Connections/ManageOllamaModal.svelte
@@ -528,7 +528,7 @@
 	<div>
 		<div class=" flex justify-between dark:text-gray-100 px-5 pt-4 pb-2">
 			<div class=" text-lg font-medium self-center font-primary">
-				{$i18n.t('Manage Ollama')} ({urlIdx})
+				{$i18n.t('Manage Ollama')}
 			</div>
 			<button
 				class="self-center"
