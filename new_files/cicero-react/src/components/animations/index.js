afd529df98bb3831a4d86a77e2a3e736c24df272
arthrod <89408329+arthrod@users.noreply.github.com>
2024-11-25 18:03:13 -0500

    Delete new_files directory

--- new_files/cicero-react/src/components/animations/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LightThemeDots } from './LightThemeDots';
-export { default as DarkThemeDots } from './DarkThemeDots';
-export { default as DotsBackground } from './DotsBackground';
