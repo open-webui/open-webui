de84e641935722f9a02f54745a2db870d7b69294
simonanthony79 <145207668+simonanthony79@users.noreply.github.com>
2024-12-21 17:44:48 +0100

    changes

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -99,9 +99,9 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "Portal")
+if WEBUI_NAME != "Portal":
+    WEBUI_NAME += " (Portal)"
 
 WEBUI_URL = os.environ.get("WEBUI_URL", "http://localhost:3000")
 
