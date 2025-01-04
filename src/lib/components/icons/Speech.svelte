9a541945c81bce5d4238c89fc1d201c953b806ce
veggiecode <alexisao@hotmail.com>
2025-01-04 15:02:39 -0600

    Add Speech icon component
    Introduce a new `Speech` icon component with an accompanying SVG asset. This component allows for customizable class names and stroke widths to adapt to various UI needs. It is designed to enhance visual representation and maintain consistency within the icon library.

---
+++ src/lib/components/icons/Speech.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let className = 'w-4 h-4';
+	export let strokeWidth = '0';
+</script>
+
+<svg
+	aria-hidden="true"
+	xmlns="http://www.w3.org/2000/svg"
+	fill="currentColor"
+	viewBox="0 0 24 24"
+	stroke-width={strokeWidth}
+	stroke="currentColor"
+	class={className}>
+
+	<svg>
+		<g>
+			<title>Layer 1</title>
+			<path id="svg_1" fill-rule="evenodd" fill="currentColor" d="m7.39241,6c-0.82843,0 -1.5,0.67157 -1.5,1.5l0,9c0,0.8284 0.67157,1.5 1.5,1.5c0.82843,0 1.5,-0.6716 1.5,-1.5l0,-9c0,-0.82843 -0.67157,-1.5 -1.5,-1.5z"/>
+			<path id="svg_2" fill-rule="evenodd" fill="currentColor" d="m3.74051,8.49367c-0.82843,0 -1.5,0.67157 -1.5,1.5l0,3c0,0.8284 0.67157,1.5 1.5,1.5c0.82843,0 1.5,-0.6716 1.5,-1.5l0,-3c0,-0.82843 -0.67157,-1.5 -1.5,-1.5z"/>
+			<path id="svg_3" fill-rule="evenodd" fill="currentColor" d="m11.52532,4c-0.82843,0 -1.5,0.67157 -1.5,1.5l0,13c0,0.8284 0.67157,1.5 1.5,1.5c0.8284,0 1.5,-0.6716 1.5,-1.5l0,-13c0,-0.82843 -0.6716,-1.5 -1.5,-1.5z"/>
+			<path id="svg_4" fill-rule="evenodd" fill="currentColor" d="m14.39241,8.5c0,-0.82843 0.6716,-1.5 1.5,-1.5c0.8284,0 1.5,0.67157 1.5,1.5l0,7c0,0.8284 -0.6716,1.5 -1.5,1.5c-0.8284,0 -1.5,-0.6716 -1.5,-1.5l0,-7z"/>
+			<path id="svg_6" fill-rule="evenodd" fill="currentColor" d="m20.25949,9c-0.8284,0 -1.5,0.67157 -1.5,1.5l0,3c0,0.8284 0.6716,1.5 1.5,1.5c0.8284,0 1.5,-0.6716 1.5,-1.5l0,-3c0,-0.82843 -0.6716,-1.5 -1.5,-1.5z"/>
+		</g>
+	</svg>
+</svg>
