490b4a1753be839b84fad40cd8e4a6380c650b59
supermyriam <myriam.sanchez@sintergica.ai>
2025-01-08 15:21:20 -0600

    Enhance input text visibility in light mode.
    Added `text-gray-600` to input fields to improve text readability in light mode while maintaining compatibility with dark mode styles. This ensures better user experience across different themes.

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -244,7 +244,7 @@
 								<input
 									bind:value={email}
 									type="email"
-									class="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200"
+									class="w-full px-3 py-2 border text-gray-600 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200"
 									required
 									placeholder={$i18n.t('user@example.com')}
 								/>
@@ -257,7 +257,7 @@
 							<input
 								bind:value={password}
 								type="password"
-								class="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200"
+								class="w-full px-3 py-2 border text-gray-600 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200"
 								required
 								placeholder={$i18n.t('Enter you password')}
 							/>
