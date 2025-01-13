58273afb169eb72d99326bc75401328eabef4ba9
KARAN_GOYAL <karan.goyal@dell.com>
2025-01-13 16:27:37 -0500

    stuff to run the app

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,2 +1,3 @@
 PORT="${PORT:-8080}"
-uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
+# ../venv/bin/python -m uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
+../venv/bin/python -m debugpy --listen 5678 -m uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
