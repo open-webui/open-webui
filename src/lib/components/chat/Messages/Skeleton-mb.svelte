a340e82ca286f1673a4797e40d211edd66d147e1
shadowfax <theshadowfaxproject@gmail.com>
2025-01-12 21:02:53 -0800

    open-webui-mb-5 Change AI-response-wait animation from default to something else.

--- src/lib/components/chat/Messages/Skeleton-mb.svelte
+++ src/lib/components/chat/Messages/Skeleton-mb.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 	import { WEBUI_BASE_URL } from '$lib/constants';
+	const metaThemeColorTag = document.querySelector('meta[name="theme-color"]');
+	let SRC = `${WEBUI_BASE_URL}/static/chat-waiting-mb-dark.gif`;
+	if (metaThemeColorTag?.content) {
+		if (metaThemeColorTag.content === '#ffffff') {
+			SRC = `${WEBUI_BASE_URL}/static/chat-waiting-mb-light.gif`;
+		}
+	}
 </script>
 
 <img
 	crossorigin="anonymous"
-	src="{WEBUI_BASE_URL}/static/chat-waiting-mb-10k.gif"
+	src="{SRC}"
 	class="w-20 h-10"
 	alt="logo"
 />
