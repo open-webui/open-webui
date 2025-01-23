412923dc915065fd264228ad18db7991db88b7dd
Antti Pyykkönen <antti.pyykkonen@kone.com>
2025-01-23 16:16:50 +0200

    feat: separate cookie settings between session & auth cookies
    Introducing two new env config options to control cookies settings regarding
    authentication. These values are taken into use when setting 'token' and 'oauth_id_token'.
    To maintain backwards compatibility, the original session cookie values are used as
    fallback.
    
    Separation is done to prevent issues with the session cookie. When the config value was
    set as 'strict', the oauth flow was broken (since the session cookie was not provided
    after the callback).
    
    Providing a separate config for auth & session cookies allows us to keep the 'strict'
    settings for auth related cookies, while also allowing the session cookie to behave as
    intended (e.g., by configuring it as 'lax').
    
    The original config was added in commit #af4f8aa. However a later commit #a2e889c reused
    this config option for other type of cookies, which was not the original intent.

--- backend/open_webui/utils/oauth.py
+++ backend/open_webui/utils/oauth.py
@@ -35,7 +35,7 @@ from open_webui.config import (
     AppConfig,
 )
 from open_webui.constants import ERROR_MESSAGES, WEBHOOK_MESSAGES
-from open_webui.env import WEBUI_SESSION_COOKIE_SAME_SITE, WEBUI_SESSION_COOKIE_SECURE
+from open_webui.env import WEBUI_AUTH_COOKIE_SAME_SITE, WEBUI_AUTH_COOKIE_SECURE
 from open_webui.utils.misc import parse_duration
 from open_webui.utils.auth import get_password_hash, create_token
 from open_webui.utils.webhook import post_webhook
@@ -323,8 +323,8 @@ class OAuthManager:
             key="token",
             value=jwt_token,
             httponly=True,  # Ensures the cookie is not accessible via JavaScript
-            samesite=WEBUI_SESSION_COOKIE_SAME_SITE,
-            secure=WEBUI_SESSION_COOKIE_SECURE,
+            samesite=WEBUI_AUTH_COOKIE_SAME_SITE,
+            secure=WEBUI_AUTH_COOKIE_SECURE,
         )
 
         if ENABLE_OAUTH_SIGNUP.value:
@@ -333,8 +333,8 @@ class OAuthManager:
                 key="oauth_id_token",
                 value=oauth_id_token,
                 httponly=True,
-                samesite=WEBUI_SESSION_COOKIE_SAME_SITE,
-                secure=WEBUI_SESSION_COOKIE_SECURE,
+                samesite=WEBUI_AUTH_COOKIE_SAME_SITE,
+                secure=WEBUI_AUTH_COOKIE_SECURE,
             )
         # Redirect back to the frontend with the JWT token
         redirect_url = f"{request.base_url}auth#token={jwt_token}"
