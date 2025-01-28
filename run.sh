eb515dfaec24924041c7557905d9441777b07175
Bruno Silva Sette <bruno.sette@softplan.com.br>
2025-01-28 15:20:22 -0300

    :construction: Change container image name

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-image_name="open-webui"
-container_name="open-webui"
+image_name="open-webui-iara"
+container_name="open-webui-iara"
 host_port=3000
 container_port=8080
 
@@ -9,7 +9,7 @@ docker build -t "$image_name" .
 docker stop "$container_name" &>/dev/null || true
 docker rm "$container_name" &>/dev/null || true
 
-docker run -d -p "$host_port":"$container_port" \
+docker run -p "$host_port":"$container_port" \
     --add-host=host.docker.internal:host-gateway \
     -v "${image_name}:/app/backend/data" \
     --name "$container_name" \
