12a7f4d43b107ab4f09c27f76eef3dc601833b85
Timothy Jaeryang Baek <tim@openwebui.com>
2025-02-04 01:03:41 -0800

    refac: styling

--- src/lib/components/chat/MessageInput.svelte
+++ src/lib/components/chat/MessageInput.svelte
@@ -1334,7 +1334,7 @@
 															xmlns="http://www.w3.org/2000/svg"
 															viewBox="0 0 24 24"
 															fill="currentColor"
-															class="size-6"
+															class="size-5"
 														>
 															<path
 																fill-rule="evenodd"
