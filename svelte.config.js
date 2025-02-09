3e690c487c643ad90d040a0090e669767980e2e6
zsombor-n <zsombornagy23@gmail.com>
2025-02-09 18:11:24 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'public',
+			assets: 'public',
 			fallback: 'index.html'
 		})
 	},
