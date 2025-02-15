22884242427171414fd94383bef1c6b13be0bc0d
ephemeral172 <145383794+ephemeral172@users.noreply.github.com>
2025-02-15 20:39:01 +0300

    Update UserMenu.svelte

--- src/lib/components/layout/Sidebar/UserMenu.svelte
+++ src/lib/components/layout/Sidebar/UserMenu.svelte
@@ -47,12 +47,11 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
           </svg>
         </div>
-        <div class="self-center truncate">{$i18n.t('Upgrade Plan')}</div>
+        <div class="self-center truncate">{$i18n.t('Update Plan')}</div>
       </button>
 
       <!-- Другие элементы меню -->
 
-      <!-- Кнопка настроек -->
       <button
         class="flex rounded-md py-2 px-3 w-full hover:bg-gray-50 dark:hover:bg-gray-800 transition"
         on:click={async () => {
@@ -64,31 +63,15 @@
           }
         }}
       >
-        <div class="self-center mr-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-            />
+        <div class=" self-center mr-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
         </div>
-        <div class="self-center truncate">{$i18n.t('Settings')}</div>
+        <div class=" self-center truncate">{$i18n.t('Settings')}</div>
       </button>
 
-      <!-- Архивированные чаты -->
       <button
         class="flex rounded-md py-2 px-3 w-full hover:bg-gray-50 dark:hover:bg-gray-800 transition"
         on:click={() => {
@@ -100,25 +83,25 @@
           }
         }}
       >
-        <div class="self-center mr-3">
+        <div class=" self-center mr-3">
           <ArchiveBox className="size-5" strokeWidth="1.5" />
         </div>
-        <div class="self-center truncate">{$i18n.t('Archived Chats')}</div>
+        <div class=" self-center truncate">{$i18n.t('Archived Chats')}</div>
       </button>
 
       {#if role === 'admin'}
-        <!-- Панель администратора -->
         <a
           class="flex rounded-md py-2 px-3 w-full hover:bg-gray-50 dark:hover:bg-gray-800 transition"
           href="/playground"
           on:click={() => {
             show = false;
+
             if ($mobile) {
               showSidebar.set(false);
             }
           }}
         >
-          <div class="self-center mr-3">
+          <div class=" self-center mr-3">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -134,7 +117,7 @@
               />
             </svg>
           </div>
-          <div class="self-center truncate">{$i18n.t('Playground')}</div>
+          <div class=" self-center truncate">{$i18n.t('Playground')}</div>
         </a>
 
         <a
@@ -142,12 +125,13 @@
           href="/admin"
           on:click={() => {
             show = false;
+
             if ($mobile) {
               showSidebar.set(false);
             }
           }}
         >
-          <div class="self-center mr-3">
+          <div class=" self-center mr-3">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -163,69 +147,39 @@
               />
             </svg>
           </div>
-          <div class="self-center truncate">{$i18n.t('Admin Panel')}</div>
+          <div class=" self-center truncate">{$i18n.t('Admin Panel')}</div>
         </a>
       {/if}
 
-      <hr class="border-gray-50 dark:border-gray-850 my-1 p-0" />
+      <hr class=" border-gray-50 dark:border-gray-850 my-1 p-0" />
 
-      <!-- Выход -->
       <button
         class="flex rounded-md py-2 px-3 w-full hover:bg-gray-50 dark:hover:bg-gray-800 transition"
-        on:click={async () => {
-          await userSignOut();
-          localStorage.removeItem('token');
-          location.href = '/auth';
-          show = false;
-        }}
+        on:click={userSignOut}
       >
-        <div class="self-center mr-3">
+        <div class=" self-center mr-3">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
             class="w-5 h-5"
           >
             <path
-              fill-rule="evenodd"
-              d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"
-              clip-rule="evenodd"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M6 10a.75.75 0 01.75-.75h9.546l-1.048-.943a.75.75 0 111.004-1.114l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 11-1.004-1.114l1.048-.943H6.75A.75.75 0 016 10z"
-              clip-rule="evenodd"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 10.5h4.5m0 0H15m4.5 0l-3-3m3 3l-3 3m-6 4.5h-6a2.25 2.25 0 0 1-2.25-2.25V6A2.25 2.25 0 0 1 6 3.75h6a2.25 2.25 0 0 1 2.25 2.25v12a2.25 2.25 0 0 1-2.25 2.25z"
             />
           </svg>
         </div>
-        <div class="self-center truncate">{$i18n.t('Sign Out')}</div>
+        <div class=" self-center truncate">{$i18n.t('Sign out')}</div>
       </button>
-
-      {#if $activeUserIds?.length > 0}
-        <hr class="border-gray-50 dark:border-gray-850 my-1 p-0" />
-
-        <Tooltip
-          content={$USAGE_POOL && $USAGE_POOL.length > 0
-            ? `${$i18n.t('Running')}: ${$USAGE_POOL.join(', ')} ✨`
-            : ''}
-        >
-          <div class="flex rounded-md py-1.5 px-3 text-xs gap-2.5 items-center">
-            <div class="flex items-center">
-              <span class="relative flex size-2">
-                <span
-                  class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"
-                />
-                <span class="relative inline-flex rounded-full h-2.5 w-2.5 bg-green-500" />
-              </span>
-            </div>
-            <div class="flex flex-col justify-center">
-              <div class="font-medium text-xs text-gray-700 dark:text-gray-300">
-                {$i18n.t('Service Active')}
-              </div>
-            </div>
-          </div>
-        </Tooltip>
-      {/if}
     </DropdownMenu.Content>
   </slot>
 </DropdownMenu.Root>
+
+<!-- Модальное окно -->
+{#if showUpdatePlanModal}
+  <UpdatePlanModal {showUpdatePlanModal} on:close={() => showUpdatePlanModal = false} />
+{/if}
