804c8b77b89cc9f3f697e1383f1d4c127c225f9b
Thomas Lehmann <t.lehmann@strato.de>
2025-02-17 11:28:44 +0100

    c/feat(Sidebar/SearchInput): move tags: option
    The actual search by typing "tags:" is left to reduce the change,
    it should not be problematic if a user is able to search by tags
    although other tag management is disabled too.
    
    Refs: PRODAI-88

--- src/lib/components/layout/Sidebar/SearchInput.svelte
+++ src/lib/components/layout/Sidebar/SearchInput.svelte
@@ -16,10 +16,6 @@
 	$: lastWord = value ? value.split(' ').at(-1) : value;
 
 	let options = [
-		{
-			name: 'tag:',
-			description: $i18n.t('search for tags')
-		}
 	];
 	let focused = false;
 	let loading = false;
