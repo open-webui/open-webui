5103c36e08ac47eb0164a9900d1aff00a0dea477
Thomas Lehmann <t.lehmann@strato.de>
2025-02-28 13:59:28 +0100

    chore(app): refactor Sidebar from +layout to +pages
    Prepare refactoring Sidebar new chat button to dispatch event which can
    be listened to to call new chat.
    
    == Background
    
    To start a new chat the Sidebar component uses getElementById() to locate
    a new chat button within the Navbar (yes) to call click().
    
    This is bad in two ways:
    
    1. It creates depedencies across the application (i.e. the Navbar new
       chat button can not be removed without breaking the Sidebar new chat
       button)
    
    2. It makes the functionality harder to read/trace.
    
    == Solution
    
    Since it's not possible to propagate events into a slot it's not
    possible to bubble it up and then down into the slot and Chat.
    
    Therefor Chat moved into the children pages.

--- src/routes/(app)/c/[id]/+page.svelte
+++ src/routes/(app)/c/[id]/+page.svelte
@@ -3,7 +3,9 @@
 
 	import Chat from '$lib/components/chat/Chat.svelte';
 	import Help from '$lib/components/layout/Help.svelte';
+	import Sidebar from '$lib/components/layout/Sidebar.svelte';
 </script>
 
+<Sidebar />
 <Help />
 <Chat chatIdProp={$page.params.id} />
--- src/routes/(app)/channels/[id]/+page.svelte
+++ src/routes/(app)/channels/[id]/+page.svelte
@@ -2,6 +2,8 @@
 	import { page } from '$app/stores';
 
 	import Channel from '$lib/components/channel/Channel.svelte';
+	import Sidebar from '$lib/components/layout/Sidebar.svelte';
 </script>
 
+<Sidebar />
 <Channel id={$page.params.id} />
