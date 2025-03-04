5f052bee05389dca45d63face2c198122d07154c
Thomas Lehmann <t.lehmann@strato.de>
2025-03-04 11:48:54 +0100

    c/feat(AgentSelector): show details on hover in agent selector
    * Agent card is expanded on hover, not on click
    * Width is expanded too
    * Changed photo aspect ratio
    
    According to review with UX the designer.
    
    Refs: PRODAI-105

--- src/lib/IONOS/components/AgentSelector.svelte
+++ src/lib/IONOS/components/AgentSelector.svelte
@@ -5,21 +5,17 @@
 	import ChevronRight from '$lib/components/icons/ChevronRight.svelte'
 
 	const i18n = getContext('i18n');
-
-	let selectedAgentId = null;
 </script>
 
 <div class="flex flex-row gap-4 items-center h-96">
 	{#each $agents as { id, name, subtitle, description, avatarUrl }}
 		<div
-			class="flex-0 w-60 pb-4 mx-3 bg-white text-left rounded-3xl shadow-xl cursor-pointer transition group {(id === selectedAgentId) ? 'selected scale-100' : 'scale-75 hover:scale-90 focus:scale-90'}"
+			class="flex-0 transition group w-48 hover:w-60 pb-4 mx-6 hover:mx-0 bg-white text-left rounded-3xl shadow-xl cursor-pointer transition group"
 			data-id={id}
 			tabindex="0"
 			role="button"
-			on:keypress={({ key }) => { if (key === "Enter") { selectedAgentId = id; } }}
-			on:click={() => selectedAgentId = id}
 		>
-			<div class="rounded-3xl">
+			<div class="overflow-hidden h-40 w-48 group-hover:w-60 group-hover:h-48 rounded-t-3xl">
 				<img
 					class="rounded-3xl rounded-b-none"
 					src={avatarUrl}
@@ -33,19 +29,21 @@
 				<h2 class="text-sm">
 					{subtitle}
 				</h2>
-				<p class="text-xs mt-4 hidden group-[.selected]:block" aria-hidden={id !== selectedAgentId}>
-					{description}
-				</p>
-				<div class="text-center hidden group-[.selected]:block" aria-hidden={id !== selectedAgentId}>
-					<button
-						class="flex flex-row items-center m-auto px-4 py-2 mt-4 bg-blue-900 hover:bg-blue-800 text-gray-100 transition rounded-3xl s-GWo4gqQzfpKs"
-						on:click={() => selectAgent(id)}
-					>
-						<span class="pr-2">
-							{$i18n.t('Chat now', { ns: 'ionos' })}
-						</span>
-						<ChevronRight className="w-4 h-4" />
-					</button>
+				<div class="mt-0 overflow-hidden transition-[height] h-0 group-hover:h-36 group-hover:mt-4 ">
+					<p class="text-xs">
+						{description}
+					</p>
+					<div class="text-center">
+						<button
+							class="flex flex-row items-center m-auto px-4 py-2 mt-4 bg-blue-900 hover:bg-blue-800 text-gray-100 transition rounded-3xl s-GWo4gqQzfpKs"
+							on:click={() => selectAgent(id)}
+						>
+							<span class="pr-2">
+								{$i18n.t('Chat now', { ns: 'ionos' })}
+							</span>
+							<ChevronRight className="w-4 h-4" />
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
