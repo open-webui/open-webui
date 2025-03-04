9c107f9d2d4cd747d5831c9251c31c57bb0bb10f
Timothy Jaeryang Baek <tim@openwebui.com>
2025-03-03 20:07:59 -0800

    refac: images "url" fallback

--- backend/open_webui/routers/images.py
+++ backend/open_webui/routers/images.py
@@ -517,7 +517,13 @@ async def image_generations(
             images = []
 
             for image in res["data"]:
-                image_data, content_type = load_b64_image_data(image["b64_json"])
+                if "url" in image:
+                    image_data, content_type = load_url_image_data(
+                        image["url"], headers
+                    )
+                else:
+                    image_data, content_type = load_b64_image_data(image["b64_json"])
+
                 url = upload_image(request, data, image_data, content_type, user)
                 images.append({"url": url})
             return images
