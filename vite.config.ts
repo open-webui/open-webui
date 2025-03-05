58c205ea306f1b8c34c79bfe0b5f9a571ccebd25
Thomas Lehmann <t.lehmann@strato.de>
2025-03-05 14:55:09 +0100

    c/feat(tailwind): configure default font OpenSans
    To comply to styleguide.
    
    Refs: PRODAI-105

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 
 // /** @type {import('vite').Plugin} */
 // const viteServerConfig = {
