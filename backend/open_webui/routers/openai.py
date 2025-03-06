2e4901006d10c60cacd0fcacca48ae5b57a3b750
orenzhang <orenzhang@ovinc.cn>
2025-03-06 12:07:21 +0800

    feat(credit): add usage calculate for stream response

--- backend/open_webui/routers/openai.py
+++ backend/open_webui/routers/openai.py
@@ -16,6 +16,7 @@ from fastapi.responses import FileResponse, StreamingResponse
 from pydantic import BaseModel
 from starlette.background import BackgroundTask
 
+from open_webui.models.credits import Credits
 from open_webui.models.models import Models
 from open_webui.config import (
     CACHE_DIR,
@@ -578,6 +579,8 @@ async def generate_chat_completion(
     user=Depends(get_verified_user),
     bypass_filter: Optional[bool] = False,
 ):
+    Credits.check_credit_by_user_id(user_id=user.id, error_msg=request.app.state.config.CREDIT_NO_CREDIT_MSG)
+
     if BYPASS_MODEL_ACCESS_CONTROL:
         bypass_filter = True
 
@@ -708,9 +711,16 @@ async def generate_chat_completion(
 
         # Check if response is SSE
         if "text/event-stream" in r.headers.get("Content-Type", ""):
+            async def consumer_content(content):
+                with CreditDeduct(
+                    request=request, user=user, model=model, body=form_data, is_stream=True
+                ) as credit_deduct:
+                    async for chunk in content:
+                        credit_deduct.run(response=chunk)
+                        yield chunk
             streaming = True
             return StreamingResponse(
-                r.content,
+                consumer_content(r.content),
                 status_code=r.status,
                 headers=dict(r.headers),
                 background=BackgroundTask(
