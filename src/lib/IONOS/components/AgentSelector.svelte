9ca71408f0982fefbcae861cd9af3011c20c7373
Thomas Lehmann <t.lehmann@strato.de>
2025-03-06 16:46:12 +0100

    c/feat(AgentSelector): rework card expand on hover
    According to updated Figma.
    
    When the agents wrap to one column the cards shall be always expanded.
    
    This partially reverts:
    
       c/feat(AgentSelector): set description width to hovered width
       c/feat(AgentSelector): reduce y-margin to reduce space, expand on hover
    
    Refs: PRODAI-105

--- src/lib/IONOS/components/AgentSelector.svelte
+++ src/lib/IONOS/components/AgentSelector.svelte
@@ -7,15 +7,15 @@
 	const i18n = getContext('i18n');
 </script>
 
-<div class="flex flex-row gap-4 items-center">
+<div class="flex flex-row gap-4 items-center h-96">
 	{#each $agents as { id, name, subtitle, description, avatarUrl }}
 		<div
-			class="flex-0 transition-[width,margin-left,margin-right,margin-top,margin-bottom] duration-[500ms] group w-44 hover:w-60 pb-4 mx-6 my-8 hover:mx-0 hover:my-0 bg-white text-left rounded-3xl shadow-xl cursor-pointer transition group"
+			class="flex-0 group w-52 pb-4 mx-6 my-8 bg-white text-left rounded-3xl shadow-xl cursor-pointer transition group"
 			data-id={id}
 			tabindex="0"
 			role="button"
 		>
-			<div class="overflow-hidden transition-[width,height] duration-[500ms] h-36 w-44 group-hover:w-60 group-hover:h-48 rounded-t-3xl">
+			<div class="overflow-hidden transition-[width,height] duration-[500ms] h-36 rounded-t-3xl">
 				<img
 					class="rounded-3xl rounded-b-none"
 					src={avatarUrl}
@@ -23,19 +23,19 @@
 				/>
 			</div>
 			<div class="px-4 cursor-default overflow-hidden">
-				<h1 class="text-sm font-semibold mt-4">
+				<h1 class="text-sm font-semibold mt-2">
 					{name}
 				</h1>
 				<h2 class="text-sm">
 					{subtitle}
 				</h2>
-				<div class="mt-0 overflow-hidden duration-[500ms] transition-[height,margin-top] w-60 h-0 group-hover:h-36 group-hover:mt-4 ">
+				<div class="mt-0 overflow-hidden duration-[500ms] transition-[height,margin-top] h-0 group-hover:h-40 group-hover:mt-4 group-focus:h-40 group-focus:mt-4 focus-within:h-40 focus-within:mt-4 max-xs:h-40 max-xs:mt-4">
 					<p class="text-xs">
 						{description}
 					</p>
 					<div class="text-center">
 						<button
-							class="flex flex-row items-center m-auto px-4 py-2 mt-4 bg-blue-900 hover:bg-blue-800 text-gray-100 transition rounded-3xl s-GWo4gqQzfpKs"
+							class="flex flex-row items-center m-auto px-4 py-2 mt-4 bg-blue-900 hover:bg-blue-800 focus:bg-ai-main-700 text-gray-100  transition rounded-3xl"
 							on:click={() => selectAgent(id)}
 						>
 							<span class="pr-2">
