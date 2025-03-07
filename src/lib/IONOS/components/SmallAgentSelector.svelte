924e66ca6443edc08dd8147ea8b78f429713c047
Thomas Lehmann <t.lehmann@strato.de>
2025-03-07 16:34:49 +0100

    c/feat(chat/Placeholder): change to custom compact agent selector in the chat home/index
    * Mostly according to Figma
    * No agent icons yet because there's no infrastructure for it right now
    * Colors will be changed in a later task all at once
    
    Refs: PRODAI-123

---
+++ src/lib/IONOS/components/SmallAgentSelector.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { getContext, onMount } from 'svelte';
+	import { models, settings } from '$lib/stores';
+	import { updateUserSettings } from '$lib/apis/users';
+
+	export let selectedModels = [''];
+
+	const withoutStockModels = ({ id }) => !id.includes('/') && id !== 'arena-model';
+
+	$: modelsUi = $models
+		.filter(withoutStockModels)
+		.map((model) => ({
+			id: model.id,
+			modelDisplayName: model.name,
+		}));
+
+	const save = async () => {
+		settings.set({ ...$settings, models: selectedModels });
+		await updateUserSettings(localStorage.token, { ui: $settings });
+	}
+
+	const select = (id) => {
+		selectedModels = [id];
+		save();
+	}
+</script>
+
+<div class="flex flex-row items-start gap-3">
+	{#each modelsUi as { id, modelDisplayName }}
+		<button
+			data-model-id={id}
+			on:click={() => select(id)}
+			class="px-4 py-1 border-2 border-sky-900 bg-white-500 hover:bg-sky-800 text-black hover:text-white transition rounded-3xl"
+		>
+			{modelDisplayName}
+		</button>
+	{/each}
+</div>
