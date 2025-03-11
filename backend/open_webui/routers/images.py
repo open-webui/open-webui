b1224b6839e88fec57c3869e453240c6f7c64f6d
Davi S. Zucon <davizucon@users.noreply.github.com>
2025-03-11 12:40:31 -0300

    fix code format

--- backend/open_webui/routers/images.py
+++ backend/open_webui/routers/images.py
@@ -517,7 +517,7 @@ async def image_generations(
             images = []
 
             for image in res["data"]:
-                if image_url := image.get("url",None):
+                if image_url := image.get("url", None):
                     image_data, content_type = load_url_image_data(image_url, headers)
                 else:
                     image_data, content_type = load_b64_image_data(image["b64_json"])
