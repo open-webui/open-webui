51bb080bce57bffda67b3d41d3a95a27d8f4516b
Anh Nguyen Ngoc <ngocanhnckh@gmail.com>
2025-03-12 20:20:15 +0700

    updated branding

--- src/lib/components/admin/Functions.svelte
+++ src/lib/components/admin/Functions.svelte
@@ -65,7 +65,7 @@
 			return null;
 		});
 
-		toast.success($i18n.t('Redirecting you to Open WebUI Community'));
+		toast.success($i18n.t('Redirecting you to Community'));
 
 		const url = 'https://openwebui.com';
 
@@ -469,7 +469,7 @@
 {#if $config?.features.enable_community_sharing}
 	<div class=" my-16">
 		<div class=" text-xl font-medium mb-1 line-clamp-1">
-			{$i18n.t('Made by Open WebUI Community')}
+			{$i18n.t('Made by Community')}
 		</div>
 
 		<a
