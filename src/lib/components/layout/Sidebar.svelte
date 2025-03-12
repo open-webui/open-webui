6465cb778fdcd87e511e5a6501ef1203ecdf710d
Jaap den Hollander <jaapdh@yahoo.com>
2025-03-12 13:26:04 +0100

    Update Sidebar.svelte
    have an id for the button to reach this.

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -477,6 +477,7 @@
 	>
 		<div class="px-1.5 flex justify-between space-x-1 text-gray-600 dark:text-gray-400">
 			<button
+				id="button-sidebar"
 				class=" cursor-pointer p-[7px] flex rounded-xl hover:bg-gray-100 dark:hover:bg-gray-900 transition"
 				on:click={() => {
 					showSidebar.set(!$showSidebar);
