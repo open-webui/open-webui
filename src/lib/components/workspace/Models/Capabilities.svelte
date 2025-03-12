d06ef91340315ba79bea62e1901d32a3b33ff35f
fwasmeier <florian.wasmeier@lindner-group.com>
2025-03-12 14:08:04 +0100

    Added websearch, codeexecution and image generation capabilities to the models. Masked the capabilities with displayNames for better UI readability.

--- src/lib/components/workspace/Models/Capabilities.svelte
+++ src/lib/components/workspace/Models/Capabilities.svelte
@@ -11,13 +11,28 @@
 		usage: $i18n.t(
 			'Sends `stream_options: { include_usage: true }` in the request.\nSupported providers will return token usage information in the response when set.'
 		),
-		citations: $i18n.t('Displays citations in the response')
+		citations: $i18n.t('Displays citations in the response'),
+		websearch: $i18n.t('Enables web search capability for this model. (if active in Admin Settings)'),
+		codeexecution: $i18n.t('Enables codeInterpreter capability for this model. (if active in Admin Settings)'),
+		imagegeneration: $i18n.t('Enables image generation capability for this model. (if active in Admin Settings)'),
 	};
+
+    const displayNames = {
+        vision: $i18n.t('Vision'),
+        usage: $i18n.t('Usage'),
+        citations: $i18n.t('Citations'),
+        websearch: $i18n.t('Web Search'),
+        codeexecution: $i18n.t('Code Execution'),
+        imagegeneration: $i18n.t('Image Generation')
+    };
 
 	export let capabilities: {
 		vision?: boolean;
 		usage?: boolean;
 		citations?: boolean;
+		websearch?: boolean;
+		codeexecution?: boolean;
+		imagegeneration?: boolean;
 	} = {};
 </script>
 
@@ -37,7 +52,7 @@
 
 				<div class=" py-0.5 text-sm capitalize">
 					<Tooltip content={marked.parse(helpText[capability])}>
-						{$i18n.t(capability)}
+						{displayNames[capability]}
 					</Tooltip>
 				</div>
 			</div>
