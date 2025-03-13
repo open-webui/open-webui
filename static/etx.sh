db5b0343e33800ed75024b9a4a65feaf1d9089d7
aderbalbotelho <aderbal@gmail.com>
2025-03-13 12:50:53 -0300

    1.2

---
+++ static/etx.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cp -rf ETX.png static/user.png
+cp -rf ETX.png static/favicon.png
+cp -rf ETX.png static/doge.png
+cp -rf ETX.png static/favicon.png
+cp -rf ETX.png static/favicon-96x96.png
+cp -rf ETX.png static/web-app-manifest-192x192.png
+cp -rf ETX.png static/web-app-manifest-512x512.png
+cp -rf ETX.png static/favicon.ico
+cp -rf logo-etx.svg static/favicon.svg
+cp -rf ETX.png static/favicon-dark.png
+cp -rf ETX.png static/favicon-96x96.png
+cp -rf ETX.png static/doge.png
+cp -rf ETX.png static/apple-touch-icon.png
+cp -rf ETX.png static/favicon.png
+cp -rf logo-etx.svg static/favicon.svg
+cp -rf etx-logo-branco.png static/splash-dark.png
+cp -rf etx-logo-branco.png static/splash.png
+cp -rf ETX.png static/static/favicon.ico
+cp -rf logo-etx.svg static/static/favicon.svg
+cp -rf ETX.png static/static/favicon-dark.png
+cp -rf ETX.png static/static/favicon-96x96.png
+cp -rf ETX.png static/static/doge.png
+cp -rf ETX.png static/static/apple-touch-icon.png
+cp -rf ETX.png static/static/favicon.png
+cp -rf logo-etx.svg static/static/favicon.svg
+cp -rf etx-logo-branco.png static/static/splash-dark.png
+cp -rf etx-logo-branco.png static/static/splash.png
+cp -rf ETX.png static/static/favicon-96x96.png
+cp -rf ETX.png static/static/web-app-manifest-192x192.png
+cp -rf ETX.png static/static/web-app-manifest-512x512.png
+
+exit 0
