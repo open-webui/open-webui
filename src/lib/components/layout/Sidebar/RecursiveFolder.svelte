18a8a375aba438953cbab382eddcc2a17d0de9e8
hurxxxx <hurxxxx@gmail.com>
2025-03-20 00:27:53 +0900

    fix: Enter edit mode with text pre-selected

--- src/lib/components/layout/Sidebar/RecursiveFolder.svelte
+++ src/lib/components/layout/Sidebar/RecursiveFolder.svelte
@@ -309,10 +309,13 @@
 
 		await tick();
 
-		// focus on the input
+		// focus on the input and select all text
 		setTimeout(() => {
 			const input = document.getElementById(`folder-${folderId}-input`);
-			input.focus();
+			if (input) {
+				input.focus();
+				input.select();
+			}
 		}, 100);
 	};
 
