a1a736b646cd567ccd8b76a7ceeab231d26bb094
t-kobyliakova <tetiana.kobyliakoba@beanmachine.dev>
2025-03-30 16:08:33 +0300

    Update left side bar folders appearance

---
+++ src/lib/components/icons/RenameIcon.svelte
@@ -0,0 +1,30 @@
+<svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g clip-path="url(#clip0_308_8222)">
+		<path
+			d="M10.8334 9.20833V2.70833C10.8334 2.10112 10.3573 1.625 9.75008 1.625C9.14287 1.625 8.66675 2.10112 8.66675 2.70833V9.20833L9.75008 10.2917L10.8334 9.20833Z"
+			stroke="currentColor"
+			stroke-width="0.8"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+		<path
+			d="M8.66675 3.79175H10.8334"
+			stroke="currentColor"
+			stroke-width="0.8"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+		<path
+			d="M9.75 10.2916H2.70833C2.42102 10.2916 2.14547 10.1774 1.9423 9.97428C1.73914 9.77112 1.625 9.49557 1.625 9.20825C1.625 8.92093 1.73914 8.64538 1.9423 8.44222C2.14547 8.23906 2.42102 8.12492 2.70833 8.12492H4.875C5.16232 8.12492 5.43787 8.01078 5.64103 7.80762C5.8442 7.60445 5.95833 7.3289 5.95833 7.04159C5.95833 6.75427 5.8442 6.47872 5.64103 6.27555C5.43787 6.07239 5.16232 5.95825 4.875 5.95825H3.25"
+			stroke="currentColor"
+			stroke-width="0.8"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</g>
+	<defs>
+		<clipPath id="clip0_308_8222">
+			<rect width="13" height="13" fill="white" />
+		</clipPath>
+	</defs>
+</svg>
---
+++ src/lib/components/icons/SignOutIcon.svelte
@@ -0,0 +1,23 @@
+<svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g clip-path="url(#clip0_256_5205)">
+		<path
+			d="M7 4.5V3.5C7 3.23478 6.89464 2.98043 6.70711 2.79289C6.51957 2.60536 6.26522 2.5 6 2.5H2.5C2.23478 2.5 1.98043 2.60536 1.79289 2.79289C1.60536 2.98043 1.5 3.23478 1.5 3.5V9.5C1.5 9.76522 1.60536 10.0196 1.79289 10.2071C1.98043 10.3946 2.23478 10.5 2.5 10.5H6C6.26522 10.5 6.51957 10.3946 6.70711 10.2071C6.89464 10.0196 7 9.76522 7 9.5V8.5"
+			stroke="currentColor"
+			stroke-width="0.8"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+		<path
+			d="M3.5 6.5H10.5M10.5 6.5L9 5M10.5 6.5L9 8"
+			stroke="currentColor"
+			stroke-width="0.8"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</g>
+	<defs>
+		<clipPath id="clip0_256_5205">
+			<rect width="12" height="12" fill="white" transform="translate(0 0.5)" />
+		</clipPath>
+	</defs>
+</svg>
