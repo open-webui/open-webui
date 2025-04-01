abba7c128c2b38199ff1a50a9de1fb277d4c840a
Erik <erik.weisser@gmail.com>
2025-04-01 08:21:03 +0200

    [FEAT]-Adjust Translations for temporary chat

--- src/lib/components/chat/ChatPlaceholder.svelte
+++ src/lib/components/chat/ChatPlaceholder.svelte
@@ -29,6 +29,8 @@
 
 	$: models = modelIds.map((id) => $_models.find((m) => m.id === id));
 
+	const tempChatTooltipText = 'This chat won’t appear in history and your messages will not be saved.';
+	
 	onMount(() => {
 		mounted = true;
 	});
@@ -67,15 +69,15 @@
 		</div>
 
 		{#if $temporaryChatEnabled}
-			<Tooltip
-				content="This chat won't appear in history and your messages will not be saved."
-				className="w-fit"
-				placement="top-start"
-			>
-				<div class="flex items-center gap-2 text-gray-500 font-medium text-lg my-2 w-fit">
-					<EyeSlash strokeWidth="2.5" className="size-5" /> Temporary Chat
-				</div>
-			</Tooltip>
+		<Tooltip
+			content={$i18n.t(tempChatTooltipText)}
+			className="w-full flex justify-center mb-0.5"
+			placement="top"
+		>
+			<div class="flex items-center gap-2 text-gray-500 font-medium text-lg my-2 w-fit">
+				<EyeSlash strokeWidth="2.5" className="size-5" />{$i18n.t('Temporary Chat')}
+			</div>
+		</Tooltip>
 		{/if}
 
 		<div
