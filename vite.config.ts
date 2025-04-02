c511210a9fdb9c3dc3e44cf9e737c77748d9d067
lee seonguk <passion1014@gmail.com>
2025-04-02 20:17:49 +0900

    ㅊㅊㅊ

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import svelte from '@sveltejs/vite-plugin-svelte'
 
 // /** @type {import('vite').Plugin} */
 // const viteServerConfig = {
@@ -16,6 +17,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 // 		});
 // 	}
 // };
+
 
 export default defineConfig({
 	plugins: [
@@ -33,7 +35,10 @@ export default defineConfig({
 	server: {
 		watch: {
 		  ignored: ['**/venv/**', '**/site-packages/**']
-		}
+		},
+		host: '0.0.0.0',
+		port: 5173,
+		allowedHosts: ['codeassist.axlrator.com'], // 여기를 추가		
 	},	
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
