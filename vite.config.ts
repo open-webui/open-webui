3f8088d0723a840db34f7667db8b4eabfafcdb1b
lee seonguk <passion1014@gmail.com>
2025-04-03 13:52:05 +0900

    123

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import svelte from '@sveltejs/vite-plugin-svelte'
-
-// /** @type {import('vite').Plugin} */
-// const viteServerConfig = {
-// 	name: 'log-request-middleware',
-// 	configureServer(server) {
-// 		server.middlewares.use((req, res, next) => {
-// 			res.setHeader('Access-Control-Allow-Origin', '*');
-// 			res.setHeader('Access-Control-Allow-Methods', 'GET');
-// 			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-// 			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-// 			next();
-// 		});
-// 	}
-// };
-
+import svelte from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
 	plugins: [
@@ -38,12 +23,17 @@ export default defineConfig({
 		},
 		host: '0.0.0.0',
 		port: 5173,
-		allowedHosts: ['codeassist.axlrator.com','axlrator.com', 'www.axlrator.com'], // 여기를 추가
+		allowedHosts: ['codeassist.axlrator.com', 'axlrator.com', 'www.axlrator.com'],
 		hmr: {
 			protocol: 'wss',
-			host: 'axlrator.com',
+			host: 'axlrator.com'
 		}
 	},	
+	preview: {
+		host: '0.0.0.0',
+		port: 4173,
+		allowedHosts: ['axlrator.com', 'www.axlrator.com']
+	},
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version),
 		APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
