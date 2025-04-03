469224d74d88882e6f49668bbf31d34666342bef
lee seonguk <passion1014@gmail.com>
2025-04-03 11:20:14 +0900

    ㅊㅊ

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -874,7 +874,7 @@ async def inspect_websocket(request: Request, call_next):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://91.99.20.220:5173","http://localhost:5173"], 
+    allow_origins=["https://axlrator.com","http://91.99.20.220:5173","http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
