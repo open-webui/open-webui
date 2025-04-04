3b2b6e183d4aaa89a65043592b49b8f1f00dae01
Marko Henning <marko.henning@posteo.de>
2025-04-04 15:30:57 +0200

    Added missing parameter for query_doc_with_hybrid_search.

--- backend/open_webui/routers/retrieval.py
+++ backend/open_webui/routers/retrieval.py
@@ -1534,8 +1534,13 @@ def query_doc_handler(
 ):
     try:
         if request.app.state.config.ENABLE_RAG_HYBRID_SEARCH:
+            collection_results = {}
+            collection_results[form_data.collection_name] = VECTOR_DB_CLIENT.get(
+                collection_name=form_data.collection_name
+            )
             return query_doc_with_hybrid_search(
                 collection_name=form_data.collection_name,
+                collection_result=collection_results[form_data.collection_name],
                 query=form_data.query,
                 embedding_function=lambda query, prefix: request.app.state.EMBEDDING_FUNCTION(
                     query, prefix=prefix, user=user
