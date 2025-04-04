aacfd11fc14f2daf83c09010610d8721a3e00c1f
Thomas Lehmann <t.lehmann@strato.de>
2025-04-04 17:35:09 +0200

    c/refactor(apis/auths): simplify overcomplicated promise + async/await mixup
    * The code was way to complicated and indirect by mixing async/await
      and promise handling.
    * A clearer exception message is now constructed
    * `console.error()` is mostly redundant as an uncatched exception shows
      up in Devtool consoles anyway
    * Content-Type was removed as this response (all GETs) does not send
      JSON in the body
    
    Note:
    
    * fetch() only rejects if the request could not be made (offline) or
      the browser is offline [1]
    
    * `err.detail` would have been undefined for errors thrown by
      fetch() itself as there's no `detail` in a `TypeError` [2], [3]
    
    * The function did not return response payload. This is not changed
      here, but will be changed in the next commit.
    
    * Instead of capturing the error in the top scope and throwing it
      conditionally just returning it in `catch()` would suffice if the
      whole chain would be returned
    
    * `console.error()` is mostly redundant as an uncatched exception shows
      up in Devtool consoles anyway
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/API/Window/fetch
    [2]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError
    [3]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
    
    Refs: PRODAI-168

--- src/lib/apis/auths/index.ts
+++ src/lib/apis/auths/index.ts
@@ -325,27 +325,19 @@ export const userSignUp = async (
 };
 
 export const userSignOut = async () => {
-	let error = null;
-
 	const res = await fetch(`${WEBUI_API_BASE_URL}/auths/signout`, {
 		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		},
 		credentials: 'include'
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res;
-		})
-		.catch((err) => {
-			console.log(err);
-			error = err.detail;
-			return null;
-		});
+	});
 
-	if (error) {
-		throw error;
+	if (!res.ok) {
+		let detail;
+
+		try {
+			detail = (await res.json())?.detail;
+		} catch (e) { /* don't try to recover if server errors are no JSON */ }
+
+		throw new Error(detail ?? `Request failed with no details: ${res.status} ${res.statusText}`);
 	}
 };
 
