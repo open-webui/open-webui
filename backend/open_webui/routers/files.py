6d5cb6b04ee188e644fc71a5e3fcbdc90dcf181a
Juan Calderon-Perez <835733+gaby@users.noreply.github.com>
2025-04-03 23:02:24 -0400

    Add query param to remove content from GET /api/v1/files

--- backend/open_webui/routers/files.py
+++ backend/open_webui/routers/files.py
@@ -162,11 +162,15 @@ def upload_file(
 
 
 @router.get("/", response_model=list[FileModelResponse])
-async def list_files(user=Depends(get_verified_user)):
+async def list_files(user=Depends(get_verified_user), content: bool = Query(True)):
     if user.role == "admin":
         files = Files.get_files()
     else:
         files = Files.get_files_by_user_id(user.id)
+
+    if not content:
+        for file in files:
+            file.data['content'] = ""
     return files
 
 
