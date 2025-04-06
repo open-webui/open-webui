2d4b49c5e6bc93c489cff8f0259259f1d3832850
tetianako-prog <t.kobyliakova@gmail.com>
2025-04-06 13:24:11 +0300

    Update models menu

--- src/lib/components/workspace/Models/ModelMenu.svelte
+++ src/lib/components/workspace/Models/ModelMenu.svelte
@@ -13,6 +13,9 @@
 	import DocumentDuplicate from '$lib/components/icons/DocumentDuplicate.svelte';
 	import ArrowDownTray from '$lib/components/icons/ArrowDownTray.svelte';
 	import ArrowUpCircle from '$lib/components/icons/ArrowUpCircle.svelte';
+	import MessageEditIcon from '$lib/components/icons/MessageEditIcon.svelte';
+	import DeleteIcon from '$lib/components/icons/DeleteIcon.svelte';
+	import ShareIcon from '$lib/components/icons/ShareIcon.svelte';
 
 	import { config } from '$lib/stores';
 
@@ -46,25 +49,37 @@
 
 	<div slot="content">
 		<DropdownMenu.Content
-			class="w-full max-w-[160px] rounded-xl px-1 py-1.5 border border-gray-300/30 dark:border-gray-700/50 z-50 bg-white dark:bg-gray-850 dark:text-white shadow"
+			class="w-full max-w-[160px] rounded-lg px-1 py-1.5 border border-gray-300/30 dark:border-customGray-700 z-50 bg-white dark:bg-customGray-900 dark:text-white shadow"
 			sideOffset={-2}
 			side="bottom"
 			align="start"
 			transition={flyAndScale}
 		>
+			{#if user?.role === 'admin' || model.user_id === user?.id || model.access_control.write.group_ids.some( (wg) => group_ids.includes(wg) )}
+				<DropdownMenu.Item>
+					<a
+						class="flex w-full items-center gap-2 self-center text-2xs dark:text-cusromGray-100 px-3 py-2 dark:text-gray-300 dark:hover:bg-customGray-950 dark:hover:text-white hover:bg-black/5 rounded-md"
+						type="button"
+						href={`/workspace/models/edit?id=${encodeURIComponent(model.id)}`}
+					>
+						<MessageEditIcon width={10} height={9}/>
+						<div class="flex items-center">{$i18n.t('Edit')}</div>
+					</a>
+				</DropdownMenu.Item>
+			{/if}
 			{#if $config?.features.enable_community_sharing}
 				<DropdownMenu.Item
-					class="flex gap-2 items-center px-3 py-2 text-sm  font-medium cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800  rounded-md"
+					class="flex gap-2 items-center px-3 py-2 text-2xs dark:text-customGray-100 font-medium cursor-pointer hover:bg-gray-50 dark:hover:bg-customGray-950  rounded-md dark:hover:text-white"
 					on:click={() => {
 						shareHandler();
 					}}
 				>
-					<Share />
-					<div class="flex items-center">{$i18n.t('Share')}</div>
+					<ShareIcon />
+					<div class="flex items-center ">{$i18n.t('Share')}</div>
 				</DropdownMenu.Item>
 			{/if}
 
-			<DropdownMenu.Item
+			<!-- <DropdownMenu.Item
 				class="flex gap-2 items-center px-3 py-2 text-sm  font-medium cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md"
 				on:click={() => {
 					cloneHandler();
@@ -73,9 +88,9 @@
 				<DocumentDuplicate />
 
 				<div class="flex items-center">{$i18n.t('Clone')}</div>
-			</DropdownMenu.Item>
+			</DropdownMenu.Item> -->
 
-			<DropdownMenu.Item
+			<!-- <DropdownMenu.Item
 				class="flex gap-2 items-center px-3 py-2 text-sm  font-medium cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md"
 				on:click={() => {
 					exportHandler();
@@ -86,15 +101,15 @@
 				<div class="flex items-center">{$i18n.t('Export')}</div>
 			</DropdownMenu.Item>
 
-			<hr class="border-gray-100 dark:border-gray-800 my-1" />
+			<hr class="border-gray-100 dark:border-gray-800 my-1" /> -->
 
 			<DropdownMenu.Item
-				class="flex  gap-2  items-center px-3 py-2 text-sm  font-medium cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md"
+				class="flex  gap-2  items-center px-3 py-2 text-2xs dark:text-customGray-100 font-medium cursor-pointer hover:bg-gray-50 dark:hover:bg-customGray-950 rounded-md dark:hover:text-white"
 				on:click={() => {
 					deleteHandler();
 				}}
 			>
-				<GarbageBin strokeWidth="2" />
+				<DeleteIcon />
 				<div class="flex items-center">{$i18n.t('Delete')}</div>
 			</DropdownMenu.Item>
 		</DropdownMenu.Content>
