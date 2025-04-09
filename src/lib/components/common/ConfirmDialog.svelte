b2a35ea50388ae5f477f1e7c31e5d6a3685c9aab
tetianako-prog <t.kobyliakova@gmail.com>
2025-04-09 19:48:39 +0300

    Restyle confirm dialog

--- src/lib/components/common/ConfirmDialog.svelte
+++ src/lib/components/common/ConfirmDialog.svelte
@@ -5,6 +5,7 @@
 
 	import { fade } from 'svelte/transition';
 	import { flyAndScale } from '$lib/utils/transitions';
+	import CloseIcon from '../icons/CloseIcon.svelte';
 
 	export let title = '';
 	export let message = '';
@@ -65,21 +66,27 @@
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<div
 		bind:this={modalElement}
-		class=" fixed top-0 right-0 left-0 bottom-0 bg-black/60 w-full h-screen max-h-[100dvh] flex justify-center z-[99999999] overflow-hidden overscroll-contain"
+		class=" fixed top-0 right-0 left-0 bottom-0 bg-[#1D1A1A]/50 backdrop-blur-[7.44px] w-full h-screen max-h-[100dvh] flex justify-center z-[99999999] overflow-hidden overscroll-contain"
 		in:fade={{ duration: 10 }}
 		on:mousedown={() => {
 			show = false;
 		}}
 	>
 		<div
-			class=" m-auto rounded-2xl max-w-full w-[32rem] mx-2 bg-gray-50 dark:bg-gray-950 max-h-[100dvh] shadow-3xl"
+			class=" m-auto relative rounded-2xl max-w-full w-[24rem] mx-2 bg-gray-50 dark:bg-customGray-800 max-h-[100dvh] shadow-3xl"
 			in:flyAndScale
 			on:mousedown={(e) => {
 				e.stopPropagation();
 			}}
 		>
-			<div class="px-[1.75rem] py-6 flex flex-col">
-				<div class=" text-lg font-semibold dark:text-gray-200 mb-2.5">
+			<button class="absolute top-4 right-4 text-customGray-300" on:click={() => {
+				show = false;
+			}}>
+				<CloseIcon/>
+			</button>
+
+			<div class="px-10 pt-14 pb-6 py-6 flex flex-col text-center">
+				<div class=" text-base dark:text-white mb-2.5 text-center">
 					{#if title !== ''}
 						{title}
 					{:else}
@@ -88,7 +95,7 @@
 				</div>
 
 				<slot>
-					<div class=" text-sm text-gray-500 flex-1">
+					<div class=" text-sm text-gray-500 flex-1 text-center">
 						{#if message !== ''}
 							{message}
 						{:else}
@@ -107,9 +114,9 @@
 					</div>
 				</slot>
 
-				<div class="mt-6 flex justify-between gap-1.5">
+				<div class="mt-6 flex justify-end gap-7">
 					<button
-						class="bg-gray-100 hover:bg-gray-200 text-gray-800 dark:bg-gray-850 dark:hover:bg-gray-800 dark:text-white font-medium w-full py-2.5 rounded-lg transition"
+						class="text-gray-800 w-fit text-sm dark:text-customGray-200 py-2.5 rounded-lg transition"
 						on:click={() => {
 							show = false;
 							dispatch('cancel');
@@ -119,7 +126,7 @@
 						{cancelLabel}
 					</button>
 					<button
-						class="bg-gray-900 hover:bg-gray-850 text-gray-100 dark:bg-gray-100 dark:hover:bg-white dark:text-gray-800 font-medium w-full py-2.5 rounded-lg transition"
+						class="bg-gray-900 text-sm dark:bg-customGray-900 border dark:border-customGray-700 dark:hover:bg-customGray-950 text-gray-100 dark:text-customGray-200 w-1/2 py-2.5 rounded-lg transition"
 						on:click={() => {
 							confirmHandler();
 						}}
---
+++ src/lib/components/icons/CloseIcon.svelte
@@ -0,0 +1,16 @@
+<svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M3.60059 3.80005L12 12.1995"
+		stroke="currentColor"
+		stroke-width="0.96"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M3.60008 12.1995L11.9995 3.80005"
+		stroke="currentColor"
+		stroke-width="0.96"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+</svg>
