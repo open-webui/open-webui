92cd9f61465f06ad1dcbc73e00faffe074bdb637
Alessandro Pelliciari <alessandro.pelliciari@webranking.it>
2025-04-10 11:37:53 +0200

    added GTM (still to be tested properly)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -612,6 +612,14 @@
 	<!-- feel free to make a PR to fix if anyone wants to see it return -->
 	<!-- <link rel="stylesheet" type="text/css" href="/themes/rosepine.css" />
 	<link rel="stylesheet" type="text/css" href="/themes/rosepine-dawn.css" /> -->
+
+	<script data-svelte-h="svelte-1a2b3c4d">{`
+		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-5CGLZH5J');
+	`}</script>
 </svelte:head>
 
 {#if loaded}
