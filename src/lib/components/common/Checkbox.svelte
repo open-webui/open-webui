df3f1deb55fdb6084a7bc8c054dfc3630d609c57
tetianako-prog <t.kobyliakova@gmail.com>
2025-04-12 18:00:34 +0300

    Add new capabilities component

--- src/lib/components/common/Checkbox.svelte
+++ src/lib/components/common/Checkbox.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import CheckmarkIcon from '../icons/CheckmarkIcon.svelte';
 	const dispatch = createEventDispatcher();
 
 	export let state = 'unchecked';
@@ -13,7 +14,7 @@
 <button
 	class=" outline -outline-offset-1 outline-[1.5px] outline-gray-200 dark:outline-gray-600 {state !==
 	'unchecked'
-		? 'bg-black outline-black '
+		? 'bg-black dark:bg-customGray-900 outline-black '
 		: 'hover:outline-gray-500 hover:bg-gray-50 dark:hover:bg-gray-800'} text-white transition-all rounded inline-block w-3.5 h-3.5 relative"
 	on:click={() => {
 		if (_state === 'unchecked') {
@@ -33,21 +34,7 @@
 >
 	<div class="top-0 left-0 absolute w-full flex justify-center">
 		{#if _state === 'checked'}
-			<svg
-				class="w-3.5 h-3.5"
-				aria-hidden="true"
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-			>
-				<path
-					stroke="currentColor"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="3"
-					d="m5 12 4.7 4.5 9.3-9"
-				/>
-			</svg>
+			<CheckmarkIcon className="size-4"/>
 		{:else if indeterminate}
 			<svg
 				class="w-3 h-3.5 text-gray-800 dark:text-white"
