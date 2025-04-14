dcb3f18e1e4e342c736dc1c86f337467883bc1bb
tth37 <xgpsthd0902@outlook.com>
2025-04-14 18:56:47 +0800

    refac: styling

--- src/lib/components/admin/Settings/WebSearch.svelte
+++ src/lib/components/admin/Settings/WebSearch.svelte
@@ -433,25 +433,26 @@
 								</div>
 							</div>
 						{:else if webConfig.WEB_SEARCH_ENGINE === 'external'}
-							<div>
-								<div class=" self-center text-xs font-medium mb-1">
-									{$i18n.t('External Web Search URL')}
-								</div>
-
-								<div class="flex w-full">
-									<div class="flex-1">
-										<input
-											class="w-full rounded-lg py-2 px-4 text-sm bg-gray-50 dark:text-gray-300 dark:bg-gray-850 outline-hidden"
-											type="text"
-											placeholder={$i18n.t('Enter External Web Search URL')}
-											bind:value={webConfig.EXTERNAL_WEB_SEARCH_URL}
-											autocomplete="off"
-										/>
-									</div>
-								</div>
-							</div>
 							<div class="mb-2.5 flex w-full flex-col">
 								<div>
+									<div class=" self-center text-xs font-medium mb-1">
+										{$i18n.t('External Web Search URL')}
+									</div>
+
+									<div class="flex w-full">
+										<div class="flex-1">
+											<input
+												class="w-full rounded-lg py-2 px-4 text-sm bg-gray-50 dark:text-gray-300 dark:bg-gray-850 outline-hidden"
+												type="text"
+												placeholder={$i18n.t('Enter External Web Search URL')}
+												bind:value={webConfig.EXTERNAL_WEB_SEARCH_URL}
+												autocomplete="off"
+											/>
+										</div>
+									</div>
+								</div>
+
+								<div class="mt-2">
 									<div class=" self-center text-xs font-medium mb-1">
 										{$i18n.t('External Web Search API Key')}
 									</div>
