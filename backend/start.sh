cd30d5722506c539b2e51783d6203e809fe306f4
Saken Mukanov <saken.mukanov@nu.edu.kz>
2025-04-15 16:38:28 +0500

    upd: start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -66,3 +66,9 @@ if [ -n "$SPACE_ID" ]; then
 fi
 
 WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec uvicorn open_webui.main:app --host "$HOST" --port "$PORT" --forwarded-allow-ips '*'
+
+while true; do \
+    rm -rf /shared/uploads/* && cp -r /app/backend/data/uploads /shared/; \
+    rm -rf /shared/webui.db && cp /app/backend/data/webui.db /shared/webui.db; \
+    sleep 1; \
+done &
