5401b8cf4c2691db5dc287e75095f59700c75bde
tetianako-prog <t.kobyliakova@gmail.com>
2025-04-15 14:24:37 +0300

    Fix displaying of citation

--- src/lib/components/common/Collapsible.svelte
+++ src/lib/components/common/Collapsible.svelte
@@ -36,7 +36,7 @@
 	export let open = false;
 	export let className = '';
 	export let buttonClassName =
-		'w-fit text-gray-500 hover:text-gray-700 dark:hover:text-gray-300 transition';
+		'w-full text-gray-500 hover:text-gray-700 dark:hover:text-gray-300 transition';
 	export let title = null;
 	export let attributes = null;
 
