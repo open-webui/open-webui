4370def4fadff9f8fcf97681dde60d8606c01e3f
Thomas Lehmann <t.lehmann@strato.de>
2025-04-15 17:24:34 +0200

    c/feat: remove help button and accompaning overlays
    The menu links to the Open WebUI documentation. We don't want that.
    
    The keyboard shortcuts remain for now.
    
    Refs: PRODAI-179

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Chat from '$lib/components/chat/Chat.svelte';
-	import Help from '$lib/components/layout/Help.svelte';
 	import Sidebar from '$lib/components/layout/Sidebar.svelte';
 	import KnowledgeManager from '$lib/IONOS/components/knowledge/KnowledgeManager.svelte';
 
@@ -8,6 +7,5 @@
 </script>
 
 <Sidebar on:new-chat={() => chat?.newChat()} />
-<Help />
 <Chat bind:this={chat} />
 <KnowledgeManager />
--- src/routes/(app)/c/[id]/+page.svelte
+++ src/routes/(app)/c/[id]/+page.svelte
@@ -2,12 +2,10 @@
 	import { page } from '$app/stores';
 
 	import Chat from '$lib/components/chat/Chat.svelte';
-	import Help from '$lib/components/layout/Help.svelte';
 	import Sidebar from '$lib/components/layout/Sidebar.svelte';
 
 	let chat: Chat;
 </script>
 
 <Sidebar on:new-chat={() => chat?.newChat()} />
-<Help />
 <Chat bind:this={chat} chatIdProp={$page.params.id} />
