bd8dda2b1351bd38d3d477eeafdbabc9e23e951b
shadowfax <theshadowfaxproject@gmail.com>
2025-04-15 23:45:52 -0700

    open-webui-36 Remove (Open WebUI) from title.

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -106,8 +106,6 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
