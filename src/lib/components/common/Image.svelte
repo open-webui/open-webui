93076fb86154a2f1a2e56b9640136ef94487c691
shadowfax <theshadowfaxproject@gmail.com>
2025-04-17 00:42:04 -0700

    open-webui-65 Customize Image component

--- src/lib/components/common/Image.svelte
+++ src/lib/components/common/Image.svelte
@@ -5,23 +5,27 @@
 	export let src = '';
 	export let alt = '';
 
-	export let className = ' w-full outline-hidden focus:outline-hidden';
-	export let imageClassName = 'rounded-lg';
+	export let className = ' w-full outline-hidden z-10';
+	export let imageClassName = 'rounded-lg mx-auto scale-85 hover:scale-115 shadow-[0_-4px_10px_rgba(0,0,0,0.5)] hover:shadow-[0_-6px_15px_rgba(0,0,0,0.6)]';
 
 	let _src = '';
 	$: _src = src.startsWith('/') ? `${WEBUI_BASE_URL}${src}` : src;
+
+	let _caption = '';
+	$: _caption = alt;
 
 	let showImagePreview = false;
 </script>
 
 <button
-	class={className}
+	class={`text-center ${className}`}
 	on:click={() => {
-		showImagePreview = true;
+		showImagePreview = false;
 	}}
 	type="button"
 >
 	<img src={_src} {alt} class={imageClassName} draggable="false" data-cy="image" />
+	{@html _caption}
 </button>
 
 <ImagePreview bind:show={showImagePreview} src={_src} {alt} />
