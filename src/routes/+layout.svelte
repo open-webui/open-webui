68cf6b71aec0d87869d4d176acd3307225a9f5dd
Alessandro Pelliciari <alessandro.pelliciari@webranking.it>
2025-04-18 10:00:05 +0200

    added UserID to GTM

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -431,9 +431,28 @@
 		}
 	};
 
+	const extractUserId = (email) => {
+		const localPart = email.split('@')[0];
+		if (localPart.includes('.')) {
+			const parts = localPart.split('.');
+			return parts[0].substring(0, 3).toUpperCase() + parts[1].substring(0, 3).toUpperCase();
+		}
+		return localPart.substring(0, 6).toUpperCase();
+	};
+
 	onMount(async () => {
+		// Add user ID to dataLayer
+		user.subscribe((value) => {
+			if (value) {
+				window.dataLayer = window.dataLayer || [];
+				window.dataLayer.push({
+					'userID': extractUserId(value.email)
+				});
+			}
+		});
 		// Initialize Google Tag Manager on client-side mount
 		(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5CGLZH5J');
+		
 		
 		if (typeof window !== 'undefined' && window.applyTheme) {
 			window.applyTheme();
