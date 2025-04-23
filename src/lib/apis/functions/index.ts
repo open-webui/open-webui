fc63456e54d9fafe76021ed9370d46626cd2df81
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-04-23 20:10:10 +0200

    Update index.ts

--- src/lib/apis/functions/index.ts
+++ src/lib/apis/functions/index.ts
@@ -1,5 +1,5 @@
 import { WEBUI_API_BASE_URL } from '$lib/constants';
-import { handleApiUnauthorized } from '$lib/utils/auth'; // Assuming location
+import { handleApiUnauthorized } from '$lib/stores';
 
 export const createNewFunction = async (token: string, func: object) => {
     let error = null;
