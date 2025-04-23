0b8e573ae900b5dfbfce38a5ac99eaaec168b0c1
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-04-23 19:56:02 +0200

    Update index.ts

--- src/lib/apis/evaluations/index.ts
+++ src/lib/apis/evaluations/index.ts
@@ -1,246 +1,255 @@
 import { WEBUI_API_BASE_URL } from '$lib/constants';
+import { handleApiUnauthorized } from '$lib/utils/auth'; // Assuming location
 
 export const getConfig = async (token: string = '') => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/config`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		}
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.then((json) => {
-			return json;
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/config`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        }
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .then((json) => {
+            return json;
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res;
 };
 
 export const updateConfig = async (token: string, config: object) => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/config`, {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		},
-		body: JSON.stringify({
-			...config
-		})
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/config`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            ...config
+        })
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res;
 };
 
 export const getAllFeedbacks = async (token: string = '') => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedbacks/all`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		}
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.then((json) => {
-			return json;
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedbacks/all`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        }
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .then((json) => {
+            return json;
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res ?? []; // Ensure return is always an array
 };
 
 export const exportAllFeedbacks = async (token: string = '') => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedbacks/all/export`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		}
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.then((json) => {
-			return json;
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedbacks/all/export`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        }
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .then((json) => {
+            return json;
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res ?? []; // Ensure return is always an array
 };
 
 export const createNewFeedback = async (token: string, feedback: object) => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback`, {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		},
-		body: JSON.stringify({
-			...feedback
-		})
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            ...feedback
+        })
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res;
 };
 
 export const getFeedbackById = async (token: string, feedbackId: string) => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback/${feedbackId}`, {
-		method: 'GET',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		}
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.then((json) => {
-			return json;
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback/${feedbackId}`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        }
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .then((json) => {
+            return json;
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res;
 };
 
 export const updateFeedbackById = async (token: string, feedbackId: string, feedback: object) => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback/${feedbackId}`, {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		},
-		body: JSON.stringify({
-			...feedback
-		})
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback/${feedbackId}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            ...feedback
+        })
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res;
 };
 
 export const deleteFeedbackById = async (token: string, feedbackId: string) => {
-	let error = null;
+    let error = null;
 
-	const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback/${feedbackId}`, {
-		method: 'DELETE',
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			authorization: `Bearer ${token}`
-		}
-	})
-		.then(async (res) => {
-			if (!res.ok) throw await res.json();
-			return res.json();
-		})
-		.catch((err) => {
-			error = err.detail;
-			console.log(err);
-			return null;
-		});
+    const res = await fetch(`${WEBUI_API_BASE_URL}/evaluations/feedback/${feedbackId}`, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`
+        }
+    })
+        .then(async (res) => {
+            if (res.status === 401 && token) { handleApiUnauthorized(); }
+            if (!res.ok) throw await res.json();
+            return res.json();
+        })
+        .catch((err) => {
+            error = err.detail;
+            console.log(err);
+            return null;
+        });
 
-	if (error) {
-		throw error;
-	}
+    if (error) {
+        throw error;
+    }
 
-	return res;
+    return res;
 };
