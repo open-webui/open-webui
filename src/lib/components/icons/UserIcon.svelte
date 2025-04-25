5710716e279a03eb276739718f0dda07e2b4b55d
tetianako-prog <t.kobyliakova@gmail.com>
2025-04-25 13:46:28 +0300

    Add show password functionality

---
+++ src/lib/components/icons/UserIcon.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let className = 'size-4';
+</script>
+<svg class={className} viewBox="0 0 47 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M36.7188 36.3929C36.7188 30.0835 30.8005 24.9688 23.5 24.9688C16.1995 24.9688 10.2812 30.0835 10.2812 36.3929C10.2812 39.4707 16.5721 41.0471 23.0165 41.1222H23.9835C30.4279 41.0471 36.7188 39.4707 36.7188 36.3929Z"
+		fill="currentColor"
+	/>
+	<circle cx="23.5" cy="13.2188" r="7.34375" fill="currentColor" />
+</svg>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -15,13 +15,22 @@
 
 	import Spinner from '$lib/components/common/Spinner.svelte';
 	import OnBoarding from '$lib/components/OnBoarding.svelte';
+	import Plus from '$lib/components/icons/Plus.svelte';
+	import UserIcon from '$lib/components/icons/UserIcon.svelte';
+	import ShowPassIcon from '$lib/components/icons/ShowPassIcon.svelte';
 
 	const i18n = getContext('i18n');
 
 	let firstName = '';
 	let lastName = '';
 	let password = '';
+	let showPassword = false;
 	let confirmPassword = '';
+	let showConfirmPassword = '';
+
+	let profileImageUrl = '';
+
+	let profileImageInputElement;
 
 	let loading = false;
 
@@ -80,7 +89,102 @@
 	<form
 		class="flex flex-col self-center dark:bg-customGray-800 rounded-2xl w-[31rem] pt-8 px-24 pb-16"
 	>
-		<div class="flex-1 mb-1.5">
+		<input
+			id="profile-image-input"
+			bind:this={profileImageInputElement}
+			type="file"
+			hidden
+			accept="image/*"
+			on:change={(e) => {
+				const files = profileImageInputElement.files ?? [];
+				let reader = new FileReader();
+				reader.onload = (event) => {
+					let originalImageUrl = `${event.target.result}`;
+
+					const img = new Image();
+					img.src = originalImageUrl;
+
+					img.onload = function () {
+						const canvas = document.createElement('canvas');
+						const ctx = canvas.getContext('2d');
+
+						// Calculate the aspect ratio of the image
+						const aspectRatio = img.width / img.height;
+
+						// Calculate the new width and height to fit within 250x250
+						let newWidth, newHeight;
+						if (aspectRatio > 1) {
+							newWidth = 250 * aspectRatio;
+							newHeight = 250;
+						} else {
+							newWidth = 250;
+							newHeight = 250 / aspectRatio;
+						}
+
+						// Set the canvas size
+						canvas.width = 250;
+						canvas.height = 250;
+
+						// Calculate the position to center the image
+						const offsetX = (250 - newWidth) / 2;
+						const offsetY = (250 - newHeight) / 2;
+
+						// Draw the image on the canvas
+						ctx.drawImage(img, offsetX, offsetY, newWidth, newHeight);
+
+						// Get the base64 representation of the compressed image
+						const compressedSrc = canvas.toDataURL('image/jpeg');
+
+						// Display the compressed image
+						profileImageUrl = compressedSrc;
+
+						profileImageInputElement.files = null;
+					};
+				};
+
+				if (
+					files.length > 0 &&
+					['image/gif', 'image/webp', 'image/jpeg', 'image/png'].includes(files[0]['type'])
+				) {
+					reader.readAsDataURL(files[0]);
+				}
+			}}
+		/>
+		<div class="self-center flex justify-center flex-shrink-0">
+			<div class="self-center">
+				<button
+					class="rounded-xl flex flex-shrink-0 items-center shadow-xl group relative"
+					type="button"
+					on:click={() => {
+						profileImageInputElement.click();
+					}}
+				>
+					{#if profileImageUrl}
+						<img
+							src={profileImageUrl}
+							alt="model profile"
+							class="rounded-lg size-16 object-cover shrink-0"
+						/>
+					{:else}
+						<div class="rounded-lg size-16 shrink-0 bg-customGray-900 dark:text-customGray-800">
+							<UserIcon className="size-16"/>
+						</div>
+					{/if}
+
+					<div class="absolute bottom-0 right-0 z-10">
+						<div class="-m-2">
+							<div
+								class="p-1 rounded-lg border border-white dark:bg-customGray-900 bg-gray-800 text-white transition dark:border-customGray-700 dark:text-white"
+							>
+								<Plus className="size-3" />
+							</div>
+						</div>
+					</div>
+				</button>
+			</div>
+		</div>
+		<div class="text-xs dark:text-customGray-100/50 mb-2.5 mt-5">{$i18n.t('We only support PNGs, JPEGs and GIFs under 10MB')}</div>
+		<div class="flex-1 mb-2.5">
 			<div class="relative w-full dark:bg-customGray-900 rounded-md">
 				{#if firstName}
 					<div class="text-xs absolute left-2.5 top-1 dark:text-customGray-100/50">
@@ -95,7 +199,7 @@
 				/>
 			</div>
 		</div>
-		<div class="flex-1 mb-1.5">
+		<div class="flex-1 mb-2.5">
 			<div class="relative w-full dark:bg-customGray-900 rounded-md">
 				{#if lastName}
 					<div class="text-xs absolute left-2.5 top-1 dark:text-customGray-100/50">
@@ -117,14 +221,34 @@
 						{$i18n.t('Create Password')}
 					</div>
 				{/if}
-				<input
-					class={`px-2.5 text-sm ${password ? 'mt-2' : 'mt-0'} w-full h-10 bg-transparent dark:text-white dark:placeholder:text-customGray-100 outline-none`}
-					type="password"
-					bind:value={password}
-					placeholder={$i18n.t('Create Password')}
-					autocomplete="new-password"
-					required
-				/>
+				{#if showPassword}
+					<input
+						class={`px-2.5 text-sm ${password ? 'mt-2' : 'mt-0'} w-full h-10 bg-transparent dark:text-white dark:placeholder:text-customGray-100 outline-none pr-10`}
+						type="text"
+						bind:value={password}
+						placeholder={$i18n.t('Create Password')}
+						autocomplete="new-password"
+						required
+					/>
+				{:else}
+					<input
+						class={`px-2.5 text-sm ${password ? 'mt-2' : 'mt-0'} w-full h-10 bg-transparent dark:text-white dark:placeholder:text-customGray-100 outline-none pr-10`}
+						type="password"
+						bind:value={password}
+						placeholder={$i18n.t('Create Password')}
+						autocomplete="new-password"
+						required
+					/>
+				{/if}
+
+				<button
+					type="button"
+					class="absolute right-2.5 top-1/2 -translate-y-1/2 text-xs text-gray-500 dark:text-white"
+					on:click={() => (showPassword = !showPassword)}
+					tabindex="-1"
+				>
+					<ShowPassIcon/>
+				</button>
 			</div>
 		</div>
 		<div class="flex flex-col w-full mb-2.5">
@@ -134,14 +258,34 @@
 						{$i18n.t('Confirm password')}
 					</div>
 				{/if}
-				<input
-					class={`px-2.5 text-sm ${confirmPassword ? 'mt-2' : 'mt-0'} w-full h-10 bg-transparent dark:text-white dark:placeholder:text-customGray-100 outline-none`}
-					type="password"
-					bind:value={confirmPassword}
-					placeholder={$i18n.t('Confirm password')}
-					autocomplete="new-password"
-					required
-				/>
+				{#if showConfirmPassword}
+					<input
+						class={`px-2.5 text-sm ${confirmPassword ? 'mt-2' : 'mt-0'} w-full h-10 bg-transparent dark:text-white dark:placeholder:text-customGray-100 outline-none pr-10`}
+						type="text"
+						bind:value={confirmPassword}
+						placeholder={$i18n.t('Create Password')}
+						autocomplete="new-password"
+						required
+					/>
+				{:else}
+					<input
+						class={`px-2.5 text-sm ${confirmPassword ? 'mt-2' : 'mt-0'} w-full h-10 bg-transparent dark:text-white dark:placeholder:text-customGray-100 outline-none pr-10`}
+						type="password"
+						bind:value={confirmPassword}
+						placeholder={$i18n.t('Create Password')}
+						autocomplete="new-password"
+						required
+					/>
+				{/if}
+
+				<button
+					type="button"
+					class="absolute right-2.5 top-1/2 -translate-y-1/2 text-xs text-gray-500 dark:text-white"
+					on:click={() => (showConfirmPassword = !showConfirmPassword)}
+					tabindex="-1"
+				>
+					<ShowPassIcon/>
+				</button>
 			</div>
 		</div>
 		<button
