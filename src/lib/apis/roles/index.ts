351f1ceb93760af70d1afa2de3d6ef52feaad447
Jesper Kristensen <cableman@linuxdev.dk>
2025-04-30 21:06:06 +0200

    Adding support for new permission in UI

--- src/lib/apis/roles/index.ts
+++ src/lib/apis/roles/index.ts
@@ -142,13 +142,37 @@ export const getRolePermissions = async (token: string, roleName: string ) => {
 	return res;
 };
 
-// GET /api/v1/roles/{role_name}/permission Add New Default Permission With Role
-// {
-//   "name": "string",
-//   "category": "workspace",
-//   "description": "string",
-//   "value": false
-// }
+export const addNewPermission = async (token: string, roleName: string, permissionName: string, categoryName: string, description: string, value: boolean ) => {
+	let error = null;
+	const res = await fetch(`${WEBUI_API_BASE_URL}/roles/${roleName}/permission`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`
+		},
+		body: JSON.stringify({
+			name: permissionName,
+			category: categoryName,
+			description: description,
+			value: value
+		})
+	})
+    .then(async (res) => {
+        if (!res.ok) throw await res.json();
+        return res.json();
+    })
+    .catch((err) => {
+        console.log(err);
+        error = err.detail;
+        return null;
+    });
+
+	if (error) {
+		throw error;
+	}
+
+	return res;
+};
 
 export const linkRoleToPermissions = async (token: string, roleName: string, categoryName: string, permissionName: string, value: boolean ) => {
 	let error = null;
--- src/lib/components/common/Permissions.svelte
+++ src/lib/components/common/Permissions.svelte
@@ -3,42 +3,8 @@
 	const i18n = getContext('i18n');
 
 	import Switch from '$lib/components/common/Switch.svelte';
-	import Tooltip from '$lib/components/common/Tooltip.svelte';
 
-	// Default values for permissions (fallback)
-	const defaultPermissions = {
-		workspace: {
-			models: false,
-			knowledge: false,
-			prompts: false,
-			tools: false
-		},
-		sharing: {
-			public_models: false,
-			public_knowledge: false,
-			public_prompts: false,
-			public_tools: false
-		},
-		chat: {
-			controls: true,
-			file_upload: true,
-			delete: true,
-			edit: true,
-			stt: true,
-			tts: true,
-			call: true,
-			multiple_models: true,
-			temporary: true,
-			temporary_enforced: false
-		},
-		features: {
-			direct_tool_servers: false,
-			web_search: true,
-			image_generation: true,
-			code_interpreter: true
-		}
-	};
-
+	export let defaultPermissions = {};
 	export let permissions = {};
 
 	// Reactive statement to ensure all fields are present in `permissions`
