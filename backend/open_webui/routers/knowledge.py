220ad3723eecd15ed5ce392be6b87f42b9071cb4
Maytown <m.weber1@gmx.at>
2025-05-02 09:17:33 +0200

    Function: Adjusted create new knowledge to send rag config

--- backend/open_webui/routers/knowledge.py
+++ backend/open_webui/routers/knowledge.py
@@ -8,6 +8,7 @@ from open_webui.models.knowledge import (
     KnowledgeForm,
     KnowledgeResponse,
     KnowledgeUserResponse,
+    RAGConfigForm
 )
 from open_webui.models.files import Files, FileModel
 from open_webui.retrieval.vector.connector import VECTOR_DB_CLIENT
@@ -140,7 +141,7 @@ async def get_knowledge_list(user=Depends(get_verified_user)):
 
 @router.post("/create", response_model=Optional[KnowledgeResponse])
 async def create_new_knowledge(
-    request: Request, form_data: KnowledgeForm, user=Depends(get_verified_user)
+    request: Request, form_data: KnowledgeForm, rag_data: RAGConfigForm, user=Depends(get_verified_user)
 ):
     if user.role != "admin" and not has_permission(
         user.id, "workspace.knowledge", request.app.state.config.USER_PERMISSIONS
@@ -150,7 +151,7 @@ async def create_new_knowledge(
             detail=ERROR_MESSAGES.UNAUTHORIZED,
         )
 
-    knowledge = Knowledges.insert_new_knowledge(user.id, form_data)
+    knowledge = Knowledges.insert_new_knowledge(user.id, form_data, rag_data)
 
     if knowledge:
         return knowledge
