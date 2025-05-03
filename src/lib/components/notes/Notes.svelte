52d32e8bf222593918b6c707f13897ba23889917
Timothy Jaeryang Baek <tim@openwebui.com>
2025-05-03 21:02:21 +0400

    refac: styling

--- src/lib/components/notes/Notes.svelte
+++ src/lib/components/notes/Notes.svelte
@@ -162,7 +162,7 @@
 		<div class="flex gap-0.5 justify-end w-full">
 			<Tooltip content={$i18n.t('Create Note')}>
 				<button
-					class="cursor-pointer p-2.5 flex rounded-full bg-gray-50 dark:bg-gray-850 hover:bg-gray-100 dark:hover:bg-gray-800 transition shadow-xl"
+					class="cursor-pointer p-2.5 flex rounded-full border border-gray-50 dark:border-none dark:bg-gray-850 hover:bg-gray-50 dark:hover:bg-gray-800 transition shadow-xl"
 					type="button"
 					on:click={async () => {
 						createNoteHandler();
