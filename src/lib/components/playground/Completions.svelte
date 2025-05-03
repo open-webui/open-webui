5a96072b54df50f22384bf287be9cf12f667ce93
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-05-04 01:06:09 +0200

    Update Completions.svelte

--- src/lib/components/playground/Completions.svelte
+++ src/lib/components/playground/Completions.svelte
@@ -105,7 +105,7 @@
 	};
 
 	onMount(async () => {
-		if ($user?.role !== 'admin') {
+		if (!$user?.role === 'admin' || !$user?.permissions?.features?.playground_access) {
 			await goto('/');
 		}
 
