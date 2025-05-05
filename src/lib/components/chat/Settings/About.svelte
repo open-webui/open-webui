8371a98e6f9bc2e2df51ec67368aab1e13b19ef3
Johnny Zhang <Evolutionai@163.com>
2025-05-05 20:03:56 +0800

    更新About

--- src/lib/components/chat/Settings/About.svelte
+++ src/lib/components/chat/Settings/About.svelte
@@ -18,6 +18,8 @@
 		latest: ''
 	};
 
+	const currentYear = new Date().getFullYear();
+
 	const checkForVersionUpdates = async () => {
 		updateAvailable = null;
 		version = await getVersionUpdates(localStorage.token).catch((error) => {
@@ -108,8 +110,18 @@
 
 		{#if $config?.license_metadata}
 			<div class="mb-2 text-xs">
-				{#if !$WEBUI_NAME.includes('Open WebUI')}
-					<span class=" text-gray-500 dark:text-gray-300 font-medium">{$WEBUI_NAME}</span> -
+				{#if !$WEBUI_NAME.includes('Evolution')}
+					{$i18n.t('This is a fork of')}
+
+					{#if $WEBUI_NAME === 'Ollama'}
+						<a
+							href="https://ollama.com"
+							target="_blank"
+							class="text-gray-500 dark:text-gray-300 font-medium"
+						>Ollama</a>
+					{:else}
+							<span class="text-gray-500 dark:text-gray-300 font-medium">{$WEBUI_NAME}</span> -
+				{/if}
 				{/if}
 
 				<span class=" capitalize">{$config?.license_metadata?.type}</span> license purchased by
@@ -117,24 +129,16 @@
 			</div>
 		{:else}
 			<div class="flex space-x-1">
-				<a href="https://discord.gg/5rJgQTnV4s" target="_blank">
-					<img
-						alt="Discord"
-						src="https://img.shields.io/badge/Discord-Open_WebUI-blue?logo=discord&logoColor=white"
-					/>
-				</a>
-
-				<a href="https://twitter.com/OpenWebUI" target="_blank">
+				<a href="https://x.com/BIGLao666" target="_blank">
 					<img
 						alt="X (formerly Twitter) Follow"
-						src="https://img.shields.io/twitter/follow/OpenWebUI"
+						src="https://img.shields.io/badge/twitter-Evolution-follow"
 					/>
 				</a>
-
-				<a href="https://github.com/open-webui/open-webui" target="_blank">
+				<a href="http://weixin.qq.com/r/mp/gha7o2LELT9SrSbA90OQ" target="_blank">
 					<img
-						alt="Github Repo"
-						src="https://img.shields.io/github/stars/open-webui/open-webui?style=social&label=Star us on Github"
+						alt="WeChat"
+						src="https://img.shields.io/badge/WeChat-Evolution-brightgreen"
 					/>
 				</a>
 			</div>
@@ -146,49 +150,31 @@
 			<a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC-BY 4.0</a>.
 		</div>
 
-		<div>
-			<pre
-				class="text-xs text-gray-400 dark:text-gray-500">Copyright (c) {new Date().getFullYear()} <a
-					href="https://openwebui.com"
-					target="_blank"
-					class="underline">Open WebUI (Timothy Jaeryang Baek)</a
-				>
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-</pre>
-		</div>
-
-		<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
-			{$i18n.t('Created by')}
+		<div class="text-xs text-gray-400 dark:text-gray-500 mt-2">
+			Evolution is built on top of open WebUI.<br />
+			Copyright (c) {currentYear}
 			<a
-				class=" text-gray-500 dark:text-gray-300 font-medium"
-				href="https://github.com/tjbck"
-				target="_blank">Timothy J. Baek</a
+				href="https://zhangbusan.notion.site/EvolutionAI-1cdf938ebd6e807b884fe76042addb51"
+				target="_blank"
+				class="underline"
 			>
+				Evolution (Johnny Zhang)
+			</a>
+			<br />
+			Copyright (c) {currentYear}
+			<a
+				href="https://openwebui.com"
+				target="_blank"
+				class="underline"
+			>
+				Open WebUI (Timothy Jaeryang Baek)
+			</a>
+			<br />
+			All rights reserved.<br /><br />
+			Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:<br />
+			1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.<br />
+			2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.<br />
+			3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 		</div>
 	</div>
 </div>
