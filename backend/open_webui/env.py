666946ab4d66b1f734789eb443fec97cfd338fbb
Johnny Zhang <Evolutionai@163.com>
2025-05-05 15:10:49 +0800

    go

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -105,11 +105,9 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = "EvolutionAI"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "/static/Daylgo.png"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
