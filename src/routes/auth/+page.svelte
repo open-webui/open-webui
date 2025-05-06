b0b93dc98d78b61bbbf4b4f7baba83573a154214
Sine Jespersen <sinejespersen@gmail.com>
2025-05-06 09:04:37 +0200

    change divs to labels, as this is what they semantically are

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -248,10 +248,11 @@
 								<div class="flex flex-col mt-4">
 									{#if mode === 'signup'}
 										<div class="mb-2">
-											<div class=" text-sm font-medium text-left mb-1">{$i18n.t('Name')}</div>
+											<label for="name" class="text-sm font-medium text-left mb-1 block">{$i18n.t('Name')}</label>
 											<input
 												bind:value={name}
 												type="text"
+												id="name"
 												class="my-0.5 w-full text-sm outline-hidden bg-transparent"
 												autocomplete="name"
 												placeholder={$i18n.t('Enter Your Full Name')}
@@ -262,23 +263,25 @@
 
 									{#if mode === 'ldap'}
 										<div class="mb-2">
-											<div class=" text-sm font-medium text-left mb-1">{$i18n.t('Username')}</div>
+											<label for="username" class="text-sm font-medium text-left mb-1 block">{$i18n.t('Username')}</label>
 											<input
 												bind:value={ldapUsername}
 												type="text"
 												class="my-0.5 w-full text-sm outline-hidden bg-transparent"
 												autocomplete="username"
 												name="username"
+												id="username"
 												placeholder={$i18n.t('Enter Your Username')}
 												required
 											/>
 										</div>
 									{:else}
 										<div class="mb-2">
-											<div class=" text-sm font-medium text-left mb-1">{$i18n.t('Email')}</div>
+											<label for="email" class="text-sm font-medium text-left mb-1 block">{$i18n.t('Email')}</label>
 											<input
 												bind:value={email}
 												type="email"
+												id="email"
 												class="my-0.5 w-full text-sm outline-hidden bg-transparent"
 												autocomplete="email"
 												name="email"
@@ -289,11 +292,11 @@
 									{/if}
 
 									<div>
-										<div class=" text-sm font-medium text-left mb-1">{$i18n.t('Password')}</div>
-
+										<label for="password" class="text-sm font-medium text-left mb-1 block">{$i18n.t('Password')}</label>
 										<input
 											bind:value={password}
 											type="password"
+											id="password"
 											class="my-0.5 w-full text-sm outline-hidden bg-transparent"
 											placeholder={$i18n.t('Enter Your Password')}
 											autocomplete="current-password"
