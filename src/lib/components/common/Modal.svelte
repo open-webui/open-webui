54b30e0b7fb1ede93420c65a39332f5c9684d8f9
Sine Jespersen <sinejespersen@gmail.com>
2025-05-07 14:21:56 +0200

    use svelte-focus-trap in modal

--- src/lib/components/common/Modal.svelte
+++ src/lib/components/common/Modal.svelte
@@ -3,7 +3,7 @@
 	import { fade } from 'svelte/transition';
 
 	import { flyAndScale } from '$lib/utils/transitions';
-
+	import { focusTrap } from 'svelte-focus-trap'
 	export let show = true;
 	export let size = 'md';
 	export let containerClassName = 'p-3';
@@ -66,6 +66,7 @@
 	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<div
 		bind:this={modalElement}
+		use:focusTrap
 		class="modal fixed top-0 right-0 left-0 bottom-0 bg-black/60 w-full h-screen max-h-[100dvh] {containerClassName} flex justify-center z-9999 overflow-y-auto overscroll-contain"
 		in:fade={{ duration: 10 }}
 		on:mousedown={() => {
