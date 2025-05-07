6d2efbd98dca8b2a4e97227566875de8097a047c
Thomas Lehmann <t.lehmann@strato.de>
2025-05-07 15:33:07 +0200

    c/feat: add LoadingCover component for use in dialog
    Include in dialogs to cover the whole content.
    
    Refs: PRODAI-189

---
+++ src/lib/IONOS/components/common/LoadingCover.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+   import Spinner from '$lib/IONOS/components/icons/Spinner.svelte';
+</script>
+
+<div class="absolute left-0 top-0 right-0 bottom-0 bg-white/75 flex justify-center items-center rounded-xl ">
+	<Spinner size="56" />
+</div>
+
