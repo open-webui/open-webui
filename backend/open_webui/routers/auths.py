a904f874ee297402b694013620fd32b6825bc19d
Timothy Jaeryang Baek <tim@openwebui.com>
2025-05-12 20:03:40 +0400

    fix: admin signup logic

--- backend/open_webui/routers/auths.py
+++ backend/open_webui/routers/auths.py
@@ -478,10 +478,6 @@ async def signup(request: Request, response: Response, form_data: SignupForm):
             "admin" if user_count == 0 else request.app.state.config.DEFAULT_USER_ROLE
         )
 
-        if user_count == 0:
-            # Disable signup after the first user is created
-            request.app.state.config.ENABLE_SIGNUP = False
-
         # The password passed to bcrypt must be 72 bytes or fewer. If it is longer, it will be truncated before hashing.
         if len(form_data.password.encode("utf-8")) > 72:
             raise HTTPException(
@@ -540,6 +536,10 @@ async def signup(request: Request, response: Response, form_data: SignupForm):
             user_permissions = get_permissions(
                 user.id, request.app.state.config.USER_PERMISSIONS
             )
+
+            if user_count == 0:
+                # Disable signup after the first user is created
+                request.app.state.config.ENABLE_SIGNUP = False
 
             return {
                 "token": token,
