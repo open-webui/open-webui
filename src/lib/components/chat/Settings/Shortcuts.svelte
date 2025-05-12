2c520b8fdbdeea057bca18989e945ce23fea06f9
edmcquinn <73904683+edmcquinn@users.noreply.github.com>
2025-05-12 10:44:09 -0400

    Added + svg

--- src/lib/components/chat/Settings/Shortcuts.svelte
+++ src/lib/components/chat/Settings/Shortcuts.svelte
@@ -311,6 +311,13 @@
                                     class="p-1 rounded hover:bg-gray-200 dark:hover:bg-gray-600 transition-colors text-green-500 mr-1 mb-1 flex items-center justify-center w-7 h-7 border border-gray-300 dark:border-gray-600"
                                     aria-label={`Add third key for ${$i18n.t(shortcut.labelKey)}`}
                                 >
+                                
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
+
+<path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+
+</svg>
                                 </button>
                             {/if}
                              <button
