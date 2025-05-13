dfd4037132cfdb8bf4c2b942658ca5f78a9efa25
Jan Kessler <jakessle@uni-mainz.de>
2025-05-13 07:48:29 +0200

    better handling of frontend updates, following svelte docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import * as child_process from 'node:child_process';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +15,12 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: 'index.html'
-		})
+		}),
+		// poll for new version name every 60 seconds (to trigger reload mechanic in +layout.svelte)
+		version: {
+			name: child_process.execSync('git rev-parse HEAD').toString().trim(),
+			pollInterval: 60000
+		}
 	},
 	vitePlugin: {
 		// inspector: {
