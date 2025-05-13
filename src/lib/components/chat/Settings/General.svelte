7041002fe3aae9301b385026daec560c92307104
imrandevofficial <official@imrandubai.com>
2025-05-13 17:27:12 +0530

    Update General.svelte

--- src/lib/components/chat/Settings/General.svelte
+++ src/lib/components/chat/Settings/General.svelte
@@ -279,10 +279,10 @@
 					Couldn't find your language?
 					<a
 						class=" text-gray-300 font-medium underline"
-						href="https://github.com/open-webui/open-webui/blob/main/docs/CONTRIBUTING.md#-translations-and-internationalization"
+						href="#"
 						target="_blank"
 					>
-						Help us translate Open WebUI!
+						Help us translate GSM!
 					</a>
 				</div>
 			{/if}
