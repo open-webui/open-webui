8b194bc781d81f0e90b77a6c28727a887e9c1d05
Jesper Kristensen <cableman@linuxdev.dk>
2025-05-14 14:53:36 +0200

    Added missing "enable_direct_connections" to config type

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -213,6 +213,7 @@ type Config = {
 		enable_admin_chat_access: boolean;
 		enable_community_sharing: boolean;
 		enable_autocomplete_generation: boolean;
+		enable_direct_connections: boolean;
 	};
 	oauth: {
 		providers: {
