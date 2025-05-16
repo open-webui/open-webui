08e4c163ea99d5310f50da6959b0d694a4e441c8
Timothy Jaeryang Baek <tim@openwebui.com>
2025-05-17 01:47:48 +0400

    feat: local/external connections

--- backend/open_webui/utils/models.py
+++ backend/open_webui/utils/models.py
@@ -49,6 +49,7 @@ async def get_all_base_models(request: Request, user: UserModel = None):
                 "created": int(time.time()),
                 "owned_by": "ollama",
                 "ollama": model,
+                "connection_type": model.get("connection_type", "local"),
                 "tags": model.get("tags", []),
             }
             for model in ollama_models["models"]
