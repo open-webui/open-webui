758b71666fb976f55b457fdbcb2d2fb0a3e5b17a
Kai Henseler <kai.henseler@strato.de>
2025-05-19 14:30:41 +0200

    c/fix(explore): privacy & user menu not showing
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- src/routes/explore/+layout.svelte
+++ src/routes/explore/+layout.svelte
@@ -4,6 +4,7 @@
 	import Footer from '$lib/IONOS/components/Footer.svelte';
 	import PrivacySlogan from '$lib/IONOS/components/PrivacySlogan.svelte';
 	import Sidebar from '$lib/components/layout/Sidebar.svelte';
+	import FilledUserAvatar from '$lib/IONOS/components/icons/FilledUserAvatar.svelte';
 
 	import {
 		user,
@@ -11,14 +12,14 @@
 	} from '$lib/stores';
 </script>
 
-<div class="flex flex-row w-full justify-between">
+<div class="flex flex-row w-full justify-between bg-gray-100">
 	{#if $user !== undefined}
 		<Sidebar />
 	{/if}
-	<div class="{($user !== undefined) ? ($showSidebar ? 'max-w-[calc(100%-260px)]' : 'max-w-[calc(100%-60px)] ml-[60px]') : 'max-w-full' } h-[100vh] overflow-scroll">
-		<nav class="fixed z-30 w-full px-1.5 py-4 -mb-8 flex items-center drag-region">
+	<div class="h-[100vh] overflow-scroll">
+		<nav class="fixed z-30 {($user !== undefined) ? ($showSidebar ? 'w-[calc(100%-260px)]' : 'w-[calc(100%-60px)] ml-[60px]') : 'w-full' } px-1.5 py-4 -mb-8 pt-10 flex items-center drag-region transition-[width]">
 			<div
-				class=" bg-gradient-to-b via-60% from-white via-white to-transparent dark:from-gray-900 dark:via-gray-900 dark:to-transparent pointer-events-none absolute inset-0 -bottom-10 -top-6 z-[-1] blur"
+				class=" bg-gradient-to-b from-gray-100 from-30% to-transparent dark:from-gray-900 dark:via-gray-900 dark:to-transparent pointer-events-none absolute inset-0 -bottom-10 -top-6 z-[-1] blur"
 			></div>
 
 			<div class=" flex w-full mx-auto px-1 pt-0.5 bg-transparent">
@@ -33,7 +34,7 @@
 
 					<PrivacySlogan />
 
-					<div class="self-start flex flex-none items-center text-gray-600 dark:text-gray-400">
+					<div class="self-center flex flex-none items-center text-gray-600 dark:text-gray-400">
 						{#if $user !== undefined}
 							<UserMenu
 								className="max-w-[200px]"
@@ -43,13 +44,8 @@
 									class="select-none flex rounded-xl p-1.5 w-full hover:bg-gray-50 dark:hover:bg-gray-850 transition"
 									aria-label="User Menu"
 								>
-									<div class=" self-center">
-										<img
-											src={$user?.profile_image_url}
-											class="size-6 object-cover rounded-full"
-											alt="User profile"
-											draggable="false"
-										/>
+									<div class="text-blue-800 self-center">
+										<FilledUserAvatar />
 									</div>
 								</button>
 							</UserMenu>
@@ -58,7 +54,7 @@
 				</div>
 			</div>
 		</nav>
-		<div class="flex flex-col pt-20">
+		<div class="flex flex-col pt-20 mt-10">
 			<div class="grow">
 				<slot />
 			</div>
