36920211b5a865a6cf7da522dc62dbf65d6da69c
Thomas Lehmann <t.lehmann@strato.de>
2025-05-27 13:15:03 +0200

    fix(Link): render links without href as buttons
    In the LoginRegisterOverlay links are used with the on:click handler
    only.
    
    This fixes an issue where clicking the link in non-Chrome browsers
    opened a new tab with the current page and loaded the actual target
    in the origin tab.
    
    Refs: PRODAI-313

--- src/lib/IONOS/components/common/Link.svelte
+++ src/lib/IONOS/components/common/Link.svelte
@@ -5,17 +5,28 @@
 	export let passive: boolean = false;
 	export let href: string = '';
 	export let className: string = '';
+
+	let classes = `${(passive
+		? "underline hover:text-blue-400 active:text-blue-400 disabled:text-gray-450"
+		: "text-blue-700 hover:text-blue-400 active:text-blue-400 hover:underline visited:text-blue-700 disabled:text-gray-450")} ${className}`;
 </script>
 
-<a
-	on:click
-	{...$$restProps}
-	{href}
-	class={`${(passive
-		? "underline hover:text-blue-400 active:text-blue-400 disabled:text-gray-450"
-		: "text-blue-700 hover:text-blue-400 active:text-blue-400 hover:underline visited:text-blue-700 disabled:text-gray-450")} ${className}`}
-	target="_blank"
-	rel="noreferrer"
->
-	<slot />
-</a>
+{#if href !== ''}
+	<a
+		{...$$restProps}
+		{href}
+		class={classes}
+		target="_blank"
+		rel="noreferrer"
+	>
+		<slot />
+	</a>
+{:else}
+	<button
+		on:click|preventDefault
+		{...$$restProps}
+		class={classes}
+	>
+		<slot />
+	</button>
+{/if}
