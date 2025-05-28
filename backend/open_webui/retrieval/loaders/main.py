7dc7d5c028a662c618f09c50e01ccf5b1833a77f
Timothy Jaeryang Baek <tim@openwebui.com>
2025-05-29 03:47:02 +0400

    refac: PLEASE FOLLOW EXISTING CONVENTION

--- backend/open_webui/retrieval/loaders/main.py
+++ backend/open_webui/retrieval/loaders/main.py
@@ -21,9 +21,11 @@ from langchain_community.document_loaders import (
 )
 from langchain_core.documents import Document
 
-from open_webui.retrieval.loaders.datalab_marker_loader import DatalabMarkerLoader
 from open_webui.retrieval.loaders.external_document import ExternalDocumentLoader
+
 from open_webui.retrieval.loaders.mistral import MistralLoader
+from open_webui.retrieval.loaders.datalab_marker import DatalabMarkerLoader
+
 
 from open_webui.env import SRC_LOG_LEVELS, GLOBAL_LOG_LEVEL
 
