8a74bdce378062015575f74cdc1db203c41e9ba3
Timothy Jaeryang Baek <tim@openwebui.com>
2025-05-28 15:36:04 +0400

    fix: message input issue

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -582,14 +582,6 @@
 			});
 		}
 	});
-
-	let screenReaderDiv: HTMLDivElement;
-
-	$: if (message.done) {
-		if (screenReaderDiv) {
-			screenReaderDiv.textContent = message.content;
-		}
-	}
 </script>
 
 <DeleteConfirmDialog
@@ -599,10 +591,6 @@
 		deleteMessageHandler();
 	}}
 />
-
-<div bind:this={screenReaderDiv} aria-live="polite" class="sr-only">
-	{message.done ? message.content : ''}
-</div>
 
 {#key message.id}
 	<div
@@ -618,7 +606,7 @@
 			/>
 		</div>
 
-		<div class="flex-auto w-0 pl-1">
+		<div class="flex-auto w-0 pl-1 relative">
 			<Name>
 				<Tooltip content={model?.name ?? message.model} placement="top-start">
 					<span class="line-clamp-1 text-black dark:text-white">
@@ -1439,6 +1427,12 @@
 					{/if}
 				{/if}
 			</div>
+
+			{#if message?.done}
+				<div aria-live="polite" class="sr-only">
+					{message?.content ?? ''}
+				</div>
+			{/if}
 		</div>
 	</div>
 {/key}
