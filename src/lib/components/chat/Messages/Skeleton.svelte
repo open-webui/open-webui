0d6537e0b67b23513e407770ac7fcf0a1285be30
shadowfax <theshadowfaxproject@gmail.com>
2025-05-30 20:54:54 -0700

    open-webui-68 Change AI-response-wait animation from default to something else.

--- src/lib/components/chat/Messages/Skeleton.svelte
+++ src/lib/components/chat/Messages/Skeleton.svelte
@@ -1,45 +1,17 @@
 <script lang="ts">
-	export let size = 'md';
+ 	import { WEBUI_BASE_URL } from '$lib/constants';
+ 	const metaThemeColorTag = document.querySelector('meta[name="theme-color"]');
+ 	let SRC = `${WEBUI_BASE_URL}/static/chat-waiting-mb-dark.gif`;
+ 	if (metaThemeColorTag?.content) {
+ 		if (metaThemeColorTag.content === '#ffffff') {
+ 			SRC = `${WEBUI_BASE_URL}/static/chat-waiting-mb-light.gif`;
+ 		}
+ 	}
 </script>
 
-<div class="w-full mt-2 mb-2">
-	<div class="animate-pulse flex w-full">
-		<div class="{size === 'md' ? 'space-y-2' : 'space-y-1.5'} w-full">
-			<div
-				class="{size === 'md' ? 'h-2' : 'h-1.5'} bg-gray-200 dark:bg-gray-600 rounded-sm mr-14"
-			/>
-
-			<div class="grid grid-cols-3 gap-4">
-				<div
-					class="{size === 'md'
-						? 'h-2'
-						: 'h-1.5'} bg-gray-200 dark:bg-gray-600 rounded-sm col-span-2"
-				/>
-				<div
-					class="{size === 'md'
-						? 'h-2'
-						: 'h-1.5'} bg-gray-200 dark:bg-gray-600 rounded-sm col-span-1"
-				/>
-			</div>
-			<div class="grid grid-cols-4 gap-4">
-				<div
-					class="{size === 'md'
-						? 'h-2'
-						: 'h-1.5'} bg-gray-200 dark:bg-gray-600 rounded-sm col-span-1"
-				/>
-				<div
-					class="{size === 'md'
-						? 'h-2'
-						: 'h-1.5'} bg-gray-200 dark:bg-gray-600 rounded-sm col-span-2"
-				/>
-				<div
-					class="{size === 'md'
-						? 'h-2'
-						: 'h-1.5'} bg-gray-200 dark:bg-gray-600 rounded-sm col-span-1 mr-4"
-				/>
-			</div>
-
-			<div class="{size === 'md' ? 'h-2' : 'h-1.5'} bg-gray-200 dark:bg-gray-600 rounded-sm" />
-		</div>
-	</div>
-</div>
+ <img
+ 	crossorigin="anonymous"
+ 	src="{SRC}"
+ 	class="w-20 h-10"
+ 	alt="logo"
+ />
