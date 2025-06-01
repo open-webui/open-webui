349e84088019b9f447816cfd1e4e0366d70aac84
shadowfax <theshadowfaxproject@gmail.com>
2025-06-01 13:14:03 -0700

    open-webui-65 Customize Image compoentn

--- src/lib/components/common/Image.svelte
+++ src/lib/components/common/Image.svelte
@@ -5,51 +5,27 @@
 	export let src = '';
 	export let alt = '';
 
-	export let className = ' w-full outline-hidden focus:outline-hidden';
-	export let imageClassName = 'rounded-lg';
-
-	export let dismissible = false;
-	export let onDismiss = () => {};
+	export let className = ' w-full outline-hidden z-10';
+	export let imageClassName = 'rounded-lg mx-auto scale-85 hover:scale-115 shadow-[0_-4px_10px_rgba(0,0,0,0.5)] hover:shadow-[0_-6px_15px_rgba(0,0,0,0.6)]';
 
 	let _src = '';
 	$: _src = src.startsWith('/') ? `${WEBUI_BASE_URL}${src}` : src;
 
+	let _caption = '';
+	$: _caption = alt;
+
 	let showImagePreview = false;
 </script>
 
+<button
+	class={`text-center ${className}`}
+	on:click={() => {
+		showImagePreview = false;
+	}}
+	type="button"
+>
+	<img src={_src} {alt} class={imageClassName} draggable="false" data-cy="image" />
+	{@html _caption}
+</button>
+
 <ImagePreview bind:show={showImagePreview} src={_src} {alt} />
-
-<div class=" relative group w-fit">
-	<button
-		class={className}
-		on:click={() => {
-			showImagePreview = true;
-		}}
-		type="button"
-	>
-		<img src={_src} {alt} class={imageClassName} draggable="false" data-cy="image" />
-	</button>
-
-	{#if dismissible}
-		<div class=" absolute -top-1 -right-1">
-			<button
-				class=" bg-white text-black border border-white rounded-full group-hover:visible invisible transition"
-				type="button"
-				on:click={() => {
-					onDismiss();
-				}}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="size-4"
-				>
-					<path
-						d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
-					/>
-				</svg>
-			</button>
-		</div>
-	{/if}
-</div>
