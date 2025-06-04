46064807e0dfbe9b405826761f2cf8a5c238d6ed
weberm1 <martin.weber1@oenb.at>
2025-06-04 08:28:51 +0200

    fix: removed embedding downloadbutton for initial knowledgebase creation - embedding gets initialized after knowledge creation

--- src/lib/components/workspace/Knowledge/CreateKnowledgeBase.svelte
+++ src/lib/components/workspace/Knowledge/CreateKnowledgeBase.svelte
@@ -567,62 +567,6 @@
                                             bind:value={embeddingModel}
                                         />
                                     </div>
-
-                                    {#if embeddingEngine === ''}
-                                        <button
-                                            class="px-2.5 bg-transparent text-gray-800 dark:bg-transparent dark:text-gray-100 rounded-lg transition"
-                                            on:click={() => {
-                                                embeddingModelUpdateHandler();
-                                            }}
-                                            disabled={updateEmbeddingModelLoading}
-                                        >
-                                            {#if updateEmbeddingModelLoading}
-                                                <div class="self-center">
-                                                    <svg
-                                                        class=" w-4 h-4"
-                                                        viewBox="0 0 24 24"
-                                                        fill="currentColor"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                    >
-                                                        <style>
-                                                            .spinner_ajPY {
-                                                                transform-origin: center;
-                                                                animation: spinner_AtaB 0.75s infinite linear;
-                                                            }
-
-                                                            @keyframes spinner_AtaB {
-                                                                100% {
-                                                                    transform: rotate(360deg);
-                                                                }
-                                                            }
-                                                        </style>
-                                                        <path
-                                                            d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
-                                                            opacity=".25"
-                                                        />
-                                                        <path
-                                                            d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
-                                                            class="spinner_ajPY"
-                                                        />
-                                                    </svg>
-                                                </div>
-                                            {:else}
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 16 16"
-                                                    fill="currentColor"
-                                                    class="w-4 h-4"
-                                                >
-                                                    <path
-                                                        d="M8.75 2.75a.75.75 0 0 0-1.5 0v5.69L5.03 6.22a.75.75 0 0 0-1.06 1.06l3.5 3.5a.75.75 0 0 0 1.06 0l3.5-3.5a.75.75 0 0 0-1.06-1.06L8.75 8.44V2.75Z"
-                                                    />
-                                                    <path
-                                                        d="M3.5 9.75a.75.75 0 0 0-1.5 0v1.5A2.75 2.75 0 0 0 4.75 14h6.5A2.75 2.75 0 0 0 14 11.25v-1.5a.75.75 0 0 0-1.5 0v1.5c0 .69-.56 1.25-1.25 1.25h-6.5c-.69 0-1.25-.56-1.25-1.25v-1.5Z"
-                                                    />
-                                                </svg>
-                                            {/if}
-                                        </button>
-                                    {/if}
                                 </div>
                             {/if}
                         </div>
