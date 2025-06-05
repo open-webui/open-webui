898e56c5f660ce5049f820e2e83756b9a37b5d70
Vaclav Cerny <va.cerny@t-mobile.cz>
2025-06-05 21:38:14 +0200

    chore: remove unused log level variables from dev script

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,7 +1,2 @@
 PORT="${PORT:-8080}"
-
-RAG_LOG_LEVEL=DEBUG
-MAIN_LOG_LEVEL=DEBUG
-CONFIG_LOG_LEVEL=DEBUG
-
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
