94dcffeafa5bf381d6ad6e792efe88e6a6774d74
Thomas Lehmann <t.lehmann@strato.de>
2025-06-11 11:39:16 +0200

    c/chore(account): fix type issue
    Declare async function return value as typed Promise

--- src/lib/IONOS/services/account.ts
+++ src/lib/IONOS/services/account.ts
@@ -3,12 +3,12 @@ import { config } from '$lib/stores';
 import { deleteAccount as deleteAccountApi } from '$lib/IONOS/apis/users';
 import { signout } from '$lib/services/auths';
 
-export const resetPassword = async (): void => {
+export const resetPassword = async (): Promise<void> => {
 	const passwordResetUrl = get(config)?.features?.ionos_password_reset_url ?? null;
 	await signout(passwordResetUrl);
 };
 
-export const deleteAccount = async (): void => {
+export const deleteAccount = async (): Promise<void> => {
 	const token = localStorage.token;
 	await deleteAccountApi(token);
 	await signout();
