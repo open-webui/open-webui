c829ce43c190346fda84fa83c3f6478e8d517162
Thomas Lehmann <t.lehmann@strato.de>
2025-06-11 09:08:35 +0200

    c/feat(Button): add "interactive" prop
    Allo buttons to be non-interactive (non-button elements) to use them
    within button elements. Apparent buttons within button elements occure
    in the agent selector cards.

--- src/lib/IONOS/components/common/Button.svelte
+++ src/lib/IONOS/components/common/Button.svelte
@@ -15,21 +15,37 @@
 	export let pressed = false;
 	export let pressable = false;
 	export let iconOnly = false;
+	/**
+	 * Non-interactive buttons have no button type/role, but just appear as buttons
+	 */
+	export let interactive = true;
 
 	const baseClasses = 'select-none font-semibold text-sm outline-offset-2';
 
 	$: classes = `${className} ${iconOnly ? 'p-1.5' : 'px-4 py-1'} ${baseClasses} ${stateClassBuilder(type, disabled, pressed)}`;
 </script>
 
-<button
-	on:click
-	{id}
-	{name}
-	data-button-type={type}
-	aria-pressed={pressable && pressed ? 'true' : (pressable ? 'false' : undefined)}
-	aria-label={label}
-	{disabled}
-	class={classes}
->
-	<slot />
-</button>
+{#if interactive}
+	<button
+		on:click
+		{id}
+		{name}
+		data-button-type={type}
+		aria-pressed={pressable && pressed ? 'true' : (pressable ? 'false' : undefined)}
+		aria-label={label}
+		{disabled}
+		class={classes}
+	>
+		<slot />
+	</button>
+{:else}
+	<div
+		{id}
+		data-button-type={type}
+		aria-pressed={pressable && pressed ? 'true' : (pressable ? 'false' : undefined)}
+		aria-label={label}
+		class={classes}
+	>
+		<slot />
+	</div>
+{/if}
