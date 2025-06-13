843810cc9f6235c02af1cbb1cd989f13ac0fd2dc
Sine Jespersen <sinejespersen@gmail.com>
2025-06-13 11:58:58 +0200

    add aria hidden to decorative svg

--- src/lib/components/AddConnectionModal.svelte
+++ src/lib/components/AddConnectionModal.svelte
@@ -285,6 +285,7 @@
 										xmlns="http://www.w3.org/2000/svg"
 										viewBox="0 0 20 20"
 										fill="currentColor"
+										aria-hidden="true"
 										class="w-4 h-4"
 									>
 										<path
