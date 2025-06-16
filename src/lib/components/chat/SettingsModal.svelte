b748d5edee80a7afbb50456f9debaf29b38eb6e5
Timothy Jaeryang Baek <tim@openwebui.com>
2025-06-16 12:42:16 +0400

    fix: direct tool servers permissions

--- src/lib/components/chat/SettingsModal.svelte
+++ src/lib/components/chat/SettingsModal.svelte
@@ -211,7 +211,9 @@
 			: []),
 
 		...($user?.role === 'admin' ||
-		($user?.role === 'user' && $user?.permissions?.features?.direct_tool_servers)
+		($user?.role === 'user' &&
+			$user?.permissions?.features?.direct_tool_servers &&
+			$config?.features?.direct_tool_servers)
 			? [
 					{
 						id: 'tools',
@@ -686,7 +688,7 @@
 								</button>
 							{/if}
 						{:else if tabId === 'tools'}
-							{#if $user?.role === 'admin' || ($user?.role === 'user' && $user?.permissions?.features?.direct_tool_servers)}
+							{#if $user?.role === 'admin' || ($user?.role === 'user' && $user?.permissions?.features?.direct_tool_servers && $config?.features?.direct_tool_servers)}
 								<button
 									role="tab"
 									aria-controls="tab-tools"
