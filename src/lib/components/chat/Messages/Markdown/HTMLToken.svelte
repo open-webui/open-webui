b082a7edbe9f33632c5959928fbd3aa21c5817be
Timothy Jaeryang Baek <tim@openwebui.com>
2025-06-16 17:43:17 +0400

    enh: allow iframe in content

--- src/lib/components/chat/Messages/Markdown/HTMLToken.svelte
+++ src/lib/components/chat/Messages/Markdown/HTMLToken.svelte
@@ -69,6 +69,21 @@
 			>
 			</iframe>
 		{/if}
+	{:else if token.text && token.text.includes('<iframe')}
+		{@const match = token.text.match(/<iframe\s+[^>]*src="([^"]+)"[^>]*><\/iframe>/)}
+		{@const iframeSrc = match && match[1]}
+		{#if iframeSrc}
+			<iframe
+				class="w-full my-2"
+				src={iframeSrc}
+				title="Embedded content"
+				frameborder="0"
+				sandbox
+				onload="this.style.height=(this.contentWindow.document.body.scrollHeight+20)+'px';"
+			></iframe>
+		{:else}
+			{token.text}
+		{/if}
 	{:else if token.text.includes(`<file type="html"`)}
 		{@const match = token.text.match(/<file type="html" id="([^"]+)"/)}
 		{@const fileId = match && match[1]}
