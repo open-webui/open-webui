1e7b284c93e0b0ba77dec20a2fd3bda6c62fb796
tetianako-prog <t.kobyliakova@gmail.com>
2025-06-16 15:33:49 +0300

    Add different colors for filters in light theme

--- src/lib/components/workspace/Prompts.svelte
+++ src/lib/components/workspace/Prompts.svelte
@@ -5,7 +5,15 @@
 
 	import { goto } from '$app/navigation';
 	import { onMount, getContext } from 'svelte';
-	import { WEBUI_NAME, config, prompts as _prompts, user, showSidebar, mobile } from '$lib/stores';
+	import {
+		WEBUI_NAME,
+		config,
+		prompts as _prompts,
+		user,
+		showSidebar,
+		mobile,
+		theme
+	} from '$lib/stores';
 
 	import {
 		createNewPrompt,
@@ -23,7 +31,7 @@
 	import ChevronRight from '../icons/ChevronRight.svelte';
 	import Spinner from '../common/Spinner.svelte';
 	import Tooltip from '../common/Tooltip.svelte';
-	import { capitalizeFirstLetter } from '$lib/utils';
+	import { capitalizeFirstLetter, tagColorsLight } from '$lib/utils';
 	import ShowSidebarIcon from '../icons/ShowSidebarIcon.svelte';
 	import { getGroups } from '$lib/apis/groups';
 	import GroupIcon from '../icons/GroupIcon.svelte';
@@ -78,7 +86,10 @@
 			const promptTags = p.meta?.tags?.map((t) => t.name.toLowerCase()) || [];
 
 			const tagsMatch =
-				selectedTags.size === 0 || Array.from(selectedTags)?.map(tag => tag?.toLowerCase())?.some((tag) => promptTags.includes(tag));
+				selectedTags.size === 0 ||
+				Array.from(selectedTags)
+					?.map((tag) => tag?.toLowerCase())
+					?.some((tag) => promptTags.includes(tag));
 
 			const accessMatch =
 				accessFilter === 'all' ||
@@ -133,8 +144,8 @@
 
 	const getTags = async () => {
 		const res = await getUserTagsForPrompts(localStorage.token);
-		tags = res.filter(tag => tag.is_system).map(tag => tag.name);
-	}
+		tags = res.filter((tag) => tag.is_system).map((tag) => tag.name);
+	};
 
 	onMount(async () => {
 		await init();
@@ -188,11 +199,10 @@
 		const res = await bookmarkPrompt(localStorage.token, command);
 		if (res) {
 			prompts = await getPromptList(localStorage.token);
-		    _prompts.set(await getPrompts(localStorage.token));
+			_prompts.set(await getPrompts(localStorage.token));
 		}
 		loadingBookmark = null;
-	}
-	
+	};
 </script>
 
 <svelte:head>
@@ -214,14 +224,19 @@
 		</div>
 	</DeleteConfirmDialog>
 
-	<div id="prompts-header" class="pl-4 md:pl-[22px] pr-4 py-2.5 border-b border-lightGray-400 dark:border-customGray-700">
+	<div
+		id="prompts-header"
+		class="pl-4 md:pl-[22px] pr-4 py-2.5 border-b border-lightGray-400 dark:border-customGray-700"
+	>
 		<div class="flex justify-between items-center">
 			<div class="flex items-center">
 				<div class="{$showSidebar ? 'md:hidden' : ''} self-center flex flex-none items-center">
-					{#if ($mobile)}
+					{#if $mobile}
 						<button class="flex items-center gap-1" on:click={() => history.back()}>
 							<BackIcon />
-							<div class="flex items-center md:self-center text-base font-medium leading-none px-0.5 text-lightGray-100 dark:text-customGray-100">
+							<div
+								class="flex items-center md:self-center text-base font-medium leading-none px-0.5 text-lightGray-100 dark:text-customGray-100"
+							>
 								{$i18n.t('Prompts')}
 							</div>
 						</button>
@@ -238,10 +253,12 @@
 								<ShowSidebarIcon />
 							</div>
 						</button>
-					{/if}	
+					{/if}
 				</div>
-				{#if (!$mobile)}
-					<div class="flex items-center text-lightGray-100 dark:text-customGray-100 md:self-center text-base font-medium leading-none px-0.5">
+				{#if !$mobile}
+					<div
+						class="flex items-center text-lightGray-100 dark:text-customGray-100 md:self-center text-base font-medium leading-none px-0.5"
+					>
 						{$i18n.t('Prompts')}
 					</div>
 				{/if}
@@ -287,16 +304,16 @@
 	</div>
 
 	<div class="pl-4 md:pl-[22px] pr-4">
-		<div
-			id="prompts-filters"
-			class="flex justify-between py-5 md:pr-[22px] flex-row items-start"
-		>
+		<div id="prompts-filters" class="flex justify-between py-5 md:pr-[22px] flex-row items-start">
 			<div class="flex items-start space-x-[5px] flex-col sm:flex-row mb-3 sm:mb-0">
-				{#if ($mobile)}
+				{#if $mobile}
 					<FilterDropdown>
 						<div class="flex flex-wrap gap-1">
-							{#each tags as tag}
+							{#each tags as tag, i}
 								<button
+									style={`background-color: ${
+										$theme === 'light' ? tagColorsLight[i % tagColorsLight.length] : ''
+									}`}
 									class={`font-medium flex items-center justify-center rounded-md text-xs leading-none px-[6px] py-[6px] ${selectedTags.has(tag) ? 'dark:bg-customBlue-800 bg-customViolet-200' : 'bg-lightGray-400 dark:bg-customGray-800'} dark:text-white`}
 									on:click={() => {
 										selectedTags.has(tag) ? selectedTags.delete(tag) : selectedTags.add(tag);
@@ -315,8 +332,11 @@
 						{$i18n.t('Filter by category:')}
 					</div>
 					<div class="flex flex-wrap gap-1">
-						{#each tags as tag}
+						{#each tags as tag, i}
 							<button
+								style={`background-color: ${
+									$theme === 'light' ? tagColorsLight[i % tagColorsLight.length] : ''
+								}`}
 								class={`font-medium flex items-center justify-center rounded-md text-xs leading-none px-[6px] py-[6px] ${selectedTags.has(tag) ? 'dark:bg-customBlue-800 bg-customViolet-200' : 'bg-lightGray-400 hover:bg-customViolet-200 dark:bg-customGray-800 dark:hover:bg-customBlue-800'} dark:text-white`}
 								on:click={() => {
 									selectedTags.has(tag) ? selectedTags.delete(tag) : selectedTags.add(tag);
@@ -334,7 +354,7 @@
 					on:click={() => (accessFilter = 'all')}
 					class={`${accessFilter === 'all' ? 'bg-lightGray-400 text-lightGray-100 dark:bg-customGray-900 rounded-md border border-lightGray-250 dark:border-customGray-700' : 'text-lightGray-100/70'} font-medium px-2 md:px-[23px] py-[7px] flex-shrink-0 text-xs leading-none dark:text-white`}
 					>{$i18n.t('All')}</button
-				> 
+				>
 				<button
 					on:click={() => (accessFilter = 'private')}
 					class={`${accessFilter === 'private' ? 'bg-lightGray-400 text-lightGray-100 dark:bg-customGray-900 rounded-md border border-lightGray-250 dark:border-customGray-700' : 'text-lightGray-100/70'} font-medium px-2 md:px-[23px] py-[7px] flex-shrink-0 text-xs leading-none dark:text-white`}
@@ -356,15 +376,19 @@
 			id="prompts-scroll-container"
 			bind:this={scrollContainer}
 			class="overflow-y-scroll pr-[3px]"
-		>	
+		>
 			{#if filteredItems.length === 0}
 				{#if selectedTags?.size > 0}
 					<div class="flex h-[calc(100dvh-200px)] w-full justify-center items-center">
-						<div class="text-sm dark:text-customGray-100/50">{$i18n.t('No prompts match the selected filters')}</div>
+						<div class="text-sm dark:text-customGray-100/50">
+							{$i18n.t('No prompts match the selected filters')}
+						</div>
 					</div>
 				{:else}
 					<div class="flex h-[calc(100dvh-200px)] w-full justify-center items-center">
-						<div class="text-sm dark:text-customGray-100/50">{$i18n.t('No prompts created yet')}</div>
+						<div class="text-sm dark:text-customGray-100/50">
+							{$i18n.t('No prompts created yet')}
+						</div>
 					</div>
 				{/if}
 			{/if}
@@ -376,42 +400,53 @@
 			<div class="mb-5 gap-2 grid lg:grid-cols-2 xl:grid-cols-3">
 				{#each filteredItems as prompt}
 					<div
-						on:mouseenter={() => hoveredPrompt = prompt.command}
-						on:mouseleave={() => hoveredPrompt = null}
+						on:mouseenter={() => (hoveredPrompt = prompt.command)}
+						on:mouseleave={() => (hoveredPrompt = null)}
 						class=" group flex flex-col gap-y-1 cursor-pointer w-full px-3 py-2 bg-lightGray-550 dark:bg-customGray-800 rounded-2xl transition"
 					>
 						<div class="flex items-start justify-between">
 							<div class="flex items-center">
 								{#if loadingBookmark === prompt?.command}
-									<Spinner className="size-3 mr-1"/>
+									<Spinner className="size-3 mr-1" />
 								{:else}
-									<button on:click={() => bookmarkPromptHandler(prompt.command)} class="text-lightGray-100 dark:text-customGray-300 mr-1">
+									<button
+										on:click={() => bookmarkPromptHandler(prompt.command)}
+										class="text-lightGray-100 dark:text-customGray-300 mr-1"
+									>
 										{#if prompt?.bookmarked_by_user}
-											<BookmarkedIcon/>
+											<BookmarkedIcon />
 										{:else}
-											<BookmarkIcon/>
+											<BookmarkIcon />
 										{/if}
-										
 									</button>
 								{/if}
-								
+
 								<div class="flex items-center gap-1 flex-wrap">
 									{#if prompt.access_control == null && prompt.prebuilt}
 										<div
-											class="flex gap-1 items-center {(hoveredPrompt === prompt.command || menuIdOpened === prompt.command) ? 'dark:text-white' : 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
+											class="flex gap-1 items-center {hoveredPrompt === prompt.command ||
+											menuIdOpened === prompt.command
+												? 'dark:text-white'
+												: 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
 										>
 											<span>{$i18n.t('Prebuilt')}</span>
 										</div>
 									{:else if prompt.access_control == null}
 										<div
-											class="flex gap-1 items-center {(hoveredPrompt === prompt.command || menuIdOpened === prompt.command) ? 'dark:text-white' : 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
+											class="flex gap-1 items-center {hoveredPrompt === prompt.command ||
+											menuIdOpened === prompt.command
+												? 'dark:text-white'
+												: 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
 										>
 											<PublicIcon />
 											<span>{$i18n.t('Public')}</span>
 										</div>
 									{:else if getGroupNamesFromAccess(prompt).length < 1}
 										<div
-											class="flex gap-1 items-center {(hoveredPrompt === prompt.command || menuIdOpened === prompt.command) ? 'dark:text-white' : 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
+											class="flex gap-1 items-center {hoveredPrompt === prompt.command ||
+											menuIdOpened === prompt.command
+												? 'dark:text-white'
+												: 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
 										>
 											<PrivateIcon />
 											<span>{$i18n.t('Private')}</span>
@@ -419,7 +454,10 @@
 									{:else}
 										{#each getGroupNamesFromAccess(prompt) as groupName}
 											<div
-												class="flex gap-1 items-center {(hoveredPrompt === prompt.command || menuIdOpened === prompt.command) ? 'dark:text-white' : 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
+												class="flex gap-1 items-center {hoveredPrompt === prompt.command ||
+												menuIdOpened === prompt.command
+													? 'dark:text-white'
+													: 'dark:text-customGray-300'} text-lightGray-100 font-medium bg-lightGray-400 text-xs dark:bg-customGray-900 px-[6px] py-[3px] rounded-md"
 											>
 												<GroupIcon />
 												<span>{groupName}</span>
@@ -429,7 +467,10 @@
 									{#if prompt.meta && Array.isArray(prompt.meta.tags)}
 										{#each prompt?.meta?.tags as promptTag}
 											<div
-												class="flex items-center {(hoveredPrompt === prompt.command || menuIdOpened === prompt.command) ? 'dark:text-white' : 'dark:text-customGray-100'} text-xs text-lightGray-100 font-medium bg-customViolet-200 dark:bg-customBlue-800 px-[6px] py-[3px] rounded-md"
+												class="flex items-center {hoveredPrompt === prompt.command ||
+												menuIdOpened === prompt.command
+													? 'dark:text-white'
+													: 'dark:text-customGray-100'} text-xs text-lightGray-100 font-medium bg-customViolet-200 dark:bg-customBlue-800 px-[6px] py-[3px] rounded-md"
 											>
 												{$i18n.t(promptTag.name)}
 											</div>
@@ -438,7 +479,11 @@
 								</div>
 							</div>
 							{#if !prompt.prebuilt && (prompt.user_id === $user?.id || $user?.role === 'admin')}
-								<div class="{(hoveredPrompt === prompt.command || menuIdOpened === prompt.command) ? 'md:visible' : 'md:invisible'}">
+								<div
+									class={hoveredPrompt === prompt.command || menuIdOpened === prompt.command
+										? 'md:visible'
+										: 'md:invisible'}
+								>
 									<PromptMenu
 										{prompt}
 										shareHandler={() => {
@@ -456,10 +501,10 @@
 										}}
 										onClose={() => {}}
 										on:openMenu={() => {
-											menuIdOpened = prompt.command
+											menuIdOpened = prompt.command;
 										}}
 										on:closeMenu={() => {
-											menuIdOpened = null
+											menuIdOpened = null;
 										}}
 									>
 										<button
@@ -473,18 +518,27 @@
 							{/if}
 						</div>
 						<div class=" flex flex-1 space-x-4 cursor-pointer w-full">
-							<button type="button" class="w-full" 
-							on:click={() => {
-								localStorage.setItem('selectedPrompt', JSON.stringify(prompt));
-								goto('/');
-							  }} 
+							<button
+								type="button"
+								class="w-full"
+								on:click={() => {
+									localStorage.setItem('selectedPrompt', JSON.stringify(prompt));
+									goto('/');
+								}}
 							>
 								<div class=" flex-1 flex items-center gap-2 self-center">
-									<div class="text-left text-base line-clamp-1 capitalize text-lightGray-100 {(hoveredPrompt === prompt.command || menuIdOpened === prompt.command) ? 'dark:text-white' : 'dark:text-customGray-100'}">
+									<div
+										class="text-left text-base line-clamp-1 capitalize text-lightGray-100 {hoveredPrompt ===
+											prompt.command || menuIdOpened === prompt.command
+											? 'dark:text-white'
+											: 'dark:text-customGray-100'}"
+									>
 										{prompt.title}
 									</div>
 								</div>
-								<div class="text-xs line-clamp-1 text-lightGray-1200 dark:text-customGray-100/50 text-left">
+								<div
+									class="text-xs line-clamp-1 text-lightGray-1200 dark:text-customGray-100/50 text-left"
+								>
 									{prompt.description}
 								</div>
 							</button>
