1812b1b6cdfe5b699491229773d4039139ab4e5d
Jean-Baptiste Bolh <jbbolh@gmail.com>
2025-06-18 14:39:50 -0500

    branded with our logos

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,24 @@ import { defineConfig } from 'vite';
 
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// /** @type {import('vite').Plugin} */
-// const viteServerConfig = {
-// 	name: 'log-request-middleware',
-// 	configureServer(server) {
-// 		server.middlewares.use((req, res, next) => {
-// 			res.setHeader('Access-Control-Allow-Origin', '*');
-// 			res.setHeader('Access-Control-Allow-Methods', 'GET');
-// 			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-// 			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-// 			next();
-// 		});
-// 	}
-// };
+/** @type {import('vite').Plugin} */
+const viteServerConfig = {
+	name: 'log-request-middleware',
+	configureServer(server) {
+		server.middlewares.use((req, res, next) => {
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', 'GET');
+			res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+			res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+			next();
+		});
+	}
+};
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		viteServerConfig,
 		viteStaticCopy({
 			targets: [
 				{
