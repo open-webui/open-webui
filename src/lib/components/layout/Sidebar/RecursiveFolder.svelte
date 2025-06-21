c245a6316fa25b9e3e5e25f148c3ac38a0333ee1
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-06-21 21:58:14 +0200

    Update RecursiveFolder.svelte

--- src/lib/components/layout/Sidebar/RecursiveFolder.svelte
+++ src/lib/components/layout/Sidebar/RecursiveFolder.svelte
@@ -47,6 +47,7 @@
 	export let parentDragged = false;
 
 	let folderElement;
+	let folderMenuShow = false;
 
 	let draggedOver = false;
 	let dragged = false;
@@ -373,7 +374,16 @@
 					    exportHandler();
 					}}
 				    >
-					<button class="p-0.5 dark:hover:bg-gray-850 hover:bg-gray-200 rounded-lg touch-auto">
+					<button 
+						class="p-0.5 dark:hover:bg-gray-850 hover:bg-gray-200 rounded-lg touch-auto"
+						on:click={(e) => {
+        						e.stopPropagation();
+        						folderMenuShow = !folderMenuShow;
+						}}
+						on:pointerup={(e) => {
+        						e.stopPropagation();
+						}}
+					>
 						<EllipsisHorizontal className="size-4" strokeWidth="2.5" />
 					</button>
 				    </FolderMenu>
