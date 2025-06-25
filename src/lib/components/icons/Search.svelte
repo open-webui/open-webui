88c4f55a51f8019c59b24b5c29dce307dc3b8ff1
Silentoplayz <50341825+Silentoplayz@users.noreply.github.com>
2025-06-25 14:00:48 -0400

    chore: update

--- src/lib/components/icons/Search.svelte
+++ src/lib/components/icons/Search.svelte
@@ -3,17 +3,10 @@
 	export let strokeWidth = '1.5';
 </script>
 
-<svg
-	xmlns="http://www.w3.org/2000/svg"
-	fill="none"
-	viewBox="0 0 24 24"
-	stroke-width={strokeWidth}
-	stroke="currentColor"
-	class={className}
->
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class={className}>
 	<path
-		stroke-linecap="round"
-		stroke-linejoin="round"
-		d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+		fill-rule="evenodd"
+		d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+		clip-rule="evenodd"
 	/>
 </svg>
