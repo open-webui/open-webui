cf24e39404823eb526f3e571d83cb8398e737689
Silentoplayz <50341825+Silentoplayz@users.noreply.github.com>
2025-06-25 17:36:23 -0400

    chore: add strokeWidth back

--- src/lib/components/icons/Search.svelte
+++ src/lib/components/icons/Search.svelte
@@ -3,7 +3,13 @@
 	export let strokeWidth = '1.5';
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class={className}>
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 20 20"
+	stroke-width={strokeWidth}
+	fill="currentColor"
+	class={className}
+>
 	<path
 		fill-rule="evenodd"
 		d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
