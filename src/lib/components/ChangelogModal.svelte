38dfa6e6db420337614b0fa2e931bd71c603b7d0
Timothy Jaeryang Baek <tim@openwebui.com>
2025-06-27 15:30:00 +0400

    refac

--- src/lib/components/ChangelogModal.svelte
+++ src/lib/components/ChangelogModal.svelte
@@ -39,7 +39,9 @@
 				}}
 				aria-label={$i18n.t('Close')}
 			>
-				<XMark />
+				<XMark>
+					<p class="sr-only">{$i18n.t('Close')}</p>
+				</XMark>
 			</button>
 		</div>
 		<div class="flex items-center mt-1">
