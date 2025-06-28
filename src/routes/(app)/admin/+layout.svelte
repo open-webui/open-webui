7523613fbc317f26fff5c2ac1c15cefc7066b6ca
Timothy Jaeryang Baek <tim@openwebui.com>
2025-06-28 19:29:28 +0400

    refac

--- src/routes/(app)/admin/+layout.svelte
+++ src/routes/(app)/admin/+layout.svelte
@@ -52,7 +52,7 @@
 						class="flex gap-1 scrollbar-none overflow-x-auto w-fit text-center text-sm font-medium rounded-full bg-transparent pt-1"
 					>
 						<a
-							class="min-w-fit rounded-full p-1.5 {['/admin/users'].includes($page.url.pathname)
+							class="min-w-fit rounded-full p-1.5 {$page.url.pathname.includes('/admin/users')
 								? ''
 								: 'text-gray-300 dark:text-gray-600 hover:text-gray-700 dark:hover:text-white'} transition"
 							href="/admin">{$i18n.t('Users')}</a
