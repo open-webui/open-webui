dd3467960fc97e6b15e29f4dcff273184a2a2db3
Avin Kumar <avin.kumar@nagarro.com>
2025-07-03 21:26:39 +0530

    Feat:[GPT-473] model dropdown css added with static Html

---
+++ src/lib/components/icons/ArrowDown.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let className = 'size-4';
+	export let strokeWidth = '2';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="10" height="7" viewBox="0 0 10 7" fill="none">
+<path d="M5 6.83366L0 1.83366L1.16667 0.666992L5 4.50033L8.83333 0.666992L10 1.83366L5 6.83366Z" fill="#424750"/>
+</svg>
