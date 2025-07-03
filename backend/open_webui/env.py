70f1611f45b1c396a52fe6c695ee650b6b9cf761
F1xGOD FWS <f1xgodim@gmail.com>
2025-07-03 00:09:33 -0500

    Main

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -106,11 +106,9 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "FixCraft AI")
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://www.fixcraft.org/favicon.png"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
