2eab43c35e3d87264d50a63c576b6d365195c97f
Timothy Jaeryang Baek <tim@openwebui.com>
2025-07-06 14:38:28 +0400

    fix

--- src/lib/apis/ollama/index.ts
+++ src/lib/apis/ollama/index.ts
@@ -366,7 +366,7 @@ export const unloadModel = async (token: string, tagName: string) => {
 			Authorization: `Bearer ${token}`
 		},
 		body: JSON.stringify({
-			name: tagName
+			model: tagName
 		})
 	}).catch((err) => {
 		error = err;
