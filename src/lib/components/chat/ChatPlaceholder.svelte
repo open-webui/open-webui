afa4746ae51449d2c77d065a38b10983e4009c12
Hakim Ali <hakim.sultan@dge.gov.ae>
2025-07-09 10:47:33 +0400

    fix: update the figma

--- src/lib/components/chat/ChatPlaceholder.svelte
+++ src/lib/components/chat/ChatPlaceholder.svelte
@@ -36,36 +36,6 @@
 
 {#key mounted}
 	<div class="m-auto w-full max-w-6xl px-8 lg:px-20">
-		<!--<div class="flex justify-start">
-			<div class="flex -space-x-4 mb-0.5" in:fade={{ duration: 200 }}>
-				{#each models as model, modelIdx}
-					<button
-						on:click={() => {
-							selectedModelIdx = modelIdx;
-						}}
-					>
-						<Tooltip
-							content={marked.parse(
-								sanitizeResponseContent(models[selectedModelIdx]?.info?.meta?.description ?? '')
-							)}
-							placement="right"
-						>
-							<img
-								crossorigin="anonymous"
-								src={model?.info?.meta?.profile_image_url ??
-									($i18n.language === 'dg-DG'
-										? `/doge.png`
-										: `${WEBUI_BASE_URL}/static/favicon.png`)}
-								class=" size-[2.7rem] rounded-full border-[1px] border-gray-100 dark:border-none"
-								alt="logo"
-								draggable="false"
-							/>
-						</Tooltip>
-					</button>
-				{/each}
-			</div>
-		</div>-->
-
 		{#if $temporaryChatEnabled}
 			<Tooltip
 				content={$i18n.t('This chat won’t appear in history and your messages will not be saved.')}
@@ -78,53 +48,17 @@
 			</Tooltip>
 		{/if}
 
-		<div
-			class=" mt-2 mb-4 text-3xl justify-center text-gray-800 dark:text-gray-100 font-medium text-left flex items-center gap-4 font-primary"
-		>
-			<div>
-				<div class=" justify-center capitalize line-clamp-1" in:fade={{ duration: 200 }}>
-					{#if models[selectedModelIdx]?.name}
-						{models[selectedModelIdx]?.name}
-					{:else}
-						{$i18n.t('Hello, {{name}}', { name: $user?.name })}
-					{/if}
+		<div class="flex flex-col gap-4 items-center justify-center w-full mt-12 mb-8">
+			<div class="w-full">
+				<div class="font-heading font-medium text-[20px] leading-[28px] text-[#6b6d70] text-center truncate">
+					Hi {($user?.name ?? 'there')} 👋
 				</div>
-
-				<div in:fade={{ duration: 200, delay: 200 }}>
-					{#if models[selectedModelIdx]?.info?.meta?.description ?? null}
-						<div
-							class=" justify-center mt-0.5 text-4xl font-normal text-gray-500 dark:text-gray-400 line-clamp-3 markdown"
-						>
-							{@html marked.parse(
-								sanitizeResponseContent(models[selectedModelIdx]?.info?.meta?.description)
-							)}
-						</div>
-						{#if models[selectedModelIdx]?.info?.meta?.user}
-							<div  justify-center class="mt-0.5 text-4xl font-normal text-dark dark:text-gray-500">
-								By
-								{#if models[selectedModelIdx]?.info?.meta?.user.community}
-									<a
-										href="https://openwebui.com/m/{models[selectedModelIdx]?.info?.meta?.user
-											.username}"
-										>{models[selectedModelIdx]?.info?.meta?.user.name
-											? models[selectedModelIdx]?.info?.meta?.user.name
-											: `@${models[selectedModelIdx]?.info?.meta?.user.username}`}</a
-									>
-								{:else}
-									{models[selectedModelIdx]?.info?.meta?.user.name}
-								{/if}
-							</div>
-						{/if}
-					{:else}
-						<div class="flex justify-center pt-[20px] pb-[40px] text-base font-normal text-dark dark:text-gray-500">
-							{$i18n.t('How can I help you today?')}
-						</div>
-					{/if}
+				<div class="font-heading font-bold text-[32px] leading-[43px] text-[#36383b] text-center truncate mt-1">
+					How can I help you?
 				</div>
 			</div>
 		</div>
-
-		<div class=" w-full font-primary" in:fade={{ duration: 200, delay: 300 }}>
+		<div class="w-full font-primary" in:fade={{ duration: 200, delay: 300 }}>
 			<Suggestions
 				className="grid grid-cols-2"
 				suggestionPrompts={atSelectedModel?.info?.meta?.suggestion_prompts ??
