91707efc8b9b0dca5e376469123fd9236db9c4a2
signorettif <signorettif@gmail.com>
2025-07-09 21:31:55 -0400

    do not return in-memory stuff if it is not current spec

--- backend/open_webui/utils/tools_cache.py
+++ backend/open_webui/utils/tools_cache.py
@@ -46,7 +46,13 @@ async def get_cached_tool_servers(request: Request):
 
     # Check in-memory cache
     if request.app.state.TOOL_SERVERS:
-        return request.app.state.TOOL_SERVERS
+        # Check if all tool servers have the required _raw property. This is for backwards compatibility. before
+        # we added the ability to cache tool servers in Redis. TODO: remove this some time in the future (added Jul 2025)
+        if all(hasattr(server, "_raw") for server in request.app.state.TOOL_SERVERS):
+            return request.app.state.TOOL_SERVERS
+        else:
+            log.debug("Tool servers cache missing _raw property, invalidating")
+            request.app.state.TOOL_SERVERS = []
 
     # Check if the tool server connections have been initialized by an environment variable
     if request.app.state.config.TOOL_SERVER_CONNECTIONS:
