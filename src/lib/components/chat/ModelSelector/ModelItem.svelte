af1f448b2c43f6b4222f35937b53a7640e3e8e96
DChemishanov <dzhovani.chemishanov@gmail.com>
2025-07-13 12:25:06 +0300

    Do not Use Hardcoded String as Aria Label for Model Name Selection on Chat Screen

--- src/lib/components/chat/ModelSelector/ModelItem.svelte
+++ src/lib/components/chat/ModelSelector/ModelItem.svelte
@@ -42,7 +42,7 @@
 </script>
 
 <button
-	aria-label="model-item"
+	aria-label="{item.value}"
 	class="flex group/item w-full text-left font-medium line-clamp-1 select-none items-center rounded-button py-2 pl-3 pr-1.5 text-sm text-gray-700 dark:text-gray-100 outline-hidden transition-all duration-75 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg cursor-pointer data-highlighted:bg-muted {index ===
 	selectedModelIdx
 		? 'bg-gray-100 dark:bg-gray-800 group-hover:bg-transparent'
