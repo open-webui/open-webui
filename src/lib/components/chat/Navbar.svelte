c189b20945c8010fb40cce548e4388b9dda01ee6
Avin Kumar <avin.kumar@nagarro.com>
2025-07-16 19:52:08 +0530

    Feat:WebSearch code added again

--- src/lib/components/chat/Navbar.svelte
+++ src/lib/components/chat/Navbar.svelte
@@ -46,7 +46,7 @@
 
 	let showShareChatModal = false;
 	let showDownloadChatModal = false;
-	let isOn = false;
+	 let isOn = false
 </script>
 
 <ShareChatModal bind:show={showShareChatModal} chatId={$chatId} />
@@ -61,7 +61,7 @@
 />
 
 <nav class="sticky top-0 z-30 w-full px-[20px] py-[18px] flex flex-col items-center drag-region">
-	<div class="flex items-center w-full">
+	<div class="flex items-center w-full ">
 		<div
 			class=" bg-surface dark:from-gray-900 dark:via-gray-900 dark:to-transparent pointer-events-none absolute inset-0 -bottom-7 z-[-1]"
 		></div>
@@ -121,18 +121,15 @@
 						</Menu>
 					{/if}-->
 					<div class="flex items-center mr-[12px]">
-						<label class="relative inline-flex items-center cursor-pointer">
-							<input type="checkbox" bind:checked={isOn} class="sr-only peer" />
-							<div class="w-[56px] h-[28px] bg-gray-1100 rounded-full peer duration-300">
-								<div
-									class=" flex items-center justify-center absolute {isOn
-										? 'left-[3px]'
-										: 'right-[3px]'}  top-[3px] w-[20px] h-[20px] bg-white rounded-full transition-transform duration-300 peer-checked:translate-x-5"
-								>
-									<LightMode strokeWidth="2" className="size-[1.1rem]" />
-								</div>
-							</div>
-						</label>
+
+					<label class="relative inline-flex items-center cursor-pointer">
+						<input type="checkbox" bind:checked={isOn} class="sr-only peer" />
+						<div class="w-[56px] h-[28px] bg-gray-1100 rounded-full peer duration-300">
+						<div class=" flex items-center justify-center absolute  {isOn ? 'left-[3px]' : 'right-[3px]'}  top-[3px] w-[20px] h-[20px] bg-white rounded-full transition-transform duration-300 peer-checked:translate-x-5">
+							<LightMode strokeWidth="2" className="size-[1.1rem]" />
+						</div>
+						</div>
+					</label>
 					</div>
 					<!--<Tooltip content={$i18n.t('Controls')}>
 						<button
