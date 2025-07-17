116ecf0033d70178020e5937a46cc595f6a7e96f
Hakim Ali <hakim.sultan@dge.gov.ae>
2025-07-17 13:34:44 +0400

    fix: update the navbar

--- src/lib/components/chat/Navbar.svelte
+++ src/lib/components/chat/Navbar.svelte
@@ -35,7 +35,6 @@
 
 	import MaterialIcon from '$lib/components/common/MaterialIcon.svelte';
 
-
 	const i18n = getContext('i18n');
 
 	export let initNewChat: Function;
@@ -49,7 +48,7 @@
 
 	let showShareChatModal = false;
 	let showDownloadChatModal = false;
-	 let isOn = false
+	let isOn = false;
 </script>
 
 <ShareChatModal bind:show={showShareChatModal} chatId={$chatId} />
@@ -63,32 +62,35 @@
 	aria-label="New Chat"
 />
 
-<nav class="w-full flex items-center justify-between px-4 py-0 h-[56px] relative z-30 { $mobile ? 'bg-white dark:bg-gray-900 border-b border-[#dee0e3] dark:border-gray-800' : 'bg-transparent' }">
-  {#if $mobile}
-    <button
-      class="flex items-center justify-center rounded-lg size-10 hover:bg-[#e5e7eb] transition"
-      aria-label="Toggle Sidebar"
-      on:click={() => showSidebar.set(!$showSidebar)}
-    >
-      <MaterialIcon name="menu" className="w-6 h-6" />
-    </button>
-    <button
-      class="flex items-center justify-center rounded-lg size-10 hover:bg-[#e5e7eb] transition"
-      aria-label="New Chat"
-      on:click={() => initNewChat()}
-    >
-      <MaterialIcon name="add" className="w-[18px] h-[18px]" />
-    </button>
-  {:else}
+<nav
+	class="w-full flex items-center justify-between px-4 py-0 h-[56px] relative z-30 {$mobile
+		? 'bg-white dark:bg-gray-900 border-b border-[#dee0e3] dark:border-gray-800'
+		: 'bg-transparent'}"
+>
+	{#if $mobile}
+		<button
+			class="flex items-center justify-center rounded-lg size-10 hover:bg-[#e5e7eb] transition"
+			aria-label="Toggle Sidebar"
+			on:click={() => showSidebar.set(!$showSidebar)}
+		>
+			<MaterialIcon name="menu" className="w-6 h-6" />
+		</button>
+		<button
+			class="flex items-center justify-center rounded-lg size-10 hover:bg-[#e5e7eb] transition"
+			aria-label="New Chat"
+			on:click={() => initNewChat()}
+		>
+			<MaterialIcon name="add" className="w-[18px] h-[18px]" />
+		</button>
+	{:else}
+		<div class="flex items-center">
+			<img
+				src="/logo-dark.png"
+				alt="GovGPT Logo"
+				class="w-[132px] h-[40px] filter dark:invert dark:brightness-0 dark:contrast-200"
+			/>
 
-  <div class="flex items-center">
-    <img
-	src="/logo-dark.png"
-	alt="GovGPT Logo"
-	class="w-[132px] h-[40px] filter dark:invert dark:brightness-0 dark:contrast-200"
-	/>
-
-	<div
+			<div
 				class="flex-1 overflow-hidden max-w-full py-0.5
 			{$showSidebar ? 'ml-1' : ''}
 			"
@@ -97,9 +99,6 @@
 					<ModelSelector bind:selectedModels showSetDefault={!shareEnabled} />
 				{/if}
 			</div>
-
-
-
-    <!-- No add/new chat icon on desktop -->
-  {/if}
+		</div>
+	{/if}
 </nav>
