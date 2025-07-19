d9a3b02e3f1ce3e9acb65d43ce04c635bcc075f6
Timothy Jaeryang Baek <tim@openwebui.com>
2025-07-19 19:03:01 +0400

    refac

--- src/lib/components/workspace/Prompts/PromptEditor.svelte
+++ src/lib/components/workspace/Prompts/PromptEditor.svelte
@@ -173,7 +173,7 @@
 
 				<div class="text-xs text-gray-400 dark:text-gray-500 underline">
 					<a href="https://docs.openwebui.com/features/workspace/prompts" target="_blank">
-						{$i18n.t('For more powerful prompt variables, check the documentation.')}
+						{$i18n.t('To learn more about powerful prompt variables, click here')}
 					</a>
 				</div>
 			</div>
