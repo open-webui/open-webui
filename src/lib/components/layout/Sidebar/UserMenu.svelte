0acd78049df35bb413347ff33cc33898d359a035
Timothy Jaeryang Baek <tim@openwebui.com>
2025-08-06 11:50:35 +0400

    refac: hide docs, releases for users

--- src/lib/components/layout/Sidebar/UserMenu.svelte
+++ src/lib/components/layout/Sidebar/UserMenu.svelte
@@ -144,32 +144,34 @@
 
 				<!-- {$i18n.t('Help')} -->
 
-				<DropdownMenu.Item
-					as="a"
-					class="flex gap-2 items-center py-1.5 px-3 text-sm select-none w-full cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md transition"
-					id="chat-share-button"
-					on:click={() => {
-						show = false;
-					}}
-					href="https://docs.openwebui.com"
-				>
-					<QuestionMarkCircle className="size-5" />
-					<div class="flex items-center">{$i18n.t('Documentation')}</div>
-				</DropdownMenu.Item>
+				{#if $user?.role === 'admin'}
+					<DropdownMenu.Item
+						as="a"
+						class="flex gap-2 items-center py-1.5 px-3 text-sm select-none w-full cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md transition"
+						id="chat-share-button"
+						on:click={() => {
+							show = false;
+						}}
+						href="https://docs.openwebui.com"
+					>
+						<QuestionMarkCircle className="size-5" />
+						<div class="flex items-center">{$i18n.t('Documentation')}</div>
+					</DropdownMenu.Item>
 
-				<!-- Releases -->
-				<DropdownMenu.Item
-					as="a"
-					class="flex gap-2 items-center py-1.5 px-3 text-sm select-none w-full cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md transition"
-					id="chat-share-button"
-					on:click={() => {
-						show = false;
-					}}
-					href="https://github.com/open-webui/"
-				>
-					<Map className="size-5" />
-					<div class="flex items-center">{$i18n.t('Releases')}</div>
-				</DropdownMenu.Item>
+					<!-- Releases -->
+					<DropdownMenu.Item
+						as="a"
+						class="flex gap-2 items-center py-1.5 px-3 text-sm select-none w-full cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md transition"
+						id="chat-share-button"
+						on:click={() => {
+							show = false;
+						}}
+						href="https://github.com/open-webui/"
+					>
+						<Map className="size-5" />
+						<div class="flex items-center">{$i18n.t('Releases')}</div>
+					</DropdownMenu.Item>
+				{/if}
 
 				<DropdownMenu.Item
 					class="flex gap-2 items-center py-1.5 px-3 text-sm select-none w-full cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-800 rounded-md transition cursor-pointer"
