8eee4d1c12083eebdbe257512f49bf4e8829de52
Sine Jespersen <sinejespersen@gmail.com>
2025-08-06 15:49:20 +0200

    increase text contrast on highContrastMode

--- src/lib/components/chat/Settings/General.svelte
+++ src/lib/components/chat/Settings/General.svelte
@@ -234,10 +234,16 @@
 				</div>
 			</div>
 			{#if $i18n.language === 'en-US' && !($config?.license_metadata ?? false)}
-				<div class="mb-2 text-xs text-gray-400 dark:text-gray-500">
+				<div
+					class="mb-2 text-xs {($settings?.highContrastMode ?? false)
+						? 'text-gray-800 dark:text-gray-100'
+						: 'text-gray-400 dark:text-gray-500'}"
+				>
 					Couldn't find your language?
 					<a
-						class=" text-gray-300 font-medium underline"
+						class="font-medium underline {($settings?.highContrastMode ?? false)
+							? 'text-gray-700 dark:text-gray-200'
+							: 'text-gray-300'}"
 						href="https://github.com/open-webui/open-webui/blob/main/docs/CONTRIBUTING.md#-translations-and-internationalization"
 						target="_blank"
 					>
@@ -289,7 +295,9 @@
 				<div class="flex justify-between items-center text-sm">
 					<div class="  font-medium">{$i18n.t('Advanced Parameters')}</div>
 					<button
-						class=" text-xs font-medium text-gray-500"
+						class=" text-xs font-medium {($settings?.highContrastMode ?? false)
+							? 'text-gray-800 dark:text-gray-100'
+							: 'text-gray-400 dark:text-gray-500'}"
 						type="button"
 						on:click={() => {
 							showAdvanced = !showAdvanced;
