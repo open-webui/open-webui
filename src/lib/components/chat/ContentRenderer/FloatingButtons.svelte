056431606b1a9f4b0aa49092eb348dae4f0142dd
Timothy Jaeryang Baek <tim@openwebui.com>
2025-08-08 13:43:30 +0400

    enh: tool calling support in quick actions

--- src/lib/components/chat/ContentRenderer/FloatingButtons.svelte
+++ src/lib/components/chat/ContentRenderer/FloatingButtons.svelte
@@ -82,6 +82,16 @@
 		}
 
 		let prompt = selectedAction?.prompt ?? '';
+		let toolIds = [];
+
+		let toolIdPattern = /\{\{TOOL:([^\}]+)\}\}/g;
+		let match;
+		while ((match = toolIdPattern.exec(prompt)) !== null) {
+			toolIds.push(match[1]);
+		}
+		// Remove all TOOL placeholders from the prompt
+		prompt = prompt.replace(toolIdPattern, '');
+
 		if (prompt.includes('{{INPUT_CONTENT}}') && !floatingInput) {
 			prompt = prompt.replace('{{INPUT_CONTENT}}', floatingInputValue);
 			floatingInputValue = '';
@@ -106,6 +116,15 @@
 				role: message.role,
 				content: message.content
 			})),
+			...(toolIds.length > 0
+				? {
+						tool_ids: toolIds
+						// params: {
+						// 	function_calling: 'native'
+						// }
+					}
+				: {}),
+
 			stream: true // Enable streaming
 		});
 
