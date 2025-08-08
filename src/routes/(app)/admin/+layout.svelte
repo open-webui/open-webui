d2c7a60a431ff721aee64e9e2b158eb84db071cc
Timothy Jaeryang Baek <tim@openwebui.com>
2025-08-08 12:07:15 +0400

    refac

--- src/routes/(app)/admin/+layout.svelte
+++ src/routes/(app)/admin/+layout.svelte
@@ -27,9 +27,9 @@
 
 {#if loaded}
 	<div
-		class=" flex flex-col w-full h-screen max-h-[100dvh] transition-width duration-200 ease-in-out {$showSidebar
+		class=" flex flex-col h-screen max-h-[100dvh] flex-1 transition-width duration-200 ease-in-out {$showSidebar
 			? 'md:max-w-[calc(100%-260px)]'
-			: ''} max-w-full"
+			: ' md:max-w-[calc(100%-49px)]'}  w-full max-w-full"
 	>
 		<nav class="   px-2.5 pt-1.5 backdrop-blur-xl drag-region">
 			<div class=" flex items-center gap-1">
