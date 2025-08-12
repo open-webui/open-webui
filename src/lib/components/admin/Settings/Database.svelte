0bd42e5c6d93d2bea2930041636124148a8b47d0
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-08-12 12:47:34 +0200

    Update Database.svelte

--- src/lib/components/admin/Settings/Database.svelte
+++ src/lib/components/admin/Settings/Database.svelte
@@ -32,7 +32,8 @@
 			delete_orphaned_knowledge_bases,
 			delete_orphaned_models,
 			delete_orphaned_notes,
-			delete_orphaned_folders
+			delete_orphaned_folders,
+			audio_cache_max_age_days
 		} = event.detail;
 		
 		const res = await pruneData(
@@ -47,7 +48,8 @@
 			delete_orphaned_knowledge_bases,
 			delete_orphaned_models,
 			delete_orphaned_notes,
-			delete_orphaned_folders
+			delete_orphaned_folders,
+			audio_cache_max_age_days
 		).catch((error) => {
 			toast.error(`${error}`);
 			return null;
@@ -243,15 +245,15 @@
 							<path
 								fill-rule="evenodd"
 								d="M13 6H3v6a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V6ZM8.75 7.75a.75.75 0 0 0-1.5 0v2.69L6.03 9.22a.75.75 0 0 0-1.06 1.06l2.5 2.5a.75.75 0 0 0 1.06 0l2.5-2.5a.75.75 0 1 0-1.06-1.06l-1.22 1.22V7.75Z"
-								clip-rule="evenodd"
-							/>
-						</svg>
-					</div>
-					<div class=" self-center text-sm font-medium">
-						{$i18n.t('Export Users')}
-					</div>
-				</button>
-			{/if}
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div class=" self-center text-sm font-medium">
+							{$i18n.t('Export Users')}
+						</div>
+					</button>
+				{/if}
 			<hr class="border-gray-100 dark:border-gray-850 my-1" />
 			<button
 				type="button"
