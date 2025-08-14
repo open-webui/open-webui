035387425f58e0988ab9ede98a19247190205621
Sine Jespersen <sinejespersen@gmail.com>
2025-08-14 15:10:44 +0200

    5077: was redirect with import.meta.env to check if it exists and not create build errors

---
+++ src/routes/was/+page.svelte
@@ -0,0 +1,11 @@
+<!-- PATCH REDIRECT TO WAS -->
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+    
+	onMount(async () => {
+		const WAS_REDIRECT = import.meta.env.PUBLIC_WAS_REDIRECT || false;
+		WAS_REDIRECT ? window.open(WAS_REDIRECT, '_blank') : goto("/"); 
+	});
+</script>
+<!-- /PATCH REDIRECT TO WAS -->
