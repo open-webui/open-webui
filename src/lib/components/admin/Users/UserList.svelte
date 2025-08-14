b935a2809d4856b6b5ed8ef6ec0a09de6003edba
silentoplayz <jacwoo21@outlook.com>
2025-08-14 00:38:31 -0400

    revert: Not sure how to do this

--- src/lib/components/admin/Users/UserList.svelte
+++ src/lib/components/admin/Users/UserList.svelte
@@ -165,13 +165,13 @@
 			{#if ($config?.license_metadata?.seats ?? null) !== null}
 				{#if total > $config?.license_metadata?.seats}
 					<span class="text-lg font-medium text-red-500"
-						>{total} {$i18n.t('of')} {$config?.license_metadata?.seats}
-						<span class="text-sm font-normal">{$i18n.t('available users')}</span></span
+						>{total} of {$config?.license_metadata?.seats}
+						<span class="text-sm font-normal">available users</span></span
 					>
 				{:else}
 					<span class="text-lg font-medium text-gray-500 dark:text-gray-300"
-						>{total} {$i18n.t('of')} {$config?.license_metadata?.seats}
-						<span class="text-sm font-normal">{$i18n.t('available users')}</span></span
+						>{total} of {$config?.license_metadata?.seats}
+						<span class="text-sm font-normal">available users</span></span
 					>
 				{/if}
 			{:else}
