1abe576904aad6d3da5b7e98bac451ac0f9990d9
_00_ <131402327+rgaricano@users.noreply.github.com>
2025-08-18 01:40:05 +0200

    UPD: "duckduckgo" MetaSearch - set concurrent requests
    UPDATE for DuckDuckGo Search
    
    Forward of WEB_SEARCH_CONCURRENT_REQUEST config varr to ddgs function searches requests, assigning this var to ddgs.theads parameter.
    
    For Reference:
    
    [DDGS library](https://github.com/deedy5/ddgs)
    ____

--- backend/open_webui/retrieval/web/duckduckgo.py
+++ backend/open_webui/retrieval/web/duckduckgo.py
@@ -5,6 +5,7 @@ from open_webui.retrieval.web.main import SearchResult, get_filtered_results
 from ddgs import DDGS
 from ddgs.exceptions import RatelimitException
 from open_webui.env import SRC_LOG_LEVELS
+from open_webui.config import WEB_SEARCH_CONCURRENT_REQUESTS
 
 log = logging.getLogger(__name__)
 log.setLevel(SRC_LOG_LEVELS["RAG"])
@@ -26,6 +27,7 @@ def search_duckduckgo(
     search_results = []
     with DDGS() as ddgs:
         # Use the ddgs.text() method to perform the search
+        ddgs.threads=WEB_SEARCH_CONCURRENT_REQUESTS
         try:
             search_results = ddgs.text(
                 query, safesearch="moderate", max_results=count, backend="lite"
