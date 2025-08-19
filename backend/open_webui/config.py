6b02fa47858f25da515941c36f162f7c257f455c
Sine Jespersen <sinejespersen@gmail.com>
2025-08-19 13:34:02 +0200

    5077: add whitespace

--- backend/open_webui/config.py
+++ backend/open_webui/config.py
@@ -1040,6 +1040,7 @@ ENABLE_SIGNUP = PersistentConfig(
     ),
 )
 
+
 # PATCH REDIRECT TO WAS
 WAS_REDIRECT_URL = PersistentConfig(
     "WAS_REDIRECT_URL",
@@ -1047,6 +1048,7 @@ WAS_REDIRECT_URL = PersistentConfig(
     os.environ.get("WAS_REDIRECT_URL", ""),
 )
 # /PATCH REDIRECT TO WAS
+
 
 ENABLE_LOGIN_FORM = PersistentConfig(
     "ENABLE_LOGIN_FORM",
