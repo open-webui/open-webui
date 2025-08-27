703df8c815a9dc732fe8fe84ea03860184ce8de4
silentoplayz <jacwoo21@outlook.com>
2025-08-27 00:07:41 -0400

    refac: update_ollama_models.sh

--- update_ollama_models.sh
+++ update_ollama_models.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # update_llm.sh
 
 # Retrieves the list of LLMs installed in the Docker container
@@ -6,5 +6,5 @@ llm_list=$(docker exec ollama ollama list | tail -n +2 | awk '{print $1}')
 
 # Loop over each LLM to update it
 for llm in $llm_list; do
-  docker exec ollama ollama pull $llm
+  docker exec ollama ollama pull "$llm"
 done
