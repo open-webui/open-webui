1b214b935df8b4dbf61e2de0aba88783c6dffd57
silentoplayz <jacwoo21@outlook.com>
2025-08-27 00:02:17 -0400

    refac: confirm_remove.sh

--- confirm_remove.sh
+++ confirm_remove.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
-echo "Warning: This will remove all containers and volumes, including persistent data. Do you want to continue? [Y/N]"
-read ans
-if [ "$ans" == "Y" ] || [ "$ans" == "y" ]; then
-  command docker-compose 2>/dev/null
-  if [ "$?" == "0" ]; then
-    docker-compose down -v
-  else
-    docker compose down -v
-  fi
+#!/usr/bin/env bash
+
+echo "Warning: This will remove all containers and volumes, including persistent data."
+echo -n "Do you want to continue? [Y/N] "
+read -r ans
+
+if [[ $ans == [Yy] ]]; then
+    if command -v docker-compose >/dev/null 2>&1; then
+        docker-compose down -v
+    else
+        docker compose down -v
+    fi
 else
-  echo "Operation cancelled."
+    echo "Operation cancelled."
 fi
