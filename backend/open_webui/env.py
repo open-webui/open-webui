80a6b7b7e1d3d06dfb23324e233016412e885ca4
Ivan <50404984+NoWay1Co@users.noreply.github.com>
2025-09-02 19:07:37 +0300

    minor translation and logos update

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -110,9 +110,9 @@ for source in log_sources:
 
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "AI Digsol")
+if WEBUI_NAME != "AI Digsol":
+    WEBUI_NAME += " (AI Digsol)"
 
 WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
 
