3c69fc4bb4124fc96626383856e95c3694cba832
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-09-09 18:20:42 +0200

    Update ChangelogModal.svelte

--- src/lib/components/ChangelogModal.svelte
+++ src/lib/components/ChangelogModal.svelte
@@ -16,6 +16,7 @@
 	export let show = false;
 
 	let changelog = null;
+	let collapsedVersions = new Map();
 
 	onMount(async () => {
 		const res = await getChangelog();
@@ -57,41 +58,62 @@
 		<div class=" overflow-y-scroll max-h-96 scrollbar-hidden">
 			<div class="mb-3">
 				{#if changelog}
-					{#each Object.keys(changelog) as version}
-						<div class=" mb-3 pr-2">
-							<div class="font-semibold text-xl mb-1 dark:text-white">
-								v{version} - {changelog[version].date}
-							</div>
+				    {#each Object.keys(changelog) as version, i}
+				        {@const isCollapsed = collapsedVersions.get(version) ?? (i > 0)}
 
-							<hr class="border-gray-100 dark:border-gray-850 my-2" />
+				        <div class="mb-3 pr-2">
+				            <!-- 1. Make the header a button to toggle the collapsed state -->
+				            <button
+				                class="w-full flex justify-between items-center text-left"
+				                on:click={() => collapsedVersions.set(version, !isCollapsed)}
+				            >
+				                <div class="font-semibold text-xl dark:text-white">
+				                    v{version} - {changelog[version].date}
+				                </div>
 
-							{#each Object.keys(changelog[version]).filter((section) => section !== 'date') as section}
-								<div class="">
-									<div
-										class="font-semibold uppercase text-xs {section === 'added'
-											? 'text-white bg-blue-600'
-											: section === 'fixed'
-												? 'text-white bg-green-600'
-												: section === 'changed'
-													? 'text-white bg-yellow-600'
-													: section === 'removed'
-														? 'text-white bg-red-600'
-														: ''}  w-fit px-3 rounded-full my-2.5"
-									>
-										{section}
-									</div>
+				                <svg
+				                    class="size-4 transform transition-transform {isCollapsed ? '-rotate-90' : 'rotate-0'}"
+				                    xmlns="http://www.w3.org/2000/svg"
+				                    viewBox="0 0 20 20"
+				                    fill="currentColor"
+				                >
+				                    <path
+				                        fill-rule="evenodd"
+				                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+				                        clip-rule="evenodd"
+				                    />
+				                </svg>
+				            </button>
 
-									<ul class="my-2.5 px-1.5 space-y-2 changelog-content">
-									    {#each changelog[version][section] as entry}
-									        <!-- The 'raw' content is already an <li>, so we just render it directly.
-									             The 'text-sm' class ensures the font size is correct. -->
-									        {@html `<div class="text-sm">${entry.raw}</div>`}
-									    {/each}
-									</ul>
-								</div>
-							{/each}
-						</div>
-					{/each}
+				            <hr class="border-gray-100 dark:border-gray-850 my-2" />
+
+				            {#if !isCollapsed}
+				                {#each Object.keys(changelog[version]).filter((section) => section !== 'date') as section}
+				                    <div class="">
+				                        <div
+				                            class="font-semibold uppercase text-xs {section === 'added'
+				                                ? 'text-white bg-blue-600'
+				                                : section === 'fixed'
+				                                    ? 'text-white bg-green-600'
+				                                    : section === 'changed'
+				                                        ? 'text-white bg-yellow-600'
+				                                        : section === 'removed'
+				                                            ? 'text-white bg-red-600'
+				                                            : ''}  w-fit px-3 rounded-full my-2.5"
+				                        >
+				                            {section}
+				                        </div>
+
+				                        <ul class="my-2.5 px-1.5 space-y-2 changelog-content">
+				                            {#each changelog[version][section] as entry}
+				                                {@html `<div class="text-sm">${entry.raw}</div>`}
+				                            {/each}
+				                        </ul>
+				                    </div>
+				                {/each}
+				            {/if}
+				        </div>
+				    {/each}
 				{/if}
 			</div>
 		</div>
