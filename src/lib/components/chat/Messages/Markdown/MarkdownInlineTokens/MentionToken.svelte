d6cdc156884dae9daa54284b8c24be815e14fcb3
Timothy Jaeryang Baek <tim@openwebui.com>
2025-09-14 18:59:09 -0400

    refac

--- src/lib/components/chat/Messages/Markdown/MarkdownInlineTokens/MentionToken.svelte
+++ src/lib/components/chat/Messages/Markdown/MarkdownInlineTokens/MentionToken.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import Tooltip from '$lib/components/common/Tooltip.svelte';
 	import type { Token } from 'marked';
+	import Tooltip from '$lib/components/common/Tooltip.svelte';
 
 	export let token: Token;
 </script>
 
 <Tooltip as="span" className="mention" content={token.id} placement="top">
-	{token.triggerChar}{token.label}
+	{token?.triggerChar ?? '@'}{token?.label ?? token?.id}
 </Tooltip>
