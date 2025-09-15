19e3214997170eea6ee92452e8c778e04a28e396
Timothy Jaeryang Baek <tim@openwebui.com>
2025-09-15 17:21:10 -0500

    refac: folder click behaviour

--- src/lib/components/layout/Sidebar/ChatItem.svelte
+++ src/lib/components/layout/Sidebar/ChatItem.svelte
@@ -401,11 +401,8 @@
 			on:click={() => {
 				dispatch('select');
 
-				if (
-					$selectedFolder &&
-					!($selectedFolder?.items?.chats.map((chat) => chat.id) ?? []).includes(id)
-				) {
-					selectedFolder.set(null); // Reset selected folder if the chat is not in it
+				if ($selectedFolder) {
+					selectedFolder.set(null);
 				}
 
 				if ($mobile) {
