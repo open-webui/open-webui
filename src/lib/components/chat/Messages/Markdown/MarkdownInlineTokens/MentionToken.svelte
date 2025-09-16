d8ad384b0602f7e115aaef9230b8339352129504
Timothy Jaeryang Baek <tim@openwebui.com>
2025-09-16 15:16:48 -0500

    refac

--- src/lib/components/chat/Messages/Markdown/MarkdownInlineTokens/MentionToken.svelte
+++ src/lib/components/chat/Messages/Markdown/MarkdownInlineTokens/MentionToken.svelte
@@ -3,8 +3,31 @@
 	import Tooltip from '$lib/components/common/Tooltip.svelte';
 
 	export let token: Token;
+
+	let triggerChar = '';
+	let label = '';
+
+	let idType = '';
+	let id = '';
+
+	$: if (token) {
+		init();
+	}
+
+	const init = () => {
+		const _id = token?.id;
+		if (_id?.includes(':')) {
+			idType = _id.split(':')[0];
+			id = _id.split(':')[1];
+		} else {
+			id = _id;
+		}
+
+		label = token?.label ?? id;
+		triggerChar = token?.triggerChar ?? '@';
+	};
 </script>
 
-<Tooltip as="span" className="mention" content={token.id} placement="top">
-	{token?.triggerChar ?? '@'}{token?.label ?? token?.id}
+<Tooltip as="span" className="mention" content={id} placement="top">
+	{triggerChar}{label}
 </Tooltip>
