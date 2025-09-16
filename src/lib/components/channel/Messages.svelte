e1824bb7b3f97b9cbcec4d59e689cbe2945bb4bc
Timothy Jaeryang Baek <tim@openwebui.com>
2025-09-16 13:11:53 -0500

    refac

--- src/lib/components/channel/Messages.svelte
+++ src/lib/components/channel/Messages.svelte
@@ -63,11 +63,7 @@
 				</div>
 			</Loader>
 		{:else if !thread}
-			<div
-				class="px-5
-			
-			{($settings?.widescreenMode ?? null) ? 'max-w-full' : 'max-w-5xl'} mx-auto"
-			>
+			<div class="px-5 max-w-full mx-auto">
 				{#if channel}
 					<div class="flex flex-col gap-1.5 pb-5 pt-10">
 						<div class="text-2xl font-medium capitalize">{channel.name}</div>
