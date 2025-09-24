c31f67233ff19c3bd25effa4eaceaf6b3cc58263
atrivedi-tsavoritesi <atrivedi@tsavoritesi.com>
2025-09-24 06:50:21 -0700

    @FIR-978: Make changes to fit the Xterm in smaller browser windows (#79)
    This change uses default terminal size for Modal to fit in smaller
    windows
    
    The test results are as follows
    
    Co-authored-by: Ashish Trivedi <atrivedi@fpga3.tsavoritesi.net>

--- src/lib/components/layout/Sidebar/TerminalModal.svelte
+++ src/lib/components/layout/Sidebar/TerminalModal.svelte
@@ -6,10 +6,12 @@
 </script>
 
 {#if show}
-	<div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-		<div class="bg-white dark:bg-gray-900 rounded-lg p-4 w-[90%] h-[90%] relative">
+	<div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[9999]">
+		<div
+			class="bg-white dark:bg-gray-900 rounded-lg p-4 font-mono w-[98ch] h-[32em] relative overflow-hidden"
+		>
 			<button class="absolute top-2 right-2 text-gray-500 hover:text-gray-800" on:click={onClose}
-				>X</button
+				>x</button
 			>
 			<iframe src={url} class="w-full h-full border-none rounded"></iframe>
 		</div>
