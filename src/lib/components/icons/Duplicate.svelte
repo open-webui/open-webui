74c193a42230fb60c9091e70e27ab9cf564ad412
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-09-28 16:40:51 +0200

    Update Duplicate.svelte

--- src/lib/components/icons/Duplicate.svelte
+++ src/lib/components/icons/Duplicate.svelte
@@ -6,7 +6,7 @@
 	stroke-width="2"
 	stroke-linecap="round"
 	stroke-linejoin="round"
-	class="size-6 {$props.class}"
+	class="size-6 {$$props.class}"
 >
 	<rect width="14" height="14" x="8" y="8" rx="2" ry="2"/>
 	<path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/>
