ebb7612eac18fb769d2ef598d6a860969abe73bb
_00_ <131402327+rgaricano@users.noreply.github.com>
2025-09-29 19:18:26 +0200

    Update env.py - Support sequential tool chaining in Default Function Calling Mode.

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -807,3 +807,9 @@ PIP_PACKAGE_INDEX_OPTIONS = os.getenv("PIP_PACKAGE_INDEX_OPTIONS", "").split()
 ####################################
 
 EXTERNAL_PWA_MANIFEST_URL = os.environ.get("EXTERNAL_PWA_MANIFEST_URL")
+
+####################################
+# SEQUENTIAL DEFAULT FUNCTION CALLING
+####################################
+
+CHAT_RESPONSE_MAX_TOOL_CALL_RETRIES = int(os.environ.get("CHAT_RESPONSE_MAX_TOOL_CALL_RETRIES", "5"))
