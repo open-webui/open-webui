1b8240b396143115c1da0bd4754e3db235533aed
_00_ <131402327+rgaricano@users.noreply.github.com>
2025-10-02 03:41:55 +0200

    Update evaluations.py  - Code Format

--- backend/open_webui/routers/evaluations.py
+++ backend/open_webui/routers/evaluations.py
@@ -89,7 +89,7 @@ async def get_all_feedbacks(user=Depends(get_admin_user)):
         user_dict = None  
         if user_obj:
             user_dict = (
-                {c.name: getattr(user_obj, c.name)for c in user_obj.__table__.columns}
+                {c.name: getattr(user_obj, c.name) for c in user_obj.__table__.columns}
                 if user_obj
                 else None
             )
