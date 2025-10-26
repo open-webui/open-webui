dcf6667a2b85b1a77b97ea7654c11278ebee4019
silentoplayz <jacwoo21@outlook.com>
2025-10-26 12:01:45 -0400

    fix icon

--- src/lib/components/admin/Settings/Interface/Banners.svelte
+++ src/lib/components/admin/Settings/Interface/Banners.svelte
@@ -85,14 +85,16 @@
 					<Tooltip content={$i18n.t('Remember Dismissal')}>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-							class="size-3.5 text-gray-500"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="size-3.5"
 						>
 							<path
-								fill-rule="evenodd"
-								d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-7-4a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM9 9a.75.75 0 0 0 0 1.5h.253a.25.25 0 0 1 .244.304l-.459 2.066A1.75 1.75 0 0 0 10.747 15H11a.75.75 0 0 0 0-1.5h-.253a.25.25 0 0 1-.244-.304l.459-2.066A1.75 1.75 0 0 0 9.253 9H9Z"
-								clip-rule="evenodd"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
 							/>
 						</svg>
 					</Tooltip>
