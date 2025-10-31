0a67d6bd2a6c732de90f986e720c5589175c96d2
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-10-31 11:55:29 +0100

    Update ConfigureModelsModal.svelte

--- src/lib/components/admin/Settings/Models/ConfigureModelsModal.svelte
+++ src/lib/components/admin/Settings/Models/ConfigureModelsModal.svelte
@@ -273,6 +273,60 @@
 							</div>
 						</div>
 
+						<hr class=" border-gray-100 dark:border-gray-700/10 my-2.5 w-full" />
+
+						<div>
+							<div class="flex flex-col w-full">
+								<div class="mb-1 flex justify-between">
+									<div class="text-xs text-gray-500">{$i18n.t('Default Pinned Models')}</div>
+								</div>
+
+								<div class="flex items-center -mr-1">
+									<select
+										class="w-full py-1 text-sm rounded-lg bg-transparent {selectedPinnedModelId
+											? ''
+											: 'text-gray-500'} placeholder:text-gray-300 dark:placeholder:text-gray-700 outline-hidden"
+										bind:value={selectedPinnedModelId}
+									>
+										<option value="">{$i18n.t('Select a model')}</option>
+										{#each $models as model}
+											<option value={model.id} class="bg-gray-50 dark:bg-gray-700"
+												>{model.name}</option
+											>
+										{/each}
+									</select>
+								</div>
+
+								{#if defaultPinnedModelIds.length > 0}
+									<div class="flex flex-col">
+										{#each defaultPinnedModelIds as modelId, modelIdx}
+											<div class=" flex gap-2 w-full justify-between items-center">
+												<div class=" text-sm flex-1 py-1 rounded-lg">
+													{$models.find((model) => model.id === modelId)?.name}
+												</div>
+												<div class="shrink-0">
+													<button
+														type="button"
+														on:click={() => {
+															defaultPinnedModelIds = defaultPinnedModelIds.filter(
+																(_, idx) => idx !== modelIdx
+															);
+														}}
+													>
+														<Minus strokeWidth="2" className="size-3.5" />
+													</button>
+												</div>
+											</div>
+										{/each}
+									</div>
+								{:else}
+									<div class="text-gray-500 text-xs text-center py-2">
+										{$i18n.t('No models selected')}
+									</div>
+								{/if}
+							</div>
+						</div>
+
 						<div class="flex justify-between pt-3 text-sm font-medium gap-1.5">
 							<Tooltip content={$i18n.t('This will delete all models including custom models')}>
 								<button
