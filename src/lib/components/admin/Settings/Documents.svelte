d59a65c8a37b0800902b91523f961d2723566814
Sam Mueller <sam.mueller@ssc-spc.gc.ca>
2025-10-31 15:43:14 -0400

    feat: add Docling support for advanced document loading and update translations

--- src/lib/components/admin/Settings/Documents.svelte
+++ src/lib/components/admin/Settings/Documents.svelte
@@ -583,6 +583,7 @@
 					>
 						<option value="">{$i18n.t('Default')} </option>
 						<option value="tika">{$i18n.t('Tika')}</option>
+						<option value="docling">{$i18n.t('Docling')}</option>
 					</select>
 				</div>
 			</div>
