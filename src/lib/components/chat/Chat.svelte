299e663d193dcc44aafd359334bcf0519bd6de12
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-11-08 17:35:08 +0100

    Update Chat.svelte

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -855,11 +855,11 @@
 
 				if (htmlContent || cssContent || jsContent) {
 					const renderedContent = `
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+						<!DOCTYPE html>
+						<html lang="en">
+						<head>
+							<meta charset="UTF-8">
+							<meta name="viewport" content="width=device-width, initial-scale=1.0">
 							<${''}style>
 								body {
 									background-color: white; /* Ensure the iframe has a white background */
@@ -867,16 +867,16 @@
 
 								${cssContent}
 							</${''}style>
-                        </head>
-                        <body>
-                            ${htmlContent}
+						</head>
+						<body>
+							${htmlContent}
 
 							<${''}script>
-                            	${jsContent}
+								${jsContent}
 							</${''}script>
-                        </body>
-                        </html>
-                    `;
+						</body>
+						</html>
+					`;
 					contents = [...contents, { type: 'iframe', content: renderedContent }];
 				} else {
 					// Check for SVG content
@@ -1935,64 +1935,64 @@
 		}
 
 		const res = await generateOpenAIChatCompletion(
-		    localStorage.token,
-		    {
-		        stream: stream,
-		        model: model.id,
-		        messages: messages,
-		        params: {
-		            ...$settings?.params,
-		            ...params,
-		            stop:
-		                (params?.stop ?? $settings?.params?.stop ?? undefined)
-		                    ? (params?.stop.split(',').map((token) => token.trim()) ?? $settings.params.stop).map(
-		                            (str) => decodeURIComponent(JSON.parse('"' + str.replace(/\"/g, '\\"') + '"'))
-		                        )
-		                    : undefined
-		        },
+			localStorage.token,
+			{
+				stream: stream,
+				model: model.id,
+				messages: messages,
+				params: {
+					...$settings?.params,
+					...params,
+					stop:
+						(params?.stop ?? $settings?.params?.stop ?? undefined)
+							? (params?.stop.split(',').map((token) => token.trim()) ?? $settings.params.stop).map(
+									(str) => decodeURIComponent(JSON.parse('"' + str.replace(/\"/g, '\\"') + '"'))
+								)
+							: undefined
+				},
 
-		        files: (files?.length ?? 0) > 0 ? files : undefined,
+				files: (files?.length ?? 0) > 0 ? files : undefined,
 
-		        filter_ids: selectedFilterIds.length > 0 ? selectedFilterIds : undefined,
-		        tool_ids: toolIds.length > 0 ? toolIds : undefined,
-		        tool_servers: ($toolServers ?? []).filter(
-		            (server, idx) => toolServerIds.includes(idx) || toolServerIds.includes(server?.id)
-		        ),
-		        features: getFeatures(),
-		        variables: {
-		            ...getPromptVariables($user?.name, $settings?.userLocation ? userLocation : undefined)
-		        },
-		        model_item: $models.find((m) => m.id === model.id),
+				filter_ids: selectedFilterIds.length > 0 ? selectedFilterIds : undefined,
+				tool_ids: toolIds.length > 0 ? toolIds : undefined,
+				tool_servers: ($toolServers ?? []).filter(
+					(server, idx) => toolServerIds.includes(idx) || toolServerIds.includes(server?.id)
+				),
+				features: getFeatures(),
+				variables: {
+					...getPromptVariables($user?.name, $settings?.userLocation ? userLocation : undefined)
+				},
+				model_item: $models.find((m) => m.id === model.id),
 
-		        session_id: $socket?.id,
-		        chat_id: $chatId,
-		        id: responseMessageId,
-		        call: $showCallOverlay,
+				session_id: $socket?.id,
+				chat_id: $chatId,
+				id: responseMessageId,
+				call: $showCallOverlay,
 
-		        background_tasks: {
-		            ...(!$temporaryChatEnabled &&
-		            (messages.length == 1 ||
-		                (messages.length == 2 &&
-		                    messages.at(0)?.role === 'system' &&
-		                    messages.at(1)?.role === 'user')) &&
-		            (selectedModels[0] === model.id || atSelectedModel !== undefined)
-		                ? {
-		                        title_generation: $settings?.title?.auto ?? true,
-		                        tags_generation: $settings?.autoTags ?? true
-		                    }
-		                : {}),
-		            follow_up_generation: $settings?.autoFollowUps ?? true
-		        },
+				background_tasks: {
+					...(!$temporaryChatEnabled &&
+					(messages.length == 1 ||
+						(messages.length == 2 &&
+							messages.at(0)?.role === 'system' &&
+							messages.at(1)?.role === 'user')) &&
+					(selectedModels[0] === model.id || atSelectedModel !== undefined)
+						? {
+								title_generation: $settings?.title?.auto ?? true,
+								tags_generation: $settings?.autoTags ?? true
+							}
+						: {}),
+					follow_up_generation: $settings?.autoFollowUps ?? true
+				},
 
-		        ...(stream && (model.info?.meta?.capabilities?.usage ?? false)
-		            ? {
-		                    stream_options: {
-		                        include_usage: true
-		                    }
-		                }
-		            : {})
-		    },
-		    `${WEBUI_BASE_URL}/api`
+				...(stream && (model.info?.meta?.capabilities?.usage ?? false)
+					? {
+							stream_options: {
+								include_usage: true
+							}
+						}
+					: {})
+			},
+			`${WEBUI_BASE_URL}/api`
 		).catch(async (error) => {
 			console.log(error);
 
@@ -2553,11 +2553,11 @@
 										}
 									}}
 									on:submit={async (e) => {
-									    clearDraft();
-									    if (e.detail || files.length > 0) {
-									        await tick();
-									        dispatch('submit', e.detail.replaceAll('\n\n', '\n'));
-									    }
+										clearDraft();
+										if (e.detail || files.length > 0) {
+											await tick();
+											dispatch('submit', e.detail.replaceAll('\n\n', '\n'));
+										}
 									}}
 								/>
 
