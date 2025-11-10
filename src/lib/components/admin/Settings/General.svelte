7037e8694d564d6db1f44ecfa072e75f75a373d8
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-11-10 09:01:18 +0100

    Update General.svelte

--- src/lib/components/admin/Settings/General.svelte
+++ src/lib/components/admin/Settings/General.svelte
@@ -305,14 +305,6 @@
 						<Switch bind:state={adminConfig.ENABLE_SIGNUP} />
 					</div>
 
-					<div class=" mb-2.5 flex w-full justify-between pr-2">
-						<div class=" self-center text-xs font-medium">
-							{$i18n.t('Enable Password-Based Login')}
-						</div>
-
-						<Switch bind:state={adminConfig.ENABLE_PASSWORD_BASED_LOGIN} />
-					</div>
-
 					<div class="mb-2.5 flex w-full items-center justify-between pr-2">
 						<div class=" self-center text-xs font-medium">
 							{$i18n.t('Show Admin Details in Account Pending Overlay')}
