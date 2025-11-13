c8b51af7ab6c69d0788647b6ea569c09e7a76923
cathytsui <48180241+cathy-thy@users.noreply.github.com>
2025-11-13 14:52:49 -0500

    refactor super admin hardcode logic to backend

--- src/lib/apis/users/index.ts
+++ src/lib/apis/users/index.ts
@@ -1,6 +1,34 @@
 import { WEBUI_API_BASE_URL } from '$lib/constants';
 import { getUserPosition } from '$lib/utils';
 
+// Add API calls for super admin checks
+export const checkIfSuperAdmin = async (token: string, email: string) => {
+	let error = null;
+
+	const res = await fetch(`${WEBUI_API_BASE_URL}/users/is-super-admin?email=${encodeURIComponent(email)}`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`
+		}
+	})
+		.then(async (res) => {
+			if (!res.ok) throw await res.json();
+			return res.json();
+		})
+		.catch((err) => {
+			console.log(err);
+			error = err.detail;
+			return false;
+		});
+
+	if (error) {
+		throw error;
+	}
+
+	return res;
+};
+
 export const getUserGroups = async (token: string) => {
 	let error = null;
 
