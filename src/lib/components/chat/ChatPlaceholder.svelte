4d4c572bbab0a9db8522e33b6645a86f55a2efb1
Timothy Jaeryang Baek <tim@openwebui.com>
2025-11-21 15:42:13 -0500

    refac

--- src/lib/components/chat/ChatPlaceholder.svelte
+++ src/lib/components/chat/ChatPlaceholder.svelte
@@ -53,7 +53,7 @@
 							placement="right"
 						>
 							<img
-								src={`${WEBUI_API_BASE_URL}/models/model/profile/image?id=${model.id}&lang=${$i18n.language}`}
+								src={`${WEBUI_API_BASE_URL}/models/model/profile/image?id=${model?.id}&lang=${$i18n.language}`}
 								class=" size-[2.7rem] rounded-full border-[1px] border-gray-100 dark:border-none"
 								alt="logo"
 								draggable="false"
