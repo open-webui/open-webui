833e3c269066d864b69002f0c34a69ca2a8212f1
Timothy Jaeryang Baek <tim@openwebui.com>
2025-11-21 17:45:55 -0500

    refac

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -627,7 +627,7 @@
 	>
 		<div class={`shrink-0 ltr:mr-3 rtl:ml-3 hidden @lg:flex mt-1 `}>
 			<ProfileImage
-				src={`${WEBUI_API_BASE_URL}/models/model/profile/image?id=${model.id}&lang=${$i18n.language}`}
+				src={`${WEBUI_API_BASE_URL}/models/model/profile/image?id=${model?.id}&lang=${$i18n.language}`}
 				className={'size-8 assistant-message-profile-image'}
 			/>
 		</div>
