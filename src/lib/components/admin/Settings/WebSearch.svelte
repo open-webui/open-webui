9116a76e448dcae38b50d72f45b0bd34372628f6
Vincenzo <sniirful@gmail.com>
2025-11-24 04:40:34 +0100

    enh: web search timeout and retry count options in the webui

--- src/lib/components/admin/Settings/WebSearch.svelte
+++ src/lib/components/admin/Settings/WebSearch.svelte
@@ -775,6 +775,30 @@
 								<Switch bind:state={webConfig.ENABLE_WEB_LOADER_SSL_VERIFICATION} />
 							</div>
 						</div>
+            <div class="mb-2.5 w-full">
+              <div class=" self-center text-xs font-medium mb-1">
+                {$i18n.t('Request Timeout (ms)')}
+              </div>
+
+              <input
+                class="w-full rounded-lg py-2 px-4 text-sm bg-gray-50 dark:text-gray-300 dark:bg-gray-850 outline-hidden"
+                placeholder={$i18n.t('Request Timeout (ms)')}
+                bind:value={webConfig.WEB_LOADER_TIMEOUT}
+                required
+              />
+            </div>
+            <div class="mb-2.5 w-full">
+              <div class=" self-center text-xs font-medium mb-1">
+                {$i18n.t('Retry Count')}
+              </div>
+
+              <input
+                class="w-full rounded-lg py-2 px-4 text-sm bg-gray-50 dark:text-gray-300 dark:bg-gray-850 outline-hidden"
+                placeholder={$i18n.t('Retry Count')}
+                bind:value={webConfig.WEB_LOADER_RETRY_COUNT}
+                required
+              />
+            </div>
 					{:else if webConfig.WEB_LOADER_ENGINE === 'playwright'}
 						<div class="mb-2.5 flex w-full flex-col">
 							<div>
