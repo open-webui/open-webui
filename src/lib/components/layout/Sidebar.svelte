9707ec20d48a8a67a6921553022e8ae0c8beef25
Gaofeng <u3011873@connect.hku.hk>
2025-11-24 21:49:04 +0800

    修改 sidebar 左上角 logo 不显示的问题

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -795,8 +795,8 @@
 				>
 					<img
 						crossorigin="anonymous"
-						src="{WEBUI_BASE_URL}/static/favicon.png"
-						class="sidebar-new-chat-icon size-6 rounded-full"
+						src="static/favicon.png"
+						class="sidebar-new-chat-icon size-6 rounded-full invert"
 						alt=""
 					/>
 				</a>
