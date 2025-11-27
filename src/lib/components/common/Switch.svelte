e132203a287328d644167991cee388ca6809e10a
Sam Mueller <sam-mueller@hotmail.com>
2025-11-27 11:39:53 -0500

    style: update button styles for consistency across components using dark mode (#295)

--- src/lib/components/common/Switch.svelte
+++ src/lib/components/common/Switch.svelte
@@ -16,7 +16,7 @@
 	aria-label={ariaLabel}
 	class="flex h-5 min-h-5 w-9 shrink-0 cursor-pointer items-center rounded-full px-[3px] mx-[1px] transition  {state
 		? ' bg-emerald-600'
-		: 'bg-gray-200 dark:bg-transparent'} outline outline-1 outline-gray-100 dark:outline-gray-800 {disabled
+		: 'bg-gray-200 dark:bg-gray-700'} outline outline-1 outline-gray-100 dark:outline-gray-500 {disabled
 		? 'opacity-50 cursor-not-allowed'
 		: ''}"
 >
