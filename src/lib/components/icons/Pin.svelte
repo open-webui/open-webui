0518749d5163320444d3ca0e5984085cbb857e0f
Timothy Jaeryang Baek <tim@openwebui.com>
2025-11-28 08:01:42 -0500

    refac: pin icons

--- src/lib/components/icons/Pin.svelte
+++ src/lib/components/icons/Pin.svelte
@@ -14,8 +14,8 @@
 	width="24"
 	height="24"
 	><path d="M9.5 14.5L3 21" stroke-linecap="round" stroke-linejoin="round"></path><path
-		d="M7.67602 7.8896L6.69713 7.78823L5.00007 9.48528L14.1925 18.6777L15.8895 16.9806L15.7879 16M11.4847 7L15.1568 2.67141L21.0065 8.5211L16.6991 12.175"
+		d="M5.00007 9.48528L14.1925 18.6777L15.8895 16.9806L15.4974 13.1944L21.0065 8.5211L15.1568 2.67141L10.4834 8.18034L6.69713 7.78823L5.00007 9.48528Z"
 		stroke-linecap="round"
 		stroke-linejoin="round"
-	></path><path d="M3 3L21 21" stroke-linecap="round" stroke-linejoin="round"></path></svg
+	></path></svg
 >
