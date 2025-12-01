3e5781ce34ba1da9e1008ef7e9923f33073f2f77
Jiaqi Yi <jy4421@nyu.edu>
2025-12-01 15:11:01 -0500

    path fix for load_animation

--- src/lib/components/common/LoadAnimation.svelte
+++ src/lib/components/common/LoadAnimation.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-  export let src: string = "/static/static/load_animation.html";
+  export let src: string = "/static/load_animation.html";
   export let title: string = "Loading";
   export let style: string = "position:absolute;top:0;left:0;width:100%;height:100%;border:0;background:transparent";
 </script>
 
 <iframe {src} {title} {style}></iframe>
-
