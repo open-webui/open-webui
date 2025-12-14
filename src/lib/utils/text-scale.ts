9fbae88b198faaac0c7fbe8021fa2b51f328ece9
Classic298 <27028174+Classic298@users.noreply.github.com>
2025-12-14 17:28:34 +0100

    Update text-scale.ts

--- src/lib/utils/text-scale.ts
+++ src/lib/utils/text-scale.ts
@@ -2,7 +2,5 @@ export const setTextScale = (scale) => {
 	if (typeof document === 'undefined') {
 		return;
 	}
-
-	console.log('setTextScale called with:', scale, 'from:', new Error().stack);
 	document.documentElement.style.setProperty('--app-text-scale', `${scale}`);
 };
