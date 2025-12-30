48fb361a0946faf33a6f912719c4bf21dcf3fb20
Leon <752488551@qq.com>
2025-12-31 00:33:04 +0800

    更新

--- src/lib/components/chat/ModelSelector/ModelItemMenu.svelte
+++ src/lib/components/chat/ModelSelector/ModelItemMenu.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { DropdownMenu } from 'bits-ui';
 	import { flyAndScale } from '$lib/utils/transitions';
+	import { goto } from '$app/navigation';
 
 	import { getContext } from 'svelte';
 
@@ -8,7 +9,8 @@
 	import Link from '$lib/components/icons/Link.svelte';
 	import Eye from '$lib/components/icons/Eye.svelte';
 	import EyeSlash from '$lib/components/icons/EyeSlash.svelte';
-	import { settings } from '$lib/stores';
+	import Cog6 from '$lib/components/icons/Cog6.svelte';
+	import { settings, user } from '$lib/stores';
 
 	const i18n = getContext('i18n');
 
@@ -92,5 +94,22 @@
 
 			<div class="flex items-center">{$i18n.t('Copy Link')}</div>
 		</DropdownMenu.Item>
+		{#if $user?.role === 'admin'}
+			<DropdownMenu.Item
+				type="button"
+				class="flex rounded-xl py-1.5 px-3 w-full hover:bg-gray-50 dark:hover:bg-gray-800 transition items-center gap-2"
+				on:click={async (e) => {
+					e.stopPropagation();
+					e.preventDefault();
+
+					show = false;
+					await goto(`/admin/settings/models?id=${encodeURIComponent(model?.id)}`);
+				}}
+			>
+				<Cog6 />
+
+				<div class="flex items-center">{$i18n.t('Model Settings')}</div>
+			</DropdownMenu.Item>
+		{/if}
 	</DropdownMenu.Content>
 </DropdownMenu.Root>
