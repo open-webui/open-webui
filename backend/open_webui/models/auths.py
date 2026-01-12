b7f2acebc10fcf8b184f77e98f82bac11726c672
Jinnyeong <wlssud0701@naver.com>
2026-01-12 19:58:05 +0900

    [01.12] sso 연동

--- backend/open_webui/models/auths.py
+++ backend/open_webui/models/auths.py
@@ -97,6 +97,8 @@ class AuthsTable:
         profile_image_url: str = "/user.png",
         role: str = "pending",
         oauth_sub: Optional[str] = None,
+        department: Optional[str] = None,
+        user_status: Optional[str] = None,
     ) -> Optional[UserModel]:
         with get_db() as db:
             log.info("insert_new_auth")
@@ -110,7 +112,8 @@ class AuthsTable:
             db.add(result)
 
             user = Users.insert_new_user(
-                id, name, email, profile_image_url, role, oauth_sub
+                id, name, email, profile_image_url, role, oauth_sub,
+                department=department, user_status=user_status
             )
 
             db.commit()
