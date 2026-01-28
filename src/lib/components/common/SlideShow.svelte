815abbd15020c4df0eb2870201ebb8140cfbb04a
您的名字 <@example.com>
2026-01-29 00:04:32 +0800

    11

--- src/lib/components/common/SlideShow.svelte
+++ src/lib/components/common/SlideShow.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { WEBUI_BASE_URL } from '$lib/constants';
 	import { onMount } from 'svelte';
 
 	export let imageUrls = [
-		`${WEBUI_BASE_URL}/assets/images/adam.jpg`,
-		`${WEBUI_BASE_URL}/assets/images/galaxy.jpg`,
-		`${WEBUI_BASE_URL}/assets/images/earth.jpg`,
-		`${WEBUI_BASE_URL}/assets/images/space.jpg`
+		'/assets/images/adam.jpg',
+		'/assets/images/galaxy.jpg',
+		'/assets/images/earth.jpg',
+		'/assets/images/space.jpg'
 	];
 	export let duration = 5000;
 	let selectedImageIdx = 0;
