5cb9c43e06444c7eebafbcaf639cc1dc70dbf134
jjdrisco <jjdrisco@ucsd.edu>
2026-02-05 12:22:11 -0800

    vite fixes for npm build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Get backend port from environment variable, default to 8080
 const BACKEND_PORT = process.env.BACKEND_PORT || '8080';
@@ -9,6 +14,15 @@ const BACKEND_HOST = process.env.BACKEND_HOST || 'localhost';
 const backendUrl = `http://${BACKEND_HOST}:${BACKEND_PORT}`;
 
 export default defineConfig({
+	resolve: {
+		conditions: ['svelte', 'import', 'module', 'browser', 'default'],
+		alias: {
+			'svelte-confetti': path.resolve(__dirname, 'node_modules/svelte-confetti/dist/index.js'),
+			paneforge: path.resolve(__dirname, 'node_modules/paneforge/dist/index.js'),
+			'bits-ui': path.resolve(__dirname, 'node_modules/bits-ui/dist/index.js'),
+			'@melt-ui/svelte': path.resolve(__dirname, 'node_modules/@melt-ui/svelte/dist/index.js')
+		}
+	},
 	plugins: [
 		sveltekit(),
 		viteStaticCopy({
