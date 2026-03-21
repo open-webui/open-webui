877bc23afc2d0f6279ee9428a2efb388715680ea
Timothy Jaeryang Baek <tim@openwebui.com>
2026-03-21 18:42:55 -0500

    refac

--- backend/open_webui/utils/middleware.py
+++ backend/open_webui/utils/middleware.py
@@ -2884,7 +2884,7 @@ async def background_tasks_handler(ctx):
                                 }
                             )
 
-                    if title == None and len(messages) == 2:
+                    if title == None and len(messages) == 2 and (not messages_map or len(messages_map) <= 2):
                         title = messages[0].get('content', user_message)
 
                         Chats.update_chat_title_by_id(metadata['chat_id'], title)
