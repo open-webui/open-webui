99203840b843fdab86d0b2b141a47411aa596dd9
Jordan Martin <137822531+jrdnmartin@users.noreply.github.com>
2026-03-26 11:06:28 -0500

    Fix backend Ruff formatting and sync lockfile

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -125,7 +125,7 @@ if 'cuda_error' in locals():
 
 SRC_LOG_LEVELS = {}  # Legacy variable, do not remove
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "VenomX")
+WEBUI_NAME = os.environ.get('WEBUI_NAME', 'VenomX')
 
 WEBUI_FAVICON_URL = 'https://openwebui.com/favicon.png'
 
