91d029302f6fd04d1fad942bd7183adfad2f7dee
Algorithm5838 <108630393+Algorithm5838@users.noreply.github.com>
2026-03-31 11:50:00 +0300

    perf: move token fade-in animation to global CSS

--- src/lib/components/chat/Messages/Markdown/MarkdownInlineTokens/CodespanToken.svelte
+++ src/lib/components/chat/Messages/Markdown/MarkdownInlineTokens/CodespanToken.svelte
@@ -19,17 +19,3 @@
 		toast.success($i18n.t('Copied to clipboard'));
 	}}>{unescapeHtml(token.text)}</code
 >
-
-<style>
-	@keyframes fade-in-token {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
-	.fade-in-token {
-		animation: fade-in-token 100ms ease-out;
-	}
-</style>
