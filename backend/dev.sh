0597eb84861807a391bd2617b101f0ec7a098aed
Brad Merrill <brad_merrill@hotmail.com>
2026-04-02 02:42:07 -0700

    WIP: commit current changes

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,4 @@
 export CORS_ALLOW_ORIGIN="http://localhost:5173;http://localhost:8080"
+export USER_AGENT="open-webui-agent"
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips "${FORWARDED_ALLOW_IPS:-*}" --reload
--- src/lib/components/chat/Messages/Message.svelte
+++ src/lib/components/chat/Messages/Message.svelte
@@ -47,7 +47,7 @@
 
 <div
 	role="listitem"
-	class="flex flex-col justify-between px-5 mb-3 w-full {($settings?.widescreenMode ?? null)
+	class="flex flex-col justify-between px-5 mb-8 w-full {($settings?.widescreenMode ?? null)
 		? 'max-w-full'
 		: 'max-w-5xl'} mx-auto rounded-lg group"
 >
