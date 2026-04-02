63b93b934ebb26ec268af10d362ecab05dfbc99d
DrMelone <27028174+Classic298@users.noreply.github.com>
2026-04-02 20:30:15 +0200

    fix: add SSRF protection to OAuth profile picture URL fetching

--- backend/open_webui/utils/oauth.py
+++ backend/open_webui/utils/oauth.py
@@ -80,6 +80,7 @@ from open_webui.utils.misc import parse_duration
 from open_webui.utils.auth import get_password_hash, create_token
 from open_webui.utils.webhook import post_webhook
 from open_webui.utils.groups import apply_default_group_assignment
+from open_webui.retrieval.web.utils import validate_url
 
 from mcp.shared.auth import (
     OAuthClientMetadata as MCPOAuthClientMetadata,
@@ -1329,6 +1330,8 @@ class OAuthManager:
             return '/user.png'
 
         try:
+            validate_url(picture_url)
+
             get_kwargs = {}
             if access_token:
                 get_kwargs['headers'] = {
