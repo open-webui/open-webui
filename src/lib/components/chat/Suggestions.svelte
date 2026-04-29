151860053aa081ec3c8f8086d204fc615e20cbec
Jaafoura <mehdi.jaafourag@gmail.com>
2026-04-29 14:30:03 +0100

    remove "suggested" text

--- src/lib/components/chat/Suggestions.svelte
+++ src/lib/components/chat/Suggestions.svelte
@@ -65,10 +65,7 @@
 </script>
 
 <div class="mb-1 flex gap-1 text-xs font-medium items-center text-gray-600 dark:text-gray-400">
-	{#if filteredPrompts.length > 0}
-		<Bolt />
-		{$i18n.t('Suggested')}
-	{:else}
+	{#if filteredPrompts.length === 0}
 		<!-- Keine Vorschläge -->
 
 		<div
