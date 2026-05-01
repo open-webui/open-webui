6b85640f251f08dc6158c4041c62facc0b8d110d
Genmin <joey@joeyroth.com>
2026-05-01 14:51:07 -0700

    test: cover documented Feishu scope precedence

--- backend/open_webui/test/utils/test_env_vars.py
+++ backend/open_webui/test/utils/test_env_vars.py
@@ -31,6 +31,23 @@ def test_get_env_with_aliases_falls_back_to_legacy_alias():
     )
 
 
+def test_get_env_with_aliases_prefers_documented_scope_env_name():
+    environ = {
+        'FEISHU_CLIENT_SCOPE': 'contact:user.base:readonly',
+        'FEISHU_OAUTH_SCOPE': 'contact:user.email:readonly',
+    }
+
+    assert (
+        get_env_with_aliases(
+            'FEISHU_CLIENT_SCOPE',
+            'FEISHU_OAUTH_SCOPE',
+            default='contact:user.base:readonly',
+            environ=environ,
+        )
+        == 'contact:user.base:readonly'
+    )
+
+
 def test_get_env_with_aliases_skips_empty_documented_env_name():
     environ = {
         'FEISHU_CLIENT_SCOPE': '',
