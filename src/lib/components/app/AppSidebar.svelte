bf723f26b38886d34b777f8686d5027062d24e53
Malek Salem <malek.salem.14@gmail.com>
2026-05-05 19:36:12 +0100

    restore old image paths

--- src/lib/components/app/AppSidebar.svelte
+++ src/lib/components/app/AppSidebar.svelte
@@ -30,7 +30,7 @@
 				}}
 			>
 				<img
-					src="{WEBUI_BASE_URL}/static/m3llm-coffee-animated.gif"
+					src="{WEBUI_BASE_URL}/static/splash.png"
 					class="size-11 dark:invert p-0.5"
 					alt="logo"
 					draggable="false"
