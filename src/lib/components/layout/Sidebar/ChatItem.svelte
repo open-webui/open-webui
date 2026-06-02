bd2245d6c6d0e9c92711960848b3c5875085c7e2
Jaafoura <mehdi.jaafourag@gmail.com>
2026-06-02 16:02:55 +0100

    Revert "change blue notifications  color"
    This reverts commit 05602f291ca1a09da40e2fba6fbacbb71c6c5291.

--- src/lib/components/layout/Sidebar/ChatItem.svelte
+++ src/lib/components/layout/Sidebar/ChatItem.svelte
@@ -495,7 +495,7 @@
 			<div class="flex self-center flex-1 w-full min-w-0">
 				{#if unread}
 					<div class="shrink-0 self-center pr-2.5 flex transition-opacity duration-300">
-						<div class="size-1.5 bg-gray-300 rounded-full" />
+						<div class="size-1.5 bg-sky-500 rounded-full" />
 					</div>
 				{/if}
 				<div
