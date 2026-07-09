db802e28d359552369830108fe4d23f273145cb3
Timothy Jaeryang Baek <tim@openwebui.com>
2026-07-09 17:29:07 -0500

    refac

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -302,6 +302,7 @@ type Config = {
 		enable_admin_analytics: boolean;
 		enable_community_sharing: boolean;
 		enable_memories: boolean;
+		enable_plugins?: boolean;
 		enable_autocomplete_generation: boolean;
 		enable_direct_connections: boolean;
 		enable_version_update_check: boolean;
