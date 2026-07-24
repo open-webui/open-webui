de939a656260284097748f00fcc94bbceda93c75
Timothy Jaeryang Baek <tim@openwebui.com>
2026-07-24 00:44:06 -0400

    refac

--- src/lib/components/admin/Settings/Connections/ManageOllamaModal.svelte
+++ src/lib/components/admin/Settings/Connections/ManageOllamaModal.svelte
@@ -29,7 +29,7 @@
 			</button>
 		</div>
 
-		<div class="flex flex-col md:flex-row w-full px-5 pb-4 md:space-x-4 dark:text-gray-200">
+		<div class="flex flex-col md:flex-row w-full px-4 pb-4 md:space-x-4 dark:text-gray-200">
 			<ManageOllama {urlIdx} />
 		</div>
 	</div>
