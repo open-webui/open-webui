fd22d15bf70ecc70272c71ae9a4ee6df21ae24a5
Frankenstein-dev197 <coumbassaabdoulaye98@gmail.com>
2026-08-21 18:11:30 +0000

    feat(android): configure Android build plugins

---
+++ android/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+}
