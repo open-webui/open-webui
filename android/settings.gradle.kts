af4a252bace9824c177e657149ebbe8b540e85af
Frankenstein-dev197 <coumbassaabdoulaye98@gmail.com>
2026-08-21 18:11:25 +0000

    feat(android): add native Android project skeleton

---
+++ android/settings.gradle.kts
@@ -0,0 +1,4 @@
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
+rootProject.name = "OpenWebUIAndroid"
+include(":app")
