b49db6f287b450effc8b13c05e914ebeef21ec80
G30 <50341825+silentoplayz@users.noreply.github.com>
2026-09-04 15:54:10 -0400

    fix: stop the browser back button creating extra notes (#29645)

--- src/routes/(app)/notes/+page.svelte
+++ src/routes/(app)/notes/+page.svelte
@@ -24,7 +24,7 @@
 			const res = await createNoteHandler(title, content);
 
 			if (res) {
-				goto(`/notes/${res.id}`);
+				goto(`/notes/${res.id}`, { replaceState: true });
 			}
 			return;
 		}
