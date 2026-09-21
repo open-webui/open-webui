a9541c18ca2a056f4ccfe56b9c733cba74b86b49
Timothy Jaeryang Baek <tim@openwebui.com>
2026-09-21 10:19:59 -0400

    refac

--- src/lib/components/layout/Sidebar/ChatItem.svelte
+++ src/lib/components/layout/Sidebar/ChatItem.svelte
@@ -41,7 +41,7 @@
 		settings,
 		user
 	} from '$lib/stores';
-	import { refreshChatList } from '$lib/stores/chatList';
+	import { refreshChatList, refreshSidebar } from '$lib/stores/chatList';
 
 	import ChatMenu from './ChatMenu.svelte';
 	import DeleteConfirmDialog from '$lib/components/common/ConfirmDialog.svelte';
@@ -241,7 +241,7 @@
 		if (res) {
 			goto(`/c/${res.id}`);
 
-			await refreshChatList(localStorage.token, { refreshPinned: true });
+			await refreshSidebar(localStorage.token);
 		}
 	};
 
